fbc0fe0ff98d20280c541da88358465fff3c3ca7
rainbowsoft <rainbowsoft@40c8d13a-81fc-95dd-32b5-41977a75f8b4>
2013-07-07 14:34:21 +0000

    接着做安装;

--- trunk/zb_system/function/c_system_base.php
+++ trunk/zb_system/function/c_system_base.php
@@ -8,13 +8,16 @@
 error_reporting(0);
 error_reporting(E_ALL);
 
-$blogpath = realpath(dirname(__FILE__).'/../../').'/';
+$blogpath = str_replace('\\','/',realpath(dirname(__FILE__).'/../../')) . '/';
 
 require_once $blogpath.'/zb_system/function/c_system_common.php';
 require_once $blogpath.'/zb_system/function/c_system_plugin.php';
 
 $c_option = include($blogpath.'zb_users/c_option.php');	
 $c_lang = include($blogpath.'zb_users/language/'.$c_option['ZC_BLOG_LANGUAGEPACK'].'.php');
+
+$cookiespath = null;
+$bloghost = GetCurrentHost($cookiespath);
 
 /*include plugin*/
 
--- trunk/zb_system/function/c_system_common.php
+++ trunk/zb_system/function/c_system_common.php
@@ -7,9 +7,31 @@
  */
 
 
-function getguid(){
+function GetGuid(){
 	$s=trim(com_create_guid(),'{..}');
 	return $s;
+}
+
+function GetCurrentHost(&$cookiespath){
+	if (array_key_exists('HTTPS',$_SERVER)) {
+		if ($_SERVER['HTTPS']=='off') {
+			$host='http://';
+		} else {
+			$host='https://';
+		}
+	} else {
+		$host='http://';
+	}
+	
+
+	$host.=$_SERVER['HTTP_HOST'].'/';
+
+	$a=$GLOBALS['blogpath'];
+	$b=str_replace('\\','/',$_SERVER['DOCUMENT_ROOT']).'/';
+	$c=str_replace($b,'',$a);
+	$cookiespath=$c==''?'/':'/'.$c;
+
+	return $host . $c;	
 }
 
 
