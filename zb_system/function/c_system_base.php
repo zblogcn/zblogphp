a0379ab26964a06ad30d063beab89f7bf3502332
rainbowsoft <rainbowsoft@gmail.com>
2013-07-07 11:14:10 +0000

    做安装;

--- zb_system/function/c_system_base.php
+++ zb_system/function/c_system_base.php
@@ -5,13 +5,16 @@
  * @copyright (C) RainbowSoft Studio
  * @version 2.0 2013-06-14
  */
-$blogpath = realpath(rtrim(dirname(__FILE__),'\/\\').'/../../');
+error_reporting(0);
+error_reporting(E_ALL);
+
+$blogpath = realpath(dirname(__FILE__).'/../../').'/';
 
 require_once $blogpath.'/zb_system/function/c_system_common.php';
 require_once $blogpath.'/zb_system/function/c_system_plugin.php';
 
-$c_option = include($blogpath.'/zb_users/c_option.php');	
-$c_lang = include($blogpath.'/zb_users/language/'.$c_option['ZC_BLOG_LANGUAGEPACK'].'.php');
+$c_option = include($blogpath.'zb_users/c_option.php');	
+$c_lang = include($blogpath.'zb_users/language/'.$c_option['ZC_BLOG_LANGUAGEPACK'].'.php');
 
 /*include plugin*/
 
@@ -20,11 +23,13 @@ $zbp=new zblogphp;
 class zblogphp{
 	// 当前应用的配置
 	public $option = array();
-	public $lang = array();	
+	public $lang = array();
+	public $path = null;
 	
 	function __construct() {
 		$this->option = $GLOBALS["c_option"];
 		$this->lang = $GLOBALS["c_lang"];
+		$this->path = $GLOBALS["blogpath"];
 		//define();
 	}
 	
