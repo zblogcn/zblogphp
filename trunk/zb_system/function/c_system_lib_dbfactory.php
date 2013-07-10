de9bf804b7706ebd2d7a7f4e8e9d18b98abb27a6
rainbowsoft <rainbowsoft@40c8d13a-81fc-95dd-32b5-41977a75f8b4>
2013-07-10 03:44:14 +0000

    加类;改table sql;

--- trunk/zb_system/function/c_system_lib_dbfactory.php
+++ trunk/zb_system/function/c_system_lib_dbfactory.php
@@ -48,8 +48,6 @@ $TableSql_MySQL=array(
 ,
 "CREATE TABLE %pre%Comment (comm_ID integer primary key auto_increment,log_ID integer default 0,comm_AuthorID integer default 0,comm_Author varchar(20) default '',comm_Content text default '',comm_Email varchar(50) default '',comm_HomePage varchar(255) default '',comm_PostTime timestamp default CURRENT_timestamp,comm_IP varchar(20) default '',comm_Agent text default '',comm_Reply text default '',comm_LastReplyIP varchar(20) default '',comm_LastReplyTime timestamp,comm_Yea integer default 0,comm_Nay integer default 0,comm_Ratting integer default 0,comm_ParentID integer default 0,comm_IsCheck bit default 0,comm_Meta text default '') ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci"
 ,
-"CREATE TABLE %pre%TrackBack (tb_ID integer primary key auto_increment,log_ID integer default 0,tb_URL varchar(255) default '',tb_Title varchar(100) default '',tb_Blog varchar(50) default '',tb_Excerpt text default '',tb_PostTime timestamp default CURRENT_timestamp,tb_IP varchar(20) default '',tb_Agent text default '',tb_Meta text default '') ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci"
-,
 "CREATE TABLE %pre%UpLoad (ul_ID integer primary key auto_increment,ul_AuthorID integer default 0,ul_FileSize integer default 0,ul_FileName varchar(255) default '',ul_PostTime timestamp default CURRENT_timestamp,ul_Quote varchar(255) default '',ul_DownNum integer default 0,ul_Fileintegerro varchar(255) default '',ul_DirByTime bit default 0,ul_Meta text default '') ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci"
 ,
 "CREATE TABLE %pre%Counter (coun_ID integer primary key auto_increment,coun_IP varchar(20) default '',coun_Agent text default '',coun_Refer varchar(255) default '',coun_PostTime timestamp default CURRENT_timestamp,coun_Content text default '',coun_UserID integer default 0,coun_PostData text default '',coun_URL text default '',coun_AllRequestHeader text default '',coun_LogName text default '') ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci"
@@ -60,7 +58,7 @@ $TableSql_MySQL=array(
 ,
 "CREATE TABLE %pre%Config (conf_Name varchar(255) not null default '',conf_Value text default '') ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci"
 ,
-"CREATE TABLE %pre%Function (fn_ID integer primary key auto_increment,fn_Name varchar(50) default '',fn_FileName varchar(50) default '',fn_Order integer default 0,fn_Content text default '',fn_IsHidden bit default 0,fn_SidebarID integer default 0,fn_HtmlID varchar(50) default '',fn_Ftype varchar(5) default '',fn_MaxLi integer default 0,fn_Source varchar(50) default '',fn_ViewType varchar(50) default '',fn_IsHideTitle bit default 0,fn_Meta text default '') ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci"
+"CREATE TABLE %pre%Module (fn_ID integer primary key auto_increment,fn_Name varchar(50) default '',fn_FileName varchar(50) default '',fn_Order integer default 0,fn_Content text default '',fn_IsHidden bit default 0,fn_SidebarID integer default 0,fn_HtmlID varchar(50) default '',fn_Ftype varchar(5) default '',fn_MaxLi integer default 0,fn_Source varchar(50) default '',fn_ViewType varchar(50) default '',fn_IsHideTitle bit default 0,fn_Meta text default '') ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci"
 ,
 "CREATE INDEX %pre%log_PostTime on %pre%Article (log_PostTime)"
 ,
@@ -79,8 +77,6 @@ $TableSql_SQLite3=array(
 ,
 "CREATE TABLE %pre%Comment (comm_ID integer primary key autoincrement,log_ID integer default 0,comm_AuthorID integer default 0,comm_Author varchar(20) default '',comm_Content text default '',comm_Email varchar(50) default '',comm_HomePage varchar(255) default '',comm_PostTime timestamp default (datetime('now', 'localtime')),comm_IP varchar(20) default '',comm_Agent text default '',comm_Reply text default '',comm_LastReplyIP varchar(20) default '',comm_LastReplyTime timestamp default (datetime('now', 'localtime')),comm_Yea integer default 0,comm_Nay integer default 0,comm_Ratting integer default 0,comm_ParentID integer default 0,comm_IsCheck bit default 0,comm_Meta text default '')"
 ,
-"CREATE TABLE %pre%TrackBack (tb_ID integer primary key autoincrement,log_ID integer default 0,tb_URL varchar(255) default '',tb_Title varchar(100) default '',tb_Blog varchar(50) default '',tb_Excerpt text default '',tb_PostTime timestamp default (datetime('now', 'localtime')),tb_IP varchar(20) default '',tb_Agent text default '',tb_Meta text default '')"
-,
 "CREATE TABLE %pre%UpLoad (ul_ID integer primary key autoincrement,ul_AuthorID integer default 0,ul_FileSize integer default 0,ul_FileName varchar(255) default '',ul_PostTime timestamp default (datetime('now', 'localtime')),ul_Quote varchar(255) default '',ul_DownNum integer default 0,ul_Fileintegerro varchar(255) default '',ul_DirByTime bit default 0,ul_Meta text default '')"
 ,
 "CREATE TABLE %pre%Counter (coun_ID integer primary key autoincrement,coun_IP varchar(20) default '',coun_Agent text default '',coun_Refer varchar(255) default '',coun_PostTime timestamp default (datetime('now', 'localtime')),coun_Content text default '',coun_UserID integer default 0,coun_PostData text default '',coun_URL text default '',coun_AllRequestHeader text default '',coun_LogName text default '')"
@@ -91,7 +87,7 @@ $TableSql_SQLite3=array(
 ,
 "CREATE TABLE %pre%Config (conf_Name varchar(255) not null default '',conf_Value text default '')"
 ,
-"CREATE TABLE %pre%Function (fn_ID integer primary key autoincrement,fn_Name varchar(50) default '',fn_FileName varchar(50) default '',fn_Order integer default 0,fn_Content text default '',fn_IsHidden bit default 0,fn_SidebarID integer default 0,fn_HtmlID varchar(50) default '',fn_Ftype varchar(5) default '',fn_MaxLi integer default 0,fn_Source varchar(50) default '',fn_ViewType varchar(50) default '',fn_IsHideTitle bit default 0,fn_Meta text default '')"
+"CREATE TABLE %pre%Module (fn_ID integer primary key autoincrement,fn_Name varchar(50) default '',fn_FileName varchar(50) default '',fn_Order integer default 0,fn_Content text default '',fn_IsHidden bit default 0,fn_SidebarID integer default 0,fn_HtmlID varchar(50) default '',fn_Ftype varchar(5) default '',fn_MaxLi integer default 0,fn_Source varchar(50) default '',fn_ViewType varchar(50) default '',fn_IsHideTitle bit default 0,fn_Meta text default '')"
 ,
 "CREATE INDEX %pre%log_PostTime on %pre%Article (log_PostTime)"
 ,
@@ -107,15 +103,13 @@ $TableSql_SQLite3=array(
 ,
 "CREATE UNIQUE INDEX %pre%comm_ID on %pre%Comment (comm_ID)"
 ,
-"CREATE UNIQUE INDEX %pre%tb_ID on %pre%TrackBack (tb_ID)"
-,
 "CREATE UNIQUE INDEX %pre%ul_ID on %pre%UpLoad (ul_ID)"
 ,
 "CREATE UNIQUE INDEX %pre%key_ID on %pre%Keyword (key_ID)"
 ,
 "CREATE UNIQUE INDEX %pre%mem_ID on %pre%Member (mem_ID)"
 ,
-"CREATE UNIQUE INDEX %pre%fn_ID on %pre%Function (fn_ID)"
+"CREATE UNIQUE INDEX %pre%fn_ID on %pre%Module (fn_ID)"
 );
 
 
@@ -128,8 +122,6 @@ $TableSql_SQLite=array(
 ,
 "CREATE TABLE %pre%Comment (comm_ID integer primary key,log_ID integer default 0,comm_AuthorID integer default 0,comm_Author varchar(20) default '',comm_Content text default '',comm_Email varchar(50) default '',comm_HomePage varchar(255) default '',comm_PostTime timestamp default current_timestamp,comm_IP varchar(20) default '',comm_Agent text default '',comm_Reply text default '',comm_LastReplyIP varchar(20) default '',comm_LastReplyTime timestamp default current_timestamp,comm_Yea integer default 0,comm_Nay integer default 0,comm_Ratting integer default 0,comm_ParentID integer default 0,comm_IsCheck bit default 0,comm_Meta text default '')"
 ,
-"CREATE TABLE %pre%TrackBack (tb_ID integer primary key,log_ID integer default 0,tb_URL varchar(255) default '',tb_Title varchar(100) default '',tb_Blog varchar(50) default '',tb_Excerpt text default '',tb_PostTime timestamp default current_timestamp,tb_IP varchar(20) default '',tb_Agent text default '',tb_Meta text default '')"
-,
 "CREATE TABLE %pre%UpLoad (ul_ID integer primary key,ul_AuthorID integer default 0,ul_FileSize integer default 0,ul_FileName varchar(255) default '',ul_PostTime timestamp default current_timestamp,ul_Quote varchar(255) default '',ul_DownNum integer default 0,ul_Fileintegerro varchar(255) default '',ul_DirByTime bit default 0,ul_Meta text default '')"
 ,
 "CREATE TABLE %pre%Counter (coun_ID integer primary key,coun_IP varchar(20) default '',coun_Agent text default '',coun_Refer varchar(255) default '',coun_PostTime timestamp default current_timestamp,coun_Content text default '',coun_UserID integer default 0,coun_PostData text default '',coun_URL text default '',coun_AllRequestHeader text default '',coun_LogName text default '')"
@@ -140,7 +132,7 @@ $TableSql_SQLite=array(
 ,
 "CREATE TABLE %pre%Config (conf_Name varchar(255) not null default '',conf_Value text default '')"
 ,
-"CREATE TABLE %pre%Function (fn_ID integer primary key,fn_Name varchar(50) default '',fn_FileName varchar(50) default '',fn_Order integer default 0,fn_Content text default '',fn_IsHidden bit default 0,fn_SidebarID integer default 0,fn_HtmlID varchar(50) default '',fn_Ftype varchar(5) default '',fn_MaxLi integer default 0,fn_Source varchar(50) default '',fn_ViewType varchar(50) default '',fn_IsHideTitle bit default 0,fn_Meta text default '')"
+"CREATE TABLE %pre%Module (fn_ID integer primary key,fn_Name varchar(50) default '',fn_FileName varchar(50) default '',fn_Order integer default 0,fn_Content text default '',fn_IsHidden bit default 0,fn_SidebarID integer default 0,fn_HtmlID varchar(50) default '',fn_Ftype varchar(5) default '',fn_MaxLi integer default 0,fn_Source varchar(50) default '',fn_ViewType varchar(50) default '',fn_IsHideTitle bit default 0,fn_Meta text default '')"
 ,
 "CREATE INDEX _log_PostTime on %pre%Article (log_PostTime)"
 ,
@@ -156,15 +148,13 @@ $TableSql_SQLite=array(
 ,
 "CREATE UNIQUE INDEX %pre%comm_ID on %pre%Comment (comm_ID)"
 ,
-"CREATE UNIQUE INDEX %pre%tb_ID on %pre%TrackBack (tb_ID)"
-,
 "CREATE UNIQUE INDEX %pre%ul_ID on %pre%UpLoad (ul_ID)"
 ,
 "CREATE UNIQUE INDEX %pre%key_ID on %pre%Keyword (key_ID)"
 ,
 "CREATE UNIQUE INDEX %pre%mem_ID on %pre%Member (mem_ID)"
 ,
-"CREATE UNIQUE INDEX %pre%fn_ID on %pre%Function (fn_ID)"
+"CREATE UNIQUE INDEX %pre%fn_ID on %pre%Module (fn_ID)"
 );
 
 
---
+++ trunk/zb_system/function/c_system_lib_member.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Z-Blog with PHP
+ * @author 
+ * @copyright (C) RainbowSoft Studio
+ * @version 2.0 2013-06-14
+ */
+
+
+
+
+?>
