b710d70922dec15bbc23ae1762d3c4cbc29642fa
rainbowsoft <rainbowsoft@40c8d13a-81fc-95dd-32b5-41977a75f8b4>
2013-07-16 16:52:48 +0000

    {^o~o^}

---
+++ trunk/zb_system/admin/index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Z-Blog with PHP
+ * @author 
+ * @copyright (C) RainbowSoft Studio
+ * @version 2.0 2013-07-05
+ */
+
+require_once '../function/c_system_base.php';
+require_once '../function/c_system_admin.php';
+
+$zbp->Initialize();
+
+
+$zbp->Terminate();
+
+echo RunTime();
+?>
