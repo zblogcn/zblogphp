00e4fbb9819d8b208ba9f56b01bfcff24315e882
involvements77@gmail.com <involvements77@gmail.com@40c8d13a-81fc-95dd-32b5-41977a75f8b4>
2013-07-18 17:40:36 +0000

    修debug

--- trunk/zb_system/function/c_system_debug.php
+++ trunk/zb_system/function/c_system_debug.php
@@ -72,7 +72,7 @@ class ZBlogException
 
 
 	static public function Log($s){
-		$f=$GLOBALS['zbp']->path . 'log' . $GLOBALS['zbp']->guid . '.txt';
+		$f=$GLOBALS['zbp']->path . 'zu_user/cache/log' . date("YmdHis"). '.txt';
 		$handle = fopen($f, 'a+');
 		fwrite($handle,date('c') . "~" . current(explode(" ", microtime()))  . ":" . $s . "\r");
 		fclose($handle);
