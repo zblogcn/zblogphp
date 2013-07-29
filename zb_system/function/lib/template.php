db759f0aabf2cc6b59edbaaea9b3d8ff5599b039
zsxsoft0@gmail.com <zsxsoft@outlook.com>
2013-07-29 07:01:12 +0000

    if功能

--- zb_system/function/lib/template.php
+++ zb_system/function/lib/template.php
@@ -43,26 +43,97 @@ class Template{
 	{
 
 
-		//替换<?php，不允许出现。
-		$content = preg_replace("/\<\?php[\d\D]+?\?\>/si", '', $content);
-		#替换template和include
-		$content = preg_replace('/\{template:([^\}]+)\}/', '{php} include $this->GetTemplate(\'$1\'); {/php}', $content);
-		//$content = preg_replace('/\{include:([^\}]+)\}/', '{php} $this->IncludeCompiled(\'$1\'); {/php}', $content);
 
-		#zblog asp 特别魔法
-		$content = preg_replace('#\{\#([^\}]+)\#\}#', '<?php echo $option[\'\\1\']; ?>', $content);
+		$this->replacePHP($content);
 
-		$content = preg_replace('/\{php\}([\D\d]+?)\{\/php\}/', '<?php $1 ?>', $content);
+		$this->parse_template($content);
+		$this->parse_option($content);
+		$this->parse_vars($content);
+		$this->parse_if($content);
+
+		$this->parsePHP($content);
+		
+
+
+		
 
 		#正则替换{$变量}
 		/*$content = preg_replace('#\{\$([^\}]+)\}#', '<?php echo $this->\\1; ?>', $content);*/
 
-		$content = preg_replace_callback('#\{\$([^\}]+)\}#',create_function('$matches','return \'<?php echo $\' . str_replace(\'.\',\'->\',$matches[1]) . \';?>\';'), $content);
+		
         
 		return $content;
 	}
 
+	private function replacePHP(&$content)
+	{
+		//替换<?php，不允许出现。
+		$content = preg_replace("/\<\?php[\d\D]+?\?\>/si", '', $content);
+	}
 
+	private function parse_template(&$content)
+	{
+		$content = preg_replace('/\{template:([^\}]+)\}/', '{php} include $this->GetTemplate(\'$1\'); {/php}', $content);
+		//$content = preg_replace('/\{include:([^\}]+)\}/', '{php} $this->IncludeCompiled(\'$1\'); {/php}', $content);
+	}
+
+	private function parse_option(&$content)
+	{
+		#zblog asp 特别魔法
+		$content = preg_replace('#\{\#([^\}]+)\#\}#', '<?php echo $option[\'\\1\']; ?>', $content);
+	}
+
+	private function parsePHP(&$content)
+	{
+		$content = preg_replace('/\{php\}([\D\d]+?)\{\/php\}/', '<?php $1 ?>', $content);
+	}
+
+	private function parse_vars(&$content)
+	{
+		$content = preg_replace_callback('#\{\$([^\}]+)\}#',array($this,'parse_vars_replace_dot'), $content);
+	}
+
+	private function parse_if(&$content)
+	{
+		while(preg_match('/\{if [^\n\}]+\}.*?\{\/if\}/s', $content))
+			$content = preg_replace_callback(
+				'/\{if ([^\n\}]+)\}(.*?)\{\/if\}/s',
+				array($this,'parse_if_sub'),
+				$content
+			);
+	}
+
+	private function parse_if_sub($matches)
+	{
+
+		$content = preg_replace_callback(
+			'/\{elseif ([^\n\}]+)\}/',
+			array($this, 'parse_elseif'),
+			$matches[2]
+		);
+
+		$ifexp = str_replace($matches[1],$this->replace_dot($matches[1]),$matches[1]);
+
+		$content = str_replace('{else}', '{php}}else{ {/php}', $content);
+		return "<?php if ($ifexp) { ?>$content<?php } ?>";
+
+	}
+
+	private function parse_elseif($matches)
+	{
+		$ifexp = str_replace($matches[1],$this->replace_dot($matches[1]),$matches[1]);
+		return "{php}}elseif($ifexp) { {/php}";
+	}
+
+	private function parse_vars_replace_dot($matches)
+	{
+		return '{php} echo $' . $this->replace_dot($matches[1]) . '; {/php}';
+	}
+
+	private function replace_dot($content)
+	{
+		return str_replace('.','->',$content);
+	}
 
 	public function GetTemplate($name)
 	{
