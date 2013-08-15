e8a2092d6a699347e72dd63f44a6af1a06bdb7ef
involvements77@gmail.com <involvements77@gmail.com@40c8d13a-81fc-95dd-32b5-41977a75f8b4>
2013-08-15 09:07:39 +0000

    回家修ke

--- trunk/zb_users/plugin/KindEditor/kindeditor/php/demo.php
+++ trunk/zb_users/plugin/KindEditor/kindeditor/php/demo.php
@@ -1,26 +1,18 @@
-<%@ CODEPAGE=65001 %>
-<%
-Option Explicit
-Response.CodePage=65001
-Response.Charset="UTF-8"
-
-Dim htmlData
-
-htmlData = Request.Form("content1")
-
-Function htmlspecialchars(str)
-	str = Replace(str, "&", "&amp;")
-	str = Replace(str, "<", "&lt;")
-	str = Replace(str, ">", "&gt;")
-	str = Replace(str, """", "&quot;")
-	htmlspecialchars = str
-End Function
-%>
+<?php
+	$htmlData = '';
+	if (!empty($_POST['content1'])) {
+		if (get_magic_quotes_gpc()) {
+			$htmlData = stripslashes($_POST['content1']);
+		} else {
+			$htmlData = $_POST['content1'];
+		}
+	}
+?>
 <!doctype html>
 <html>
 <head>
 	<meta charset="utf-8" />
-	<title>KindEditor ASP</title>
+	<title>KindEditor PHP</title>
 	<link rel="stylesheet" href="../themes/default/default.css" />
 	<link rel="stylesheet" href="../plugins/code/prettify.css" />
 	<script charset="utf-8" src="../kindeditor.js"></script>
@@ -30,8 +22,8 @@ End Function
 		KindEditor.ready(function(K) {
 			var editor1 = K.create('textarea[name="content1"]', {
 				cssPath : '../plugins/code/prettify.css',
-				uploadJson : '../asp/upload_json.asp',
-				fileManagerJson : '../asp/file_manager_json.asp',
+				uploadJson : '../php/upload_json.php',
+				fileManagerJson : '../php/file_manager_json.php',
 				allowFileManager : true,
 				afterCreate : function() {
 					var self = this;
@@ -50,11 +42,12 @@ End Function
 	</script>
 </head>
 <body>
-	<%=htmlData%>
-	<form name="example" method="post" action="demo.asp">
-		<textarea name="content1" style="width:700px;height:200px;visibility:hidden;"><%=htmlspecialchars(htmlData)%></textarea>
+	<?php echo $htmlData; ?>
+	<form name="example" method="post" action="demo.php">
+		<textarea name="content1" style="width:700px;height:200px;visibility:hidden;"><?php echo htmlspecialchars($htmlData); ?></textarea>
 		<br />
 		<input type="submit" name="button" value="提交内容" /> (提交快捷键: Ctrl + Enter)
 	</form>
 </body>
 </html>
+
