582a11c48eb8a2bd86dc66144a5b6dff239bb7a1
involvements77@gmail.com <involvements77@gmail.com>
2013-08-28 16:43:23 +0000

    文件管理系统,吐槽者MJJ

---
+++ zb_users/plugin/FileSystem/include.php
@@ -0,0 +1,25 @@
+<?php
+
+
+#注册插件
+RegisterPlugin("FileSystem","ActivePlugin_FileSystem");
+
+
+function ActivePlugin_FileSystem() {
+Add_Filter_Plugin('Filter_Plugin_Admin_LeftMenu','FileSystem_AddMenu');
+
+}
+
+function InstallPlugin_FileSystem(){
+
+}
+
+function UninstallPlugin_FileSystem(){
+
+}
+function FileSystem_AddMenu(&$m){
+	global $zbp;
+	$m[]=MakeLeftMenu("root","文件管理",$zbp->host . "zb_users/plugin/FileSystem/main.php","nav_FileSystem","aFileSystem",$zbp->host . "zb_system/image/common/file_1.png");	
+}
+
+?>
