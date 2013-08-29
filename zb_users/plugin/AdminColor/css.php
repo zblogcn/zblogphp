f0bca0144fe81ab4b01463ae8a84958df6e61f81
rainbowsoft <rainbowsoft@users.noreply.github.com>
2013-08-29 10:58:52 +0000

    果断提交admincolor4php版!!!

---
+++ zb_users/plugin/AdminColor/css.php
@@ -0,0 +1,84 @@
+<?php
+require '../../../zb_system/function/c_system_base.php';
+
+require '../../../zb_system/function/c_system_admin.php';
+
+header('Content-Type: text/css; Charset=utf8');  
+
+$id=10;
+$c='';
+
+$c .="header{background-color:#3a6ea5;}". "\r\n";
+$c .="input.button,input[type='submit'],input[type='button'] {background-color:#3a6ea5;}". "\r\n";
+$c .="div.theme-now .betterTip img{box-shadow: 0 0 10px #3a6ea5;}". "\r\n";
+
+$c .="#divMain a,#divMain2 a{color:#1d4c7d;}". "\r\n";
+
+$c .=".menu ul li a:hover {background-color: #b0cdee;}". "\r\n";
+$c .="#leftmenu a:hover {background-color: #b0cdee!important;}". "\r\n";
+$c .="div.theme-now{background-color:#b0cdee;}". "\r\n";
+$c .="div.theme-other .betterTip img:hover{border-color:#b0cdee;}". "\r\n";
+$c .=".SubMenu a:hover {background-color:#b0cdee;}". "\r\n";
+$c .=".siderbar-header:hover {background-color:#b0cdee;}". "\r\n";
+
+$c .="#leftmenu .on a,#leftmenu #on a:hover {background-color:#3399cc!important;}". "\r\n";
+$c .="input.button,input[type=\"submit\"],input[type=\"button\"] { border-color:#3399cc;}". "\r\n";
+$c .="input.button:hover {background-color: #3399cc;}". "\r\n";
+$c .="div.theme-other .betterTip img:hover{box-shadow: 0 0 10px #3399cc;}". "\r\n";
+$c .=".SubMenu{border-bottom-color:#3399cc;}". "\r\n";
+$c .=".SubMenu span.m-now{background-color:#3399cc;}". "\r\n";
+$c .="div #BT_title {background-color: #3399cc;border-color:#3399cc;}". "\r\n";
+
+$c .="a:hover { color:#d60000;}". "\r\n";
+$c .="#divMain a:hover,#divMain2  a:hover{color:#d60000;}". "\r\n";
+
+
+//appcenter
+$c .=".tabs { border-bottom-color:#3a6ea5!important;}". "\r\n";
+$c .=".tabs li a.selected {background-color:#3a6ea5!important;}". "\r\n";
+$c .="div.heart-vote {background-color:#3a6ea5!important;}". "\r\n";
+$c .="div.heart-vote ul {border-color:#3a6ea5!important;}". "\r\n";
+$c .=".install {background-color:#3a6ea5!important;}". "\r\n";
+$c .=".install:hover{background-color: #3399cc!important;}". "\r\n";
+$c .="input.button{background-color:#3a6ea5!important;border-color:#3399cc!important;}". "\r\n";
+$c .="input.button:hover{background-color:#3399cc!important;}". "\r\n";
+$c .=".themes_body ul li img:hover,.plugin_body ul li img:hover,.main_plugin ul li img:hover,.main_theme ul li img:hover{box-shadow: 0 0 10px #3399cc!important;}". "\r\n";
+$c .=".left_nav h2,.text h2 {color: #3a6ea5!important;}". "\r\n";
+$c .=".pagebar span{ background:#3399cc!important; border-color:#3399cc!important;color:#fff;}". "\r\n";
+$c .=".pagebar span.now-page,.pagebar span:hover{ background:#eee!important;border-color:#eee!important; color:#3399cc!important;}". "\r\n";
+
+
+//zbdk
+$c .="#divMain .DIVBlogConfignav ul li a:hover {background-color: #3399cc!important;}". "\r\n";
+$c .="#divMain .DIVBlogConfignav ul li a.clicked{background-color: #b0cdee!important;}". "\r\n";
+$c .=".DIVBlogConfignav {background-color: #ededed!important;}". "\r\n";
+$c .="#divMain .DIVBlogConfigtop {background-color: #3399cc!important;}". "\r\n";
+$c .="#divMain .DIVBlogConfig {background-color: #ededed!important;}". "\r\n";
+
+
+//$c .=Response_Plugin_AdminColor_Css
+
+
+$c1="#1d4c7d";
+$c2="#3a6ea5";
+$c3="#b0cdee";
+$c4="#3399cc";
+$c5="#d60000";
+
+$c=str_replace($c1,$BlodColor[$id],$c);
+$c=str_replace($c2,$NormalColor[$id],$c);
+$c=str_replace($c3,$LightColor[$id],$c);
+$c=str_replace($c4,$HighColor[$id],$c);
+$c=str_replace($c5,$AntiColor[$id],$c);
+
+
+$c .="\r\n" . "/*AdminColor*/" . "\r\n" . "#admin_color{float:right;line-height: 2.5em;font-size: 0.5em;letter-spacing: -0.1em;}";
+
+if($id==10){
+$c.='header {background:url(header.jpg) repeat-x 0 0;}';
+$c.='body{background:url(body.jpg) no-repeat 0 0;background-attachment:fixed;}';
+$c.='#topmenu{opacity:0.8;}*/';
+}
+
+echo $c;
+?>
---
+++ zb_users/plugin/AdminColor/include.php
@@ -0,0 +1,113 @@
+<?php
+#注册插件
+RegisterPlugin("AdminColor","ActivePlugin_AdminColor");
+
+
+
+function ActivePlugin_AdminColor() {
+
+	//Add_Filter_Plugin('Filter_Plugin_Admin_LeftMenu','AppCentre_AddMenu');
+	//Call Add_Response_Plugin("Response_Plugin_Admin_SiteInfo","<script type='text/javascript'>$('.divHeader').append(""<div id='admin_color'>"&s&"</div>"");</script>")
+
+	Add_Filter_Plugin('Filter_Plugin_Admin_Header','AdminColor_Css');
+
+}
+
+function AdminColor_Css(){
+	global $zbp;
+	echo '<link rel="stylesheet" type="text/css" href="'. $zbp->host .'zb_users/plugin/AdminColor/css.php"/>';
+}
+
+function AdminColor_ColorButton(){
+	global $zbp;
+
+	for ($i=1; $i < 9; $i++) { 
+		$s.="&nbsp;&nbsp;<a href='". $zbp->host ."zb_users/plugin/AdminColor/css.php?color=".$i."'>
+		<span style='height:16px;width:16px;background:".$NormalColor[$i]."'>
+		<img src='". $zbp->host ."zb_system/image/admin/none.gif' width='16' height='16' alt='' /></span></a>&nbsp;&nbsp;";
+	}
+
+	$s.="&nbsp;&nbsp;<a href='". $zbp->host ."zb_users/plugin/AdminColor/css.php?color=art' title='文艺范'>
+	<span style='height:16px;width:16px;background:#eee;'>
+	<img src='". $zbp->host ."zb_system/image/admin/none.gif' width='16' alt=''/></span></a>&nbsp;&nbsp;";
+
+	echo $s;
+}
+
+
+
+$BlodColor[0]='#1d4c7d';
+$NormalColor[0]='#3a6ea5';
+$LightColor[0]='#b0cdee';
+$HighColor[0]='#3399cc';
+$AntiColor[0]='#d60000';
+
+
+$BlodColor[1]='#143c1f';
+$NormalColor[1]='#5b992e';
+$LightColor[1]='#bee3a3';
+$HighColor[1]='#6ac726';
+$AntiColor[1]='#d60000';
+
+
+$BlodColor[2]='#06282b';
+$NormalColor[2]='#2db1bd';
+$LightColor[2]='#87e6ef';
+$HighColor[2]='#119ba7';
+$AntiColor[2]='#d60000';
+
+
+$BlodColor[3]='#3e1165';
+$NormalColor[3]='#5c2c84';
+$LightColor[3]='#a777d0';
+$HighColor[3]='#8627d7';
+$AntiColor[3]='#08a200';
+
+
+$BlodColor[4]='#3f280d';
+$NormalColor[4]='#b26e1e';
+$LightColor[4]='#e3b987';
+$HighColor[4]='#d88625';
+$AntiColor[4]='#d60000';
+
+
+$BlodColor[5]='#0a4f3e';
+$NormalColor[5]='#267662';
+$LightColor[5]='#68cdb4';
+$HighColor[5]='#25bb96';
+$AntiColor[5]='#d60000';
+
+
+$BlodColor[6]='#3a0b19';
+$NormalColor[6]='#7c243f';
+$LightColor[6]='#d57c98';
+$HighColor[6]='#d31b54';
+$AntiColor[6]='#2039b7';
+
+
+$BlodColor[7]='#2d2606';
+$NormalColor[7]='#d9b611';
+$LightColor[7]='#ebd87d';
+$HighColor[7]='#c4a927';
+$AntiColor[7]='#d60000';
+
+$BlodColor[8]='#3f0100';
+$NormalColor[8]='#e5535f';
+$LightColor[8]='#ffb3a7';
+$HighColor[8]='#da4b4a';
+$AntiColor[8]='#ff000c';
+
+
+$BlodColor[9]='#333333';
+$NormalColor[9]='#555555';
+$LightColor[9]='#ababab';
+$HighColor[9]='#8b8b8b';
+$AntiColor[9]='#d60000';
+
+$BlodColor[10]='#4a380b';
+$NormalColor[10]='#896a1c';
+$LightColor[10]='#caa855';
+$HighColor[10]='#b08313';
+$AntiColor[10]='#2227e0';
+
+?>
