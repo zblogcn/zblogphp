3296924c4c2ae3e9ce52ee804f4ba9c289a6f914
involvements77@gmail.com <involvements77@gmail.com@40c8d13a-81fc-95dd-32b5-41977a75f8b4>
2013-08-30 16:28:56 +0000

    FS->del_file, down_file

--- trunk/zb_users/plugin/FileSystem/action.php
+++ trunk/zb_users/plugin/FileSystem/action.php
@@ -16,22 +16,65 @@ $current_path = urldecode($_POST['current_path']);
 $selected_file_list = $_POST['selected_file_list'];
 
 switch ($cmd_arg) {
-    case 1:
+    case 1://新建文件
         create_dir();
         break;
-	case 2:
+	case 2://新建文件夹
         create_file();
         break;
-	case 888:
+	case 3://删除文件
+		del_file(iconv('UTF-8','GB2312',$current_path.$selected_file_list));
+		break;
+	case 4://下载文件
+		download();
+		break;
+	case 888://phpinfo
 		phpinfo();
 		break;
 }
 
+function download(){
+	global $cmd_data, $current_path;
+	$file = iconv('UTF-8','GB2312',$current_path.$cmd_data);
+    if(file_exists($file)){
+		$size = filesize($file);
+		header("Content-Type: application/save");
+		header("Content-Length: $size");
+		header("Content-Disposition: attachment; filename=\"$cmd_data\"");
+		header("Content-Transfer-Encoding: binary");
+		if ($fh = fopen("$file", "rb")){
+			fpassthru($fh);
+			fclose($fh);
+		}
+    }
+}
+
+function del_file($arg){
+	delfile($arg);
+	$error_msg = 0;
+	if (file_exists($arg)) $error_msg = '文件删除失败。';
+	back_url($error_msg);
+}
+
+function delfile($arg){
+	if (file_exists($arg)) {
+        @chmod($arg,0777);
+        if (is_dir($arg)) {
+            $handle = opendir($arg);
+            while(false !== ($aux = readdir($handle))) {
+                if ($aux != "." && $aux != "..") delfile($arg."/".$aux);
+            }
+            @closedir($handle);
+            rmdir($arg);
+        } else unlink($arg);
+    }
+}
+
 
 function create_file(){
-	global $cmd_data, $current_path, $selected_file_list;
+	global $cmd_data, $current_path;
+	$cmd_data = iconv('UTF-8','GB2312',$current_path.$cmd_data);
 	if (strlen($cmd_data)){
-		$cmd_data = $current_path.$cmd_data;
 		if (!file_exists($cmd_data)){
 			if ($fh = @fopen($cmd_data, "w")){
 				@fclose($fh);
@@ -44,9 +87,9 @@ function create_file(){
 }
 
 function create_dir(){
-	global $cmd_data, $current_path, $selected_file_list;
+	global $cmd_data, $current_path;
+	$cmd_data = iconv('UTF-8','GB2312',$current_path.$cmd_data);
 	if (strlen($cmd_data)){
-		$cmd_data = format_path($current_path.$cmd_data);
 		if (!file_exists($cmd_data)){
 			mkdir($cmd_data,0777);
 			chmod($cmd_data,0777);
