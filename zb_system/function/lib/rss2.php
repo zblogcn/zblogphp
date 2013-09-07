cac15ac2b86c56f0155e8eeef05cda30761688a1
rainbowsoft <rainbowsoft@users.noreply.github.com>
2013-09-07 08:49:53 +0000

    修正了RSS的问题；对UE的HR加了more；

--- zb_system/function/lib/rss2.php
+++ zb_system/function/lib/rss2.php
@@ -16,21 +16,23 @@ class Rss2 extends DOMDocument {
 
 		$root = $this->appendChild($this->createElement('rss'));
 		$root->setAttribute('version','2.0');
+		
+		$root->setAttribute('xmlns:dc','http://purl.org/dc/elements/1.1/');		
 
 		$channel = $root->appendChild($this->createElement('channel'));
 
-		$channel->appendChild($this->createElement('title',$title));
+		$channel->appendChild($this->createElement('title',str_replace('&nbsp','&#160',$title)));
 		$channel->appendChild($this->createElement('link',$link));
-		$channel->appendChild($this->createElement('description',$description));
+		$channel->appendChild($this->createElement('description',str_replace('&nbsp','&#160',$description)));
 
 		$this->channel = $channel;
 	}
 
 	public function addItem($title,$link,$description,$date){
 		$item = $this->createElement('item');
-		$item->appendChild($this->createElement('title',$title));
+		$item->appendChild($this->createElement('title',str_replace('&nbsp','&#160',$title)));
 		$item->appendChild($this->createElement('link',$link));
-		$item->appendChild($this->createElement('description',"<![CDATA[".$description."]]>"));
+		$item->appendChild($this->createElement('description',str_replace('&nbsp','&#160',$description)));
 		$item->appendChild($this->createElement('pubDate',date(DATE_RSS,$date)));
 
 		$this->channel->appendChild($item);
