2b512903e5283d3a8edc7bb744f93addeb0f410b
rainbowsoft <rainbowsoft@users.noreply.github.com>
2013-11-20 06:26:10 +0000

    修正了一个后台URL的问题；修正了一个设置参数在两处未使用的问题；

--- zb_system/admin/edit.php
+++ zb_system/admin/edit.php
@@ -348,7 +348,7 @@ function AutoIntro() {
 	  if(s.indexOf("<hr class=\"more\"/>")>-1){
       editor_api.editor.intro.put(s.split("<hr class=\"more\"/>")[0]);
   	}else{
-	  	editor_api.editor.intro.put(s.substring(0,250));
+	  	editor_api.editor.intro.put(s.substring(0,<?php echo $zbp->option['ZC_ARTICLE_EXCERPT_MAX'];?>));
   	}
   }
 	$("#divIntro").show();
