f6e69ed5521c7144926b81b5dcd8b4874b5bf158
involvements77@gmail.com <involvements77@gmail.com@40c8d13a-81fc-95dd-32b5-41977a75f8b4>
2013-12-22 14:11:12 +0000

    删除无用插件，asp2php插件

---
+++ trunk/zb_users/plugin/CloudStorage/include.php
@@ -0,0 +1,10 @@
+<?php
+
+#注册插件
+RegisterPlugin("CloudStorage","ActivePlugin_CloudStorage");
+
+function ActivePlugin_CloudStorage() {}
+function InstallPlugin_CloudStorage() {}
+function UninstallPlugin_CloudStorage() {}
+
+?>
