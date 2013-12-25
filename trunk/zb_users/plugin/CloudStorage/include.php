1d1bf20cd3cbec4a37b82be4a1ea058cc948b883
involvements77@gmail.com <involvements77@gmail.com@40c8d13a-81fc-95dd-32b5-41977a75f8b4>
2013-12-25 13:13:48 +0000

    CloudStorage完成

--- trunk/zb_users/plugin/CloudStorage/include.php
+++ trunk/zb_users/plugin/CloudStorage/include.php
@@ -25,13 +25,24 @@ function CloudStorage($tmp, &$upload){
 			define('OSS_ACCESS_ID', $zbp->Config('CloudStorage')->CS_Ali_KeyID);	//ACCESS_ID
 			define('OSS_ACCESS_KEY', $zbp->Config('CloudStorage')->CS_Ali_KeySecret);//ACCESS_KEY
 			require_once dirname(__FILE__).'/api/oss/sdk.class.php';
-			$oss_sdk_service = new ALIOSS();
-			$oss_sdk_service->set_debug_mode(FALSE);
-			//$oss_sdk_service->set_enable_domain_style(TRUE);//二级域名
-			$response = $oss_sdk_service->upload_file_by_file($bucket,$object,$file_path);//将本地文件上传到云
+			$os_service = new ALIOSS();
+			$os_service->set_debug_mode(FALSE);
+			//$os_service->set_enable_domain_style(TRUE);//二级域名
+			$response = $os_service->upload_file_by_file($bucket,$object,$file_path);//将本地文件上传到云
 			$upload->Metas->CS_URL = $response->header['_info']['url'];
 			break;
 		case '2':
+			require_once dirname(__FILE__).'/api/qiniu/io.php';
+			require_once dirname(__FILE__).'/api/qiniu/rs.php';
+			$accessKey = $zbp->Config('CloudStorage')->CS_QNiu_KeyID;
+			$secretKey = $zbp->Config('CloudStorage')->CS_QNiu_KeySecret;
+			Qiniu_SetKeys($accessKey, $secretKey);
+			$putPolicy = new Qiniu_RS_PutPolicy($bucket);
+			$upToken = $putPolicy->Token(null);
+			//$putExtra = new Qiniu_PutExtra();
+			//$putExtra->Crc32 = 1;
+			list($ret, $err) = Qiniu_PutFile($upToken, $object, $file_path, null);
+			$upload->Metas->CS_URL = 'http://'.$bucket.'.u.qiniudn.com/'.$ret['key'];
 			break;
 		case '3':
 			define ( 'BCS_AK', $zbp->Config('CloudStorage')->CS_Baidu_KeyID);//AK 公钥
@@ -39,8 +50,10 @@ function CloudStorage($tmp, &$upload){
 			require_once dirname(__FILE__).'/api/bcs/bcs.class.php';
 			$baidu_bcs = new BaiduBCS();
 			$object = "/".$object;
-			$response = $baidu_bcs->create_object ( $bucket, $object, $file_path );
-
+			$response = $baidu_bcs->create_object ( $bucket, $object, $file_path, array('acl'=>'public-read') );
+			//var_dump($response);die();
+			$upload->Metas->CS_URL = "http://bcs.duapp.com/".$bucket.$object; //$response->header['_info']['url'];
+			//未解决文件标识
 			break;
 		default:
 			break;
@@ -53,29 +66,34 @@ function CloudStorage($tmp, &$upload){
 
 function CloudStorage_Del(&$upload){
 	global $zbp;
+	$bucket = $zbp->Config('CloudStorage')->CS_Bucket;
 	switch ( $upload->Metas->CS_Tpye ) {
 		case '1':
 			define('OSS_ACCESS_ID', $zbp->Config('CloudStorage')->CS_Ali_KeyID);	//ACCESS_ID
 			define('OSS_ACCESS_KEY', $zbp->Config('CloudStorage')->CS_Ali_KeySecret);//ACCESS_KEY
 			require_once dirname(__FILE__).'/api/oss/sdk.class.php';
-			$oss_sdk_service = new ALIOSS();
-			$oss_sdk_service->set_debug_mode(FALSE);
-			$bucket = $zbp->Config('CloudStorage')->CS_Bucket;
+			$os_service = new ALIOSS();
+			$os_service->set_debug_mode(FALSE);
 			$object = str_replace("http://".$bucket.".oss.aliyuncs.com/", '', $upload->Metas->CS_URL);
-			$oss_sdk_service->delete_object($bucket,$object);
+			$os_service->delete_object($bucket,$object);
 			break;
 		case '2':
+			require_once dirname(__FILE__).'/api/qiniu/io.php';
+			require_once dirname(__FILE__).'/api/qiniu/rs.php';
+			$accessKey = $zbp->Config('CloudStorage')->CS_QNiu_KeyID;
+			$secretKey = $zbp->Config('CloudStorage')->CS_QNiu_KeySecret;
+			Qiniu_SetKeys($accessKey, $secretKey);
+			$client = new Qiniu_MacHttpClient(null);
+			$object = str_replace("http://".$bucket.".u.qiniudn.com/", '', $upload->Metas->CS_URL);
+			Qiniu_RS_Delete($client, $bucket, $object);
 			break;
 		case '3':
 			define ( 'BCS_AK', $zbp->Config('CloudStorage')->CS_Baidu_KeyID);//AK 公钥
 			define ( 'BCS_SK', $zbp->Config('CloudStorage')->CS_Baidu_KeySecret);//SK 私钥
 			require_once dirname(__FILE__).'/api/bcs/bcs.class.php';
 			$baidu_bcs = new BaiduBCS();
-
-
-			$bucket = $zbp->Config('CloudStorage')->CS_Bucket;
-			$object = str_replace("http://".$bucket.".oss.aliyuncs.com/", '', $upload->Metas->CS_URL);
-			$oss_sdk_service->delete_object($bucket,$object);
+			$object = str_replace("http://bcs.duapp.com/".$bucket, '', $upload->Metas->CS_URL);
+			$baidu_bcs->delete_object ( $bucket, $object);
 			break;
 		default:
 			break;
@@ -94,7 +112,7 @@ function InstallPlugin_CloudStorage() {
 	global $zbp;
 	if(!$zbp->Config('CloudStorage')->HasKey('Version')){
 		$zbp->Config('CloudStorage')->Version = '1.0';
-		$zbp->Config('CloudStorage')->CS_Storage = '';
+		$zbp->Config('CloudStorage')->CS_Storage = '1';
 		$zbp->Config('CloudStorage')->CS_Bucket = 'imzhou';
 		$zbp->Config('CloudStorage')->CS_Dir = '';
 		$zbp->Config('CloudStorage')->CS_Ali_KeyID = '';
@@ -107,5 +125,3 @@ function InstallPlugin_CloudStorage() {
 	}
 }
 function UninstallPlugin_CloudStorage() {}
-
-?>
