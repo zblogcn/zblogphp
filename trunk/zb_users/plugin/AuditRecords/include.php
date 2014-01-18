74c07b1d1cff38b403d822f09e15dcb3122c9829
rainbowsoft@gmail.com <rainbowsoft@gmail.com@40c8d13a-81fc-95dd-32b5-41977a75f8b4>
2014-01-18 16:05:20 +0000

    search.php页有改进，优先指定默认search模板；强化了审核插件为应用中心；

--- trunk/zb_users/plugin/AuditRecords/include.php
+++ trunk/zb_users/plugin/AuditRecords/include.php
@@ -5,12 +5,19 @@
 RegisterPlugin("AuditRecords","ActivePlugin_AuditRecords");
 
 
+$actions['audit']=2;
+
+//参数有3个
+//1 $article：被审核的文章object (传入引用)
+//2 $opeate指令 -1(申诉) 0(未通过) 1(通过)
+//3 $logs文本记录
+$Filter_Plugin_AuditRecords_Submit=array();
+
 function ActivePlugin_AuditRecords() {
 
 	Add_Filter_Plugin('Filter_Plugin_Edit_Response','AuditRecords_Edit_Response');
 	Add_Filter_Plugin('Filter_Plugin_PostArticle_Core','AuditRecords_Main');
 	Add_Filter_Plugin('Filter_Plugin_Edit_Response3','AuditRecords_Edit_Response3');
-
 	Add_Filter_Plugin('Filter_Plugin_Admin_LeftMenu','AuditRecords_AddMenu');	
 }
 
@@ -20,7 +27,7 @@ function AuditRecords_AddMenu(&$m){
 	array_unshift($m,'');
 	$n=GetValueInArrayByCurrent($zbp->db->Query('SELECT COUNT(log_ID) AS num FROM ' . $GLOBALS['table']['Post'] . ' WHERE log_Type=0 AND log_Status=2'),'num');
 	$m[0]=$m[1];
-	$m[1]=MakeLeftMenu("ArticleAll","(".$n.")审核管理",$zbp->host . "zb_users/plugin/AuditRecords/main.php","nav_AuditRecords","aAuditRecords","");
+	$m[1]=MakeLeftMenu("audit","(".$n.")审核管理",$zbp->host . "zb_users/plugin/AuditRecords/main.php","nav_AuditRecords","aAuditRecords","");
 }
 
 
@@ -67,25 +74,44 @@ function AuditRecords_Edit_Response(){
 	global $article;
 	if($article->ID==0)return ;
 
-	if($zbp->CheckRights('ArticleAll')){
-		echo '<p><b>审核者意见：</b><textarea name="AuditRecords_logs" style="width:100%;height:50px;"></textarea></p>';
+	echo '<p>';
+	if(!$zbp->CheckRights('audit')){
+		echo '<label><input name="AuditRecords_op" value="-1" type="radio" /><b>发布者意见(作者更新了请选择该项并留下相关意见)</b></label>';
 	}else{
 		if($zbp->user->ID==$article->AuthorID)
-			echo '<p><b>发布者意见：</b><textarea name="AuditRecords_logs" style="width:100%;height:40px;"></textarea></p>';
+		echo '<label><input name="AuditRecords_op" value="-1" type="radio" /><b>发布者意见</b>&nbsp;&nbsp;</label>';
+		echo '<label><input name="AuditRecords_op" value="0" type="radio" onclick="$(\'#cmbPostStatus\').get(0).selectedIndex=2; " /><b>审核者不通过并留下意见</b>&nbsp;&nbsp;</label>';
+		echo '<label><input name="AuditRecords_op" value="1" type="radio" onclick="$(\'#cmbPostStatus\').get(0).selectedIndex=0; "  /><b>审核者通过并留下意见</b>&nbsp;&nbsp;</label>';
 	}
+	echo '<textarea name="AuditRecords_logs" style="width:100%;height:60px;"></textarea></p>';
+
 }
 
 function AuditRecords_Main($article){
 	global $zbp;
+	if(isset($_POST['AuditRecords_op'])){
 	
 	$ar=new AuditRecords;
 	$ar->LogID=$article->ID;
 	$ar->AuthorID=$zbp->user->ID;
 	$ar->Logs=Trim(GetVars('AuditRecords_logs','POST'));
-	$ar->Opeate=$article->Status==ZC_POST_STATUS_AUDITING?0:1;
+	$ar->Opeate=GetVars('AuditRecords_op','POST');
+	
+	if(!$ar->Logs){
+		if($ar->Opeate==-1)$ar->Logs="我有更新了，麻烦请审核一下。";
+		if($ar->Opeate==0 )$ar->Logs="不想写审核意见，还是不通过。";
+		if($ar->Opeate==1 )$ar->Logs="通过了通过了，真的没有意见。";
+	}
+	
 	$ar->PostTime=time();
 
-	if($ar->Logs)$ar->Save();
+	$ar->Save();
+	
+	foreach ($GLOBALS['Filter_Plugin_AuditRecords_Submit'] as $fpname => &$fpsignal) {
+		$fpname($article,$ar->Opeate,$ar->Logs);
+	}
+
+	}
 
 }
 
@@ -108,11 +134,15 @@ function AuditRecords_Edit_Response3(){
 	$array=$zbp->GetList('AuditRecords',$sql);
 	foreach ($array as $key => $ar) {
 		echo '<dd style="text-align:left;padding-bottom:5px;"">';
-		if($ar->Opeate){
-			echo '<img src="'.$zbp->host.'zb_users/plugin/AuditRecords/thumb_up.png" alt="通过" width="16" />';
-		}else{
-			echo '<img src="'.$zbp->host.'zb_users/plugin/AuditRecords/thumb_down.png" alt="未通过" width="16" />';
+		
+		if($ar->Opeate==1){
+			echo '<img src="'.$zbp->host.'zb_users/plugin/AuditRecords/tick.png" alt="通过" width="16" />';
+		}elseif($ar->Opeate==0){
+			echo '<img src="'.$zbp->host.'zb_users/plugin/AuditRecords/cancel.png" alt="未通过" width="16" />';
+		}elseif($ar->Opeate==-1){
+			echo '<img src="'.$zbp->host.'zb_users/plugin/AuditRecords/error.png" alt="申诉" width="16" />';
 		}
+		
 		echo '<b>'.$zbp->GetMemberByID($ar->AuthorID)->Name .'</b>于'.date('Y-m-d',$ar->PostTime).'发布意见:';
 		echo '<br/>'.htmlspecialchars($ar->Logs);
 		echo '</dd>';
