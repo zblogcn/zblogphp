5b54619524078c338fad01b85eb38b35f2ec251d
zsxsoft0@gmail.com <zsxsoft0@gmail.com@40c8d13a-81fc-95dd-32b5-41977a75f8b4>
2014-01-30 03:55:57 +0000

    网络组件是否可调用判断；curl组件

--- trunk/zb_system/function/lib/network.php
+++ trunk/zb_system/function/lib/network.php
@@ -26,9 +26,32 @@ class NetworkFactory
 {
 
 	public $networktype = null;
-
-	public static function Create($type)
+	public $network_list = array();
+	public $curl = false;
+	public $fso = false;
+	
+	function __construct()
 	{
+		if ((bool)ini_get('allow_url_fopen'))
+		{
+			if(function_exists('file_get_contents')) $network_list[] = 'file_get_contents';
+			if(function_exists('fsockopen')) $network_list[] = 'fsockopen';
+			$this->fso = true;
+		}
+		
+		if (function_exists('curl_init'))
+		{
+			$network_list[] = 'curl';
+			$this->curl = true;
+		}
+		
+		
+	}
+	
+	public function Create($type)
+	{
+		if((!$this->fso) && ($type == 'file_get_contents' || $type == 'fsockopen')) throw new Exception('function '.$type.' not allowed');
+		if((!$this->curl) && ($type == 'curl')) throw new Exception('function '.$type.' not allowed');
 		$newtype='network'.$type;
 		$network=New $newtype();
 		return $network;
