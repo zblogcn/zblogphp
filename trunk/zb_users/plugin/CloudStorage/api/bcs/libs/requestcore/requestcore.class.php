a256f1f6d600e68b7c71d0ff17d0e4a4d0ab6b9d
involvements77@gmail.com <involvements77@gmail.com@40c8d13a-81fc-95dd-32b5-41977a75f8b4>
2014-02-19 15:25:50 +0000

    云存储

--- trunk/zb_users/plugin/CloudStorage/api/bcs/libs/requestcore/requestcore.class.php
+++ trunk/zb_users/plugin/CloudStorage/api/bcs/libs/requestcore/requestcore.class.php
@@ -492,7 +492,7 @@ class BCS_RequestCore {
 		curl_setopt ( $curl_handle, CURLOPT_FILETIME, true );
 		curl_setopt ( $curl_handle, CURLOPT_FRESH_CONNECT, false );
 		curl_setopt ( $curl_handle, CURLOPT_SSL_VERIFYPEER, false );
-		curl_setopt ( $curl_handle, CURLOPT_SSL_VERIFYHOST, true );
+		curl_setopt ( $curl_handle, CURLOPT_SSL_VERIFYHOST, 2 );//未寒
 		curl_setopt ( $curl_handle, CURLOPT_CLOSEPOLICY, CURLCLOSEPOLICY_LEAST_RECENTLY_USED );
 		curl_setopt ( $curl_handle, CURLOPT_MAXREDIRS, 5 );
 		curl_setopt ( $curl_handle, CURLOPT_HEADER, true );
