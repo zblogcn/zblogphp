8aa99a3d5d07b7a2a5f0cf08d996ceaf69d5a49c
zsxsoft0@gmail.com <zsxsoft@outlook.com>
2014-03-01 12:44:00 +0000

    单独用户配置权限

--- zb_users/plugin/Howl/include.php
+++ zb_users/plugin/Howl/include.php
@@ -87,27 +87,43 @@ function UninstallPlugin_Howl(){
 
 function Howl_CheckRights(&$action){
 	global $zbp;
-$a=array();
-$a[1]=array();
-$a[2]=array();
-$a[3]=array();
-$a[4]=array();
-$a[5]=array();
-$a[6]=array();
-
-if($zbp->Config('Howl')->HasKey('Group1')){$a[1]=$zbp->Config('Howl')->Group1;}
-if($zbp->Config('Howl')->HasKey('Group2')){$a[2]=$zbp->Config('Howl')->Group2;}
-if($zbp->Config('Howl')->HasKey('Group3')){$a[3]=$zbp->Config('Howl')->Group3;}	
-if($zbp->Config('Howl')->HasKey('Group4')){$a[4]=$zbp->Config('Howl')->Group4;}
-if($zbp->Config('Howl')->HasKey('Group5')){$a[5]=$zbp->Config('Howl')->Group5;}
-if($zbp->Config('Howl')->HasKey('Group6')){$a[6]=$zbp->Config('Howl')->Group6;}
-
-
-$g=$zbp->user->Level;
-if(array_key_exists($action, $a[$g])){
-	$GLOBALS['Filter_Plugin_Zbp_CheckRights']['Howl_CheckRights']=PLUGIN_EXITSIGNAL_RETURN;
-	return (boolean)$a[$g][$action];
-}
+	$a = array();
+	$a[1] = array();
+	$a[2] = array();
+	$a[3] = array();
+	$a[4] = array();
+	$a[5] = array();
+	$a[6] = array();
+	$a[0] = array();
+	
+	if($zbp->Config('Howl')->HasKey('Group1')){$a[1]=$zbp->Config('Howl')->Group1;}
+	if($zbp->Config('Howl')->HasKey('Group2')){$a[2]=$zbp->Config('Howl')->Group2;}
+	if($zbp->Config('Howl')->HasKey('Group3')){$a[3]=$zbp->Config('Howl')->Group3;}	
+	if($zbp->Config('Howl')->HasKey('Group4')){$a[4]=$zbp->Config('Howl')->Group4;}
+	if($zbp->Config('Howl')->HasKey('Group5')){$a[5]=$zbp->Config('Howl')->Group5;}
+	if($zbp->Config('Howl')->HasKey('Group6')){$a[6]=$zbp->Config('Howl')->Group6;}
+	if($zbp->Config('Howl')->HasKey('User')){$a[0] = json_decode($zbp->Config('Howl')->User);}
+	
+	$g = $zbp->user->Level;
+	
+	if(($g < 6) && isset($a[0] -> $action)) 
+	{
+		$GLOBALS['Filter_Plugin_Zbp_CheckRights']['Howl_CheckRights'] = PLUGIN_EXITSIGNAL_RETURN;
+		$id = $zbp->user->ID;
+		return (boolean)isset($a[0] -> $action -> $id);
+		/*数据结构：
+		$a = array(
+			"action1" => array(
+				"userid" => "userid",
+				"userid" => "userid"
+			)
+		)
+		*/
+	}
+	else if(array_key_exists($action, $a[$g])){
+		$GLOBALS['Filter_Plugin_Zbp_CheckRights']['Howl_CheckRights'] = PLUGIN_EXITSIGNAL_RETURN;
+		return (boolean)$a[$g][$action];
+	}
 
 }
 
