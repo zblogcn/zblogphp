88e7d6a03d5cd43f0c2a4a14fe4108fbe7586905
zsxsoft0@gmail.com <zsxsoft@outlook.com>
2014-05-01 09:11:12 +0000

    加入自动拉黑网址功能

--- zb_users/plugin/Totoro/include.php
+++ zb_users/plugin/Totoro/include.php
@@ -3,7 +3,7 @@
  *  原有配置不考虑进行转移或升级
  *  1. 提取IP和网址
  */
-RegisterPlugin("Totoro","ActivePlugin_Totoro");
+RegisterPlugin("Totoro", "ActivePlugin_Totoro");
 define('TOTORO_PATH', dirname(__FILE__));
 define('TOTORO_INCPATH', TOTORO_PATH . '/inc/');
 
@@ -17,8 +17,9 @@ function Totoro_init()
 
 function ActivePlugin_Totoro()
 {
-	Add_Filter_Plugin('Filter_Plugin_Admin_CommentMng_SubMenu','Totoro_Admin_CommentMng_SubMenu');
-	Add_Filter_Plugin('Filter_Plugin_PostComment_Core','Totoro_PostComment_Core');
+	Add_Filter_Plugin('Filter_Plugin_Admin_CommentMng_SubMenu', 'Totoro_Admin_CommentMng_SubMenu');
+	Add_Filter_Plugin('Filter_Plugin_PostComment_Core', 'Totoro_PostComment_Core');
+	Add_Filter_Plugin('Filter_Plugin_Cmd_Begin', 'Totoro_Cmd_Begin');
 }
 
 
@@ -46,3 +47,21 @@ function Totoro_PostComment_Core(&$comment)
 	}
 }
 
+
+function Totoro_Cmd_Begin()
+{
+	global $zbp;
+
+	if(GetVars('act', 'GET') == 'CommentChk')
+	{
+		if(!$zbp->ValidToken(GetVars('token','GET'))){$zbp->ShowError(5,__FILE__,__LINE__);die();}
+			$id = (int) GetVars('id', 'GET');
+			$ischecking = (bool)GetVars('ischecking', 'GET');
+			if ($ischecking)
+			{
+				Totoro_init();
+				global $Totoro;
+				$Totoro->add_black_list($id);
+			}	
+	}
+}
