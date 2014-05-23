0af2247bd9f56c1bd45d00e37db629601f2d4a7d
zsxsoft0@gmail.com <zsxsoft0@gmail.com@40c8d13a-81fc-95dd-32b5-41977a75f8b4>
2014-05-23 14:54:44 +0000

    修改ZBPDK部分结构

--- trunk/zb_users/plugin/ZBPDK/extensions/BlogConfig/main.php
+++ trunk/zb_users/plugin/ZBPDK/extensions/BlogConfig/main.php
@@ -59,7 +59,12 @@ if(isset($_POST['act']))
 			exit();
 		break;
 		case 'e_edit':
-			$zbp->configs[$_POST['name2']]->$_POST['name1'] = $_POST['post'];
+			$config = $zbp->configs[$_POST['name2']]->$_POST['name1'];
+			$value = $_POST['post'];
+			if (gettype($config) == 'boolean') $value = (bool)$value;
+			elseif (gettype($config) == 'integer') $value = (int)$value;
+			$zbp->configs[$_POST['name2']]->$_POST['name1'] = $value;
+			
 			$zbp->SaveConfig($_POST['name2']);
 			echo blogconfig_exportlist($_POST['name2']);
 			exit();
--- trunk/zb_users/plugin/ZBPDK/include.php
+++ trunk/zb_users/plugin/ZBPDK/include.php
@@ -1,6 +1,5 @@
 <?php
-#注册插件
-RegisterPlugin("ZBPDK","ActivePlugin_ZBPDK");
+RegisterPlugin("ZBPDK", "ActivePlugin_ZBPDK");
 
 function ActivePlugin_ZBPDK()
 {
