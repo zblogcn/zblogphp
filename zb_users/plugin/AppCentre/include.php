9923422902a308a3f3f3e67931472aec8ab4acba
rainbowsoft <rainbowsoft@users.noreply.github.com>
2014-06-04 04:46:37 +0000

    再次还原；

--- zb_users/plugin/AppCentre/include.php
+++ zb_users/plugin/AppCentre/include.php
@@ -2,7 +2,7 @@
 #注册插件
 RegisterPlugin("AppCentre","ActivePlugin_AppCentre");
 
-define('APPCENTRE_URL','http://app.rainbowsoft.org/client/');
+define('APPCENTRE_URL','http://app.zblogcn.com/client/');
 
 define('APPCENTRE_SYSTEM_UPDATE','http://update.zblogcn.com/zblogphp/');
 
