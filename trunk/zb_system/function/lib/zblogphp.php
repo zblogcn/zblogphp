ac30ebf8a0f5dcd45fbd585a606659fc6377fd69
rainbowsoft@gmail.com <rainbowsoft@gmail.com@40c8d13a-81fc-95dd-32b5-41977a75f8b4>
2014-06-04 04:46:37 +0000

    再次还原；

--- trunk/zb_system/function/lib/zblogphp.php
+++ trunk/zb_system/function/lib/zblogphp.php
@@ -1592,10 +1592,6 @@ function AddBuildModuleAll(){
 	function StartGzip(){
 		if($this->isgziped)return false;
 
-		if(in_array('default output handler',ob_list_handlers())){
-			ob_end_clean();
-		}
-		
 		if($this->isgzip&&isset($this->option['ZC_GZIP_ENABLE'])&&$this->option['ZC_GZIP_ENABLE']){
 			if(!headers_sent()&&extension_loaded("zlib")&&isset($_SERVER["HTTP_ACCEPT_ENCODING"])&&strstr($_SERVER["HTTP_ACCEPT_ENCODING"],"gzip")){
 				ini_set('zlib.output_compression', 'On');
@@ -1605,8 +1601,6 @@ function AddBuildModuleAll(){
 				return true;
 			}
 		}
-
-		ob_start();
 	}
 
 
