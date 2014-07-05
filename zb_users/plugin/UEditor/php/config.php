8ed783b420ed27b5480c3e4a30de9309f21efc93
zsxsoft0@gmail.com <zsxsoft@outlook.com>
2014-07-05 12:57:56 +0000

    继续修正BUG

--- zb_users/plugin/UEditor/php/config.php
+++ zb_users/plugin/UEditor/php/config.php
@@ -8,15 +8,16 @@ $upload_path = '';// = $upload_dir = '';
 
 $upload_dir = $bloghost . $upload_dir;
 $upload_allow = explode('|', $zbp->option['ZC_UPLOAD_FILETYPE']);
+$max_size = $zbp->option['ZC_UPLOAD_FILESIZE'] * 1024 * 1024;
 
 $CONFIG = array(
     /* 上传图片配置项 */
     "imageActionName" => "uploadimage", /* 执行上传图片的action名称 */
     "imageFieldName" => "upfile", /* 提交的图片表单名称 */
-    "imageMaxSize" => 2048000, /* 上传大小限制，单位B */
+    "imageMaxSize" => $max_size, /* 上传大小限制，单位B */
     "imageAllowFiles" => $upload_allow, /* 上传图片格式显示 */
     "imageCompressEnable" => true, /* 是否压缩图片,默认是true */
-    "imageCompressBorder" => 1600, /* 图片压缩最长边限制 */
+    "imageCompressBorder" => 2940, /* 图片压缩最长边限制 */
     "imageInsertAlign" => "none", /* 插入的图片浮动方式 */
     "imageUrlPrefix" => "", /* 图片访问路径前缀 */
     "imagePathFormat" => $upload_dir. "{yyyy}{mm}{dd}{time}{rand:6}", /* 上传保存路径,可以自定义保存路径和文件名格式 */
@@ -37,7 +38,7 @@ $CONFIG = array(
     "scrawlActionName" => "uploadscrawl", /* 执行上传涂鸦的action名称 */
     "scrawlFieldName" => "upfile", /* 提交的图片表单名称 */
     "scrawlPathFormat" => $upload_dir. "{yyyy}{mm}{dd}{time}{rand:6}", /* 上传保存路径,可以自定义保存路径和文件名格式 */
-    "scrawlMaxSize" => 2048000, /* 上传大小限制，单位B */
+    "scrawlMaxSize" => $max_size, /* 上传大小限制，单位B */
     "scrawlUrlPrefix" => "", /* 图片访问路径前缀 */
     "scrawlInsertAlign" => "none",
 
@@ -47,7 +48,7 @@ $CONFIG = array(
     "catcherFieldName" => "source", /* 提交的图片列表表单名称 */
     "catcherPathFormat" => $upload_dir. "{yyyy}{mm}{dd}{time}{rand:6}", /* 上传保存路径,可以自定义保存路径和文件名格式 */
     "catcherUrlPrefix" => "", /* 图片访问路径前缀 */
-    "catcherMaxSize" => 2048000, /* 上传大小限制，单位B */
+    "catcherMaxSize" => $max_size, /* 上传大小限制，单位B */
     "catcherAllowFiles" => $upload_allow, /* 抓取图片格式显示 */
 
     /* 上传视频配置 */
@@ -55,7 +56,7 @@ $CONFIG = array(
     "videoFieldName" => "upfile", /* 提交的视频表单名称 */
     "videoPathFormat" => $upload_dir. "{yyyy}{mm}{dd}{time}{rand:6}", /* 上传保存路径,可以自定义保存路径和文件名格式 */
     "videoUrlPrefix" => "", /* 视频访问路径前缀 */
-    "videoMaxSize" => 102400000, /* 上传大小限制，单位B，默认100MB */
+    "videoMaxSize" => $max_size, /* 上传大小限制，单位B，默认100MB */
     "videoAllowFiles" => $upload_allow, /* 上传视频格式显示 */
 
     /* 上传文件配置 */
@@ -63,7 +64,7 @@ $CONFIG = array(
     "fileFieldName" => "upfile", /* 提交的文件表单名称 */
     "filePathFormat" => $upload_dir. "{yyyy}{mm}{dd}{time}{rand:6}", /* 上传保存路径,可以自定义保存路径和文件名格式 */
     "fileUrlPrefix" => "", /* 文件访问路径前缀 */
-    "fileMaxSize" => 51200000, /* 上传大小限制，单位B，默认50MB */
+    "fileMaxSize" => $max_size, /* 上传大小限制，单位B，默认50MB */
     "fileAllowFiles" => $upload_allow, /* 上传文件格式显示 */
 
 );
