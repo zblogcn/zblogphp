a0514845e3e02a94d6fb301086c25d6ee0a6bd3e
zsxsoft0@gmail.com <zsxsoft0@gmail.com@40c8d13a-81fc-95dd-32b5-41977a75f8b4>
2014-07-10 12:53:48 +0000

    水印功能完成

--- trunk/zb_users/plugin/qiniuyun/include.php
+++ trunk/zb_users/plugin/qiniuyun/include.php
@@ -3,6 +3,9 @@ include_once dirname(__FILE__) . "/qiniu/rs.php";
 include_once dirname(__FILE__) . "/qiniu/io.php";
 include_once dirname(__FILE__) . "/qiniu.class.php";
 include_once dirname(__FILE__) . "/function.php";
+//define('QINIU_WATER_URL', 'http://su.bdimg.com/static/superplus/img/logo_white.png');
+define('QINIU_WATER_URL', $bloghost . 'zb_users/plugin/qiniuyun/water.png');
+
 $qiniu = new QINIU();
 
 RegisterPlugin("qiniuyun","ActivePlugin_qiniuyun");
@@ -25,7 +28,7 @@ function qiniuyun_upload_url(&$upload)
 	init_qiniu();
 	global $zbp; global $qiniu;
 	$file = $zbp->GetUploadByID($upload->ID);
-	$url = $qiniu->get_url($file->Metas->qiniu_key);
+	$url = $qiniu->get_url($file->Metas->qiniu_key, ($qiniu->water_enable && !$qiniu->water_overwrite));
 	return $url;
 }
 
@@ -35,6 +38,7 @@ function qiniuyun_upload_savefile($tmp, &$upload)
 	global $zbp; global $qiniu;
 	$file_path = $zbp->usersdir . 'upload/zbp.qiniutmp.' .time();
 	$file_name = date("Ymd", time()) . mt_rand(1000, 9999) . '_' . mt_rand(0, 10000) . '.' . GetFileExt($upload->SourceName);
+	$upload_water = ($qiniu->water_enable && $qiniu->water_overwrite);
 	
 	if (is_file($tmp))
 		@move_uploaded_file($tmp, $file_path);//先上传到本地
@@ -44,11 +48,11 @@ function qiniuyun_upload_savefile($tmp, &$upload)
 	$upload->Name = $file_name;	
 	
 	$cloud_path = $qiniu->cloudpath . date("Y/m/", time()) . $file_name;	//构造云文件名
-	
-	$ret = $qiniu->upload($cloud_path, $file_path);
+
+	$ret = $qiniu->upload($cloud_path, $file_path, $upload_water);
 	$upload->Metas->qiniu_key = $ret['key'];
 	
-	
+	unlink($file_path);
 	$GLOBALS['Filter_Plugin_Upload_SaveFile']['qiniuyun_upload_savefile'] = PLUGIN_EXITSIGNAL_RETURN;
 	$GLOBALS['Filter_Plugin_Upload_SaveBase64File']['qiniuyun_upload_savefile'] = PLUGIN_EXITSIGNAL_RETURN;
 	return true;
--- trunk/zb_users/plugin/qiniuyun/qiniu.class.php
+++ trunk/zb_users/plugin/qiniuyun/qiniu.class.php
@@ -58,6 +58,12 @@ class QINIU
 		$this->cfg->bucket = '';
 		$this->cfg->domain = '';
 		$this->cfg->cloudpath = '';
+		$this->cfg->water_enable = FALSE;
+		$this->cfg->water_overwrite = FALSE;
+		$this->cfg->water_dissolve = '100';	
+		$this->cfg->water_gravity = 'SouthEast';
+		$this->cfg->water_dx = '10';
+		$this->cfg->water_dy = '10';
 		$this->cfg->version = '1.0';
 		return $this->save_config();
 		
@@ -69,10 +75,13 @@ class QINIU
 	}
 	
 	
-	public function get_url($key)
+	public function get_url($key, $water = FALSE)
 	{
 		$domain = ($this->cfg->domain == '' ? $this->bucket . '.qiniudn.com' : $this->cfg->domain);
-		return Qiniu_RS_MakeBaseUrl($domain, $key);
+		$return = Qiniu_RS_MakeBaseUrl($domain, $key);
+		if ($water && qiniu_test_image($return))
+			$return = $this->get_waterimage_url($return, QINIU_WATER_URL, $this->water_dissolve, $this->water_gravity, $this->water_dx, $this->water_dy);
+		return $return;
 	}
 	
 	public function delete($key)
@@ -81,12 +90,24 @@ class QINIU
 		return Qiniu_RS_Delete($client, $this->bucket, $key);
 	}
 	
-	public function upload($filepath_cloud, $filepath_local)
+	public function upload($filepath_cloud, $filepath_local, $watermark = FALSE)
 	{
 		$upload_token = $this->get_upload_token();
 		$putExtra = new Qiniu_PutExtra();
 		$putExtra->Crc32 = 1;
 		list($ret, $err) = Qiniu_PutFile($upload_token, $filepath_cloud, $filepath_local, $putExtra);
+
+		if ($watermark && $err == NULL)
+		{
+			$url = $this->get_url($ret['key'], TRUE);
+			if (!qiniu_test_image($url)) return $ret;
+			$local_url = $this->download_waterimage($url);
+			$this->delete($ret['key']);
+			$return = $this->upload($filepath_cloud, $local_url, FALSE);
+			unlink($local_url);
+			return $return;
+		}
+		
 		return $ret;
 	}
 	
@@ -94,5 +115,32 @@ class QINIU
 	{
 		$putPolicy = new Qiniu_RS_PutPolicy($this->bucket);
 		return $putPolicy->Token(NULL);
+	}
+	
+	private function download_waterimage($url_cloud)
+	{
+		global $zbp;
+		$local_file = $zbp->usersdir . 'upload/zbp.qiniutmp.water.' .time();
+		$ajax = Network::Create();
+		if(!$ajax) throw new Exception('主机没有开启网络功能');
+		
+		$ajax->open('GET', $url_cloud);
+		$ajax->send();
+		file_put_contents($local_file, $ajax->responseText);
+		return $local_file;
+	}
+	
+	private function get_waterimage_url($url_cloud, $image_local, $dissolve = 100, $gravity = 'SouthEast', $dx = 10, $dy = 10)
+	{
+		$param = array(
+			"watermark", "1",
+			"image", str_replace(array('+', '/'),array('-', '_'),base64_encode($image_local)),
+			"dissolve", $dissolve,
+			"gravity", $gravity,
+			"dx", $dx,
+			"dy", $dy
+		);
+		
+		return $url_cloud . '?' . implode($param, '/');
 	}
 }
