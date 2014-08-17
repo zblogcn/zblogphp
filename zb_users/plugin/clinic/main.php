4ad721919588299f720ca7f1009a0c5ad094e1e1
zsxsoft0@gmail.com <zsxsoft@outlook.com>
2014-08-17 13:37:03 +0000

    Z-BlogPHP Clinic

---
+++ zb_users/plugin/clinic/main.php
@@ -0,0 +1,41 @@
+<?php
+require '../../../zb_system/function/c_system_base.php';
+require '../../../zb_system/function/c_system_admin.php';
+$zbp->Load();
+$action='root';
+if (!$zbp->CheckRights($action)) {$zbp->ShowError(6);die();}
+if (!$zbp->CheckPlugin('clinic')) {$zbp->ShowError(48);die();}
+require 'clinic.php';
+$module = GetVars('module', 'GET');
+$module = (isset($clinic->modules[$module]) ? $clinic->modules[$module] : NULL);
+$blogtitle = 'Z-BlogPHP诊断工具' . ($module ? ' - ' . $module->name : '');
+
+require $blogpath . 'zb_system/admin/admin_header.php';
+echo '<style type="text/css">tr{height: 32px}</style><script type="text/javascript" src="include/clinic.js"></script>';
+require $blogpath . 'zb_system/admin/admin_top.php';
+?>
+<div id="divMain">
+  <div class="divHeader"><?php echo $blogtitle;?></div>
+  <div class="SubMenu">
+  </div>
+  <div id="divMain2">
+<?php
+
+if ($module) {
+	require('include/gui_module.inc');
+}
+else {
+	require('include/gui_main.inc');
+}
+?>
+
+  </div>
+</div>
+
+<?php
+require $blogpath . 'zb_system/admin/admin_footer.php';
+RunTime();
+
+
+
+
---
+++ zb_users/plugin/clinic/modules/enviroment-check/enviroment-check.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Z-BlogPHP Clinic check enviroment
+ * @package enviroment-check
+ * @subpackage enviroment-check.php
+ */
+
+
+class enviroment_check extends clinic {
+	
+	private $_functions = array(
+		'gd_info' => 'GD库 - 用于生成验证码等',
+		'curl_init' => 'curl（非必须） - 用于更高效地访问网络'
+	);
+	
+	/**
+	 * Build queue
+	 * @return null
+	 */
+	public function get_queue() {
+		foreach($this->_functions as $name => $value)
+			$this->set_queue('check_function', $name);	
+	}
+	
+	/**
+	 * Check function exists
+	 * @param string $param 
+	 * @return null
+	 */
+	public function check_function($param) {
+		
+		if (function_exists($param)) {
+			$output = '';
+			switch($param) {
+				case 'gd_info':
+					$info = $param();
+					$output = $info['GD Version'];
+					break;
+				case 'curl_init':
+					$output = '';
+				break;
+			}
+			
+			$this->output('success', $this->_functions[$param] . ' - ' . $output);
+		}
+		else {
+			$this->output('error', $this->_functions[$param] . ' 未安装，请联系空间商');
+		}
+		
+		
+	}
+	
+}
