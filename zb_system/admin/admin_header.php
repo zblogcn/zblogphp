e414b99598658831aa0b61d3702faa415a8d7334
zsxsoft <zsxsoft@outlook.com>
2014-09-19 20:10:40 +0800

    header简单修改

--- zb_system/admin/admin_header.php
+++ zb_system/admin/admin_header.php
@@ -8,13 +8,20 @@
 <meta name="generator" content="Z-BlogPHP <?php echo $option['ZC_BLOG_VERSION']?>" />
 <meta name="robots" content="none" />
 <title><?php echo $blogname . '-' . $blogtitle?></title>
-<link href="<?php echo $bloghost?>zb_system/css/admin2.css" rel="stylesheet" type="text/css" />
-<script src="<?php echo $bloghost?>zb_system/script/common.js" type="text/javascript"></script>
-<script src="<?php echo $bloghost?>zb_system/script/c_admin_js_add.php" type="text/javascript"></script>
+<link href="<?php echo $bloghost?>zb_system/static/css/bootstrap.css" rel="stylesheet" type="text/css" />
+<link href="<?php echo $bloghost?>zb_system/static/css/bootstrap-admin.css" rel="stylesheet" type="text/css" />
+<link href="<?php echo $bloghost?>zb_system/static/css/bootstrap-theme.css" rel="stylesheet" type="text/css" />
 <link rel="stylesheet" href="<?php echo $bloghost?>zb_system/css/jquery.bettertip.css" type="text/css" media="screen" />
+<link rel="stylesheet" type="text/css" href="<?php echo $bloghost?>zb_system/css/jquery-ui.custom.css"/>
+
+
+
+<script src="<?php echo $bloghost?>zb_system/script/common.js" type="text/javascript"></script>
+<script src="<?php echo $bloghost?>zb_system/static/js/bootstrap.js" type="text/javascript"></script>
+<script src="<?php echo $bloghost?>zb_system/script/c_admin_js_add.php" type="text/javascript"></script>
 <script src="<?php echo $bloghost?>zb_system/script/jquery.bettertip.pack.js" type="text/javascript"></script>
 <script src="<?php echo $bloghost?>zb_system/script/jquery-ui.custom.min.js" type="text/javascript"></script>
-<link rel="stylesheet" type="text/css" href="<?php echo $bloghost?>zb_system/css/jquery-ui.custom.css"/>
+
 <?php
 	foreach ($GLOBALS['Filter_Plugin_Admin_Header'] as $fpname => &$fpsignal) {$fpname();}
 ?>
