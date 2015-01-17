9bcbf3f7ba2e5a52b2650e5459ac5ff3e7962f2c
zsx <zsx@zsxsoft.com>
2015-01-17 11:25:00 +0800

    修改前端js

--- zb_system/script/c_html_js_add.php
+++ zb_system/script/c_html_js_add.php
@@ -13,29 +13,41 @@ ob_clean();
 
 header('Content-Type: application/x-javascript; charset=utf-8');
 ?>
-var bloghost="<?php echo $zbp->host; ?>";
-var cookiespath="<?php echo $zbp->cookiespath; ?>";
-var ajaxurl="<?php echo $zbp->ajaxurl; ?>";
-var lang_comment_name_error="<?php echo $lang['error']['72']; ?>";
-var lang_comment_email_error="<?php echo $lang['error']['29']; ?>";
-var lang_comment_content_error="<?php echo $lang['error']['46']; ?>";
+var bloghost = "<?php echo $zbp->host; ?>";
+var cookiespath = "<?php echo $zbp->cookiespath; ?>";
+var ajaxurl = "<?php echo $zbp->ajaxurl; ?>";
+var lang_comment_name_error = "<?php echo $lang['error']['72']; ?>";
+var lang_comment_email_error = "<?php echo $lang['error']['29']; ?>";
+var lang_comment_content_error = "<?php echo $lang['error']['46']; ?>";
 
 <?php
-echo '$(document).ready(function(){';
+echo '$(function () {';
+echo 'var $cpLogin = $(".cp-login").find("a");';
+echo 'var $cpVrs = $(".cp-vrs").find("a");';
 
 if ($zbp->CheckRights('admin')){
-	echo "$('.cp-hello').html('" . $zbp->lang['msg']['welcome'] . ' ' . $zbp->user->Name .  " ("  . $zbp->user->LevelName  . ")');";
-	echo "if($('.cp-login').find('a').length==1 && $('.cp-login').find('a').html().indexOf('[')>-1)$('.cp-login').find('a').html('[" . $zbp->lang['msg']['admin'] . "]');else $('.cp-login').find('a').html('" . $zbp->lang['msg']['admin'] . "');";
-}
-if ($zbp->CheckRights('ArticleEdt')){
-	echo "if($('.cp-login').find('a').length==1 && $('.cp-vrs').find('a').html().indexOf('[')>-1)$('.cp-vrs').find('a').html('[" . $zbp->lang['msg']['new_article'] . "]');else $('.cp-vrs').find('a').html('" . $zbp->lang['msg']['new_article'] . "');";
-	echo "$('.cp-vrs').find('a').attr('href','" . $zbp->host . "zb_system/cmd.php?act=ArticleEdt');";
+	echo '$(".cp-hello").html("' . $zbp->lang['msg']['welcome'] . ' ' . $zbp->user->Name .  ' ('  . $zbp->user->LevelName  . ')");';
+	
+	echo 'if ($cpLogin.length == 1 && $cpLogin.html().indexOf("[") > -1) { ';
+	echo '$cpLogin.html("[' . $zbp->lang['msg']['admin'] . ']");';
+	echo '} else {';
+	echo '$cpLogin.html("' . $zbp->lang['msg']['admin'] . '");';
+	echo '};';
 }
 
-	echo "SetCookie('timezone',(new Date().getTimezoneOffset()/60)*(-1));";
+if ($zbp->CheckRights('ArticleEdt')){
+	echo 'if ($cpLogin.length == 1 && $cpVrs.html().indexOf("[") > -1) {';
+	echo '$cpVrs.html("[' . $zbp->lang['msg']['new_article'] . ']"); ';
+	echo '} else {';
+	echo '$cpVrs.html("' . $zbp->lang['msg']['new_article'] . '");';
+	echo '};';
+	echo '$cpVrs.attr("href", "' . $zbp->host . 'zb_system/cmd.php?act=ArticleEdt");';
+}
+
+	echo 'SetCookie("timezone", (new Date().getTimezoneOffset()/60)*(-1));';
 
 if ($zbp->user->ID==0){
-	echo "LoadRememberInfo();";
+	echo 'LoadRememberInfo();';
 }
 
 echo '});' . "\r\n";
