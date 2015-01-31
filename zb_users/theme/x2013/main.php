f888da358d535d472576f9e64516cb45bf062e51
未寒 <involvements@users.noreply.github.com>
2015-01-31 18:49:58 +0800

    增加颜色选择器

--- zb_users/theme/x2013/main.php
+++ zb_users/theme/x2013/main.php
@@ -44,6 +44,10 @@ if(isset($_POST['DisplayFeed']) && $_POST['DisplayFeed'] != ''){
 	}
 	$zbp->Config('x2013')->DisplayFeed = $DisplayFeed;
 	$zbp->Config('x2013')->SetMailKey = $SetMailKey;
+	$zbp->Config('x2013')->Css = $_POST['color'];
+	$css = @file_get_contents('style.css.html');
+	$css = str_replace('{%background%}', $zbp->Config('x2013')->Css, $css);
+	@file_put_contents($zbp->path.'zb_users/theme/x2013/style/style.css', $css);
 	$zbp->SaveConfig('x2013');
 	if(isset($tips)){$zbp->ShowHint('good', $tips);}
 	//var_dump($_POST);
@@ -52,7 +56,16 @@ if(isset($_POST['DisplayFeed']) && $_POST['DisplayFeed'] != ''){
 <style>
 input.text{background:#FFF;border:1px double #aaa;font-size:1em;padding:0.25em;}
 p{line-height:1.5em;padding:0.5em 0;}
+.tc{border: solid 2px #E1E1E1;width: 50px;height: 23px;float: left;margin: 0.25em;cursor: pointer}
+.tc:hover,.active{border: 2px solid #2694E8;}
 </style>
+<script type="text/javascript" src="farbtastic.js"></script>
+<link rel="stylesheet" href="farbtastic.css" type="text/css" />
+ <script type="text/javascript" charset="utf-8">
+  $(document).ready(function() {
+    $('#picker').farbtastic('#color');
+  });
+ </script>
 <div id="divMain">
 	<div class="divHeader"><?php echo $blogtitle;?></div>
   	<div class="SubMenu"><?php x2013_SubMenu(0);?></div>
@@ -67,7 +80,7 @@ p{line-height:1.5em;padding:0.5em 0;}
   <tr>
     <td><b><label for="SetWeiboSina"><p align="center">新浪微博</p></label></b></td>
     <td><p align="left"><input name="SetWeiboSina" type="text" id="SetWeiboSina" size="100%" value="<?php echo $zbp->Config('x2013')->SetWeiboSina;?>" /></p></td>
-    
+
   </tr>
   <tr>
     <td><b><label for="SetWeiboQQ"><p align="center">腾讯微博</p></label></b></td>
@@ -81,6 +94,29 @@ p{line-height:1.5em;padding:0.5em 0;}
     <td><b><label for="SetMailKey"><p align="center"><a href="http://list.qq.com/" target="_black">QQMail邮件订阅key</a></p></label></b></td>
     <td><p align="left"><input name="SetMailKey" type="text" id="SetMailKey" size="100%" value="<?php echo $zbp->Config('x2013')->SetMailKey;?>" /></p></td>
   </tr>  
+</table>
+<table width="100%" border="1" width="100%" class="tableBorder">
+	<tr>
+		<th scope="col" height="32" width="150px">颜色配置</th>
+		<th scope="col" width="120px">
+		<div style="float:left;margin: 0.25em">推荐颜色：</div>
+		</th>
+		<th >
+			<div id="loadconfig">
+			<div class="tc" onclick='$("#color").val("#38A3DB");$("#color").css("background-color","#38A3DB");' style="background-color:#38A3DB"></div>
+			<div class="tc" onclick='$("#color").val("#D581A2");$("#color").css("background-color","#D581A2");' style="background-color:#D581A2"></div>
+			<div class="tc" onclick='$("#color").val("#CC0000");$("#color").css("background-color","#CC0000");' style="background-color:#CC0000"></div>
+			<div class="tc" onclick='$("#color").val("#00ccca");$("#color").css("background-color","#00ccca");' style="background-color:#00ccca"></div>
+			<div class="tc" onclick='$("#color").val("#ccaf00");$("#color").css("background-color","#ccaf00");' style="background-color:#ccaf00"></div>
+			<div class="tc" onclick='$("#color").val("#18ae04");$("#color").css("background-color","#18ae04");' style="background-color:#18ae04"></div>
+			</div>
+		</th>
+	</tr>
+	<tr>
+		<td>主题色调</td>
+		<td><input type="text" id="color" name="color" value="<?php echo $zbp->Config('x2013')->Css;?>"/></div></td>
+		<td><div id="picker"></div></td>
+	</tr>
 </table>
  <br />
    <input name="" type="Submit" class="button" value="保存"/>
---
+++ zb_users/theme/x2013/save.asp
@@ -0,0 +1,49 @@
+﻿<%@LANGUAGE="VBSCRIPT" CODEPAGE="65001"%>
+<% Option Explicit %>
+<% 'On Error Resume Next %>
+<% Response.Charset="UTF-8" %>
+<!-- #include file="..\..\..\c_option.asp" -->
+<!-- #include file="..\..\..\..\zb_system\function\c_function.asp" -->
+<!-- #include file="..\..\..\..\zb_system\function\c_system_lib.asp" -->
+<!-- #include file="..\..\..\..\zb_system\function\c_system_base.asp" -->
+<!-- #include file="..\..\..\..\zb_system\function\c_system_event.asp" -->
+<!-- #include file="..\..\..\..\zb_system\function\c_system_plugin.asp" -->
+<!-- #include file="..\..\..\plugin\p_config.asp" -->
+<%
+Call System_Initialize()
+'检查非法链接
+Call CheckReference("")
+'检查权限
+If BlogUser.Level>1 Then Call ShowError(6)
+
+Dim strc
+strc=Request.Form("color")
+
+'Tconfig
+Dim c
+Set c = New TConfig
+c.Load("SimplePro")
+If c.Exists("Color")=True Then
+	c.Write "Color",strc
+	c.Save
+End If 
+Set c =Nothing
+
+'******************************************************************************************
+' 保存css文件
+'******************************************************************************************
+	'替换模版标签
+	Dim strContent
+	strContent=LoadFromFile(BlogPath & "zb_users\theme\SimplePro\plugin\style.css.html" ,"utf-8")
+	strContent=Replace(strContent,"{%Color%}",strc)
+	Call SaveToFile(BlogPath & "zb_users\theme\SimplePro\style\style.css",strContent,"utf-8",False)
+
+
+
+Call SetBlogHint(True,Empty,Empty)
+
+Response.Redirect "editor.asp"
+
+%>
+
+<%Call System_Terminate()%>
