500260ab54803dec33838abbb9f829f0f42bb5b5
rainbowsoft <rainbowsoft@gmail.com>
2015-02-17 00:28:06 +0800

    删除一个无用的设置项；

--- zb_system/defend/option.php
+++ zb_system/defend/option.php
@@ -60,11 +60,11 @@ return array(
 	'ZC_BLOG_CLSID' => '',
 	'ZC_TIME_ZONE_NAME' => 'Asia/Shanghai',
 	'ZC_UPDATE_INFO_URL' => 'http://update.zblogcn.com/info/',
-	// '固定域名,默认为false,如启用则'ZC_BLOG_HOST生效而'ZC_MULTI_DOMAIN_SUPPORT无效
+
+	// '固定域名,默认为false
 	'ZC_PERMANENT_DOMAIN_ENABLE' => false,
 	'ZC_PERMANENT_DOMAIN_REDIRECT' => true,
 	'ZC_PERMANENT_DOMAIN_WITH_ADMIN' => false,
-	'ZC_MULTI_DOMAIN_SUPPORT' => false,
 
 	//
 	'ZC_DEBUG_MODE' => false,
