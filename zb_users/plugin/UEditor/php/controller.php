4613ddead94d64119f86f3bde8dfb6211e8f9de2
zsx <zsx@zsxsoft.com>
2015-02-16 13:17:08 +0800

    防止因为BOM头导致数据出错

--- zb_users/plugin/UEditor/php/controller.php
+++ zb_users/plugin/UEditor/php/controller.php
@@ -4,46 +4,46 @@
 //date_default_timezone_set("Asia/chongqing");
 header("Content-Type: text/html; charset=utf-8");
 
-require('ue.config.php');
+require 'ue.config.php';
 $action = $_GET['action'];
 
 switch ($action) {
-    case 'config':
-        $result =  json_encode($CONFIG);
-        break;
+	case 'config':
+		$result = json_encode($CONFIG);
+		break;
 
-    /* 上传图片 */
-    case 'uploadimage':
-    /* 上传涂鸦 */
-    case 'uploadscrawl':
-    /* 上传视频 */
-    case 'uploadvideo':
-    /* 上传文件 */
-    case 'uploadfile':
-        $result = include("action_upload.php");
-        break;
+	/* 上传图片 */
+	case 'uploadimage':
+	/* 上传涂鸦 */
+	case 'uploadscrawl':
+	/* 上传视频 */
+	case 'uploadvideo':
+	/* 上传文件 */
+	case 'uploadfile':
+		$result = include "action_upload.php";
+		break;
 
-    /* 抓取远程文件 */
-    case 'catchimage':
-        $result = include("action_crawler.php");
-        break;
+	/* 抓取远程文件 */
+	case 'catchimage':
+		$result = include "action_crawler.php";
+		break;
 
-    default:
-        $result = json_encode(array(
-            'state'=> '请求地址出错'
-        ));
-        break;
+	default:
+		$result = json_encode(array(
+			'state' => '请求地址出错',
+		));
+		break;
 }
-
+ob_clean();
 /* 输出结果 */
 if (isset($_GET["callback"])) {
-    if (preg_match("/^[\w_]+$/", $_GET["callback"])) {
-        echo htmlspecialchars($_GET["callback"]) . '(' . $result . ')';
-    } else {
-        echo json_encode(array(
-            'state'=> 'callback参数不合法'
-        ));
-    }
+	if (preg_match("/^[\w_]+$/", $_GET["callback"])) {
+		echo htmlspecialchars($_GET["callback"]) . '(' . $result . ')';
+	} else {
+		echo json_encode(array(
+			'state' => 'callback参数不合法',
+		));
+	}
 } else {
-    echo $result;
+	echo $result;
 }
--- zb_users/plugin/clinic/ajax.php
+++ zb_users/plugin/clinic/ajax.php
@@ -2,19 +2,22 @@
 require '../../../zb_system/function/c_system_base.php';
 require '../../../zb_system/function/c_system_admin.php';
 $zbp->Load();
-$action='root';
+$action = 'root';
 if (!$zbp->CheckRights($action)) {$zbp->ShowError(6);die();}
 if (!$zbp->CheckPlugin('clinic')) {$zbp->ShowError(48);die();}
 
 require 'clinic.php';
 $module = GetVars('module', 'GET');
 $module = (isset($clinic->modules[$module]) ? $clinic->modules[$module] : NULL);
-if (!$module) exit(json_encode(array("err" => "no this module")));
+if (!$module) {
+	exit(json_encode(array("err" => "no this module")));
+}
 
 $func = GetVars('function', 'POST');
-$param = GetVars('param', 'POST') | '';
+$param = GetVars('param', 'POST')|'';
 $class = $clinic->load_module($module['id']);
 $class->$func($param);
+ob_clean();
 echo '[';
 echo implode(',', $class->output_json);
 echo ']';
