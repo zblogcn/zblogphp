5e4b3feb5c4e8aea5bc3fece94607945ce293b7a
zsx <zsx@zsxsoft.com>
2015-02-23 21:37:34 +0800

    对zblogphp.js进行格式化

--- zb_system/script/zblogphp.js
+++ zb_system/script/zblogphp.js
@@ -1,8 +1,8 @@
 ﻿/** 
- * Z-BlogPHP JavaScript Framework 
+ * Z-BlogPHP JavaScript Framework
  * @author zsx<zsx@zsxsoft.com>
- */  
-(function () {
+ */
+(function() {
 	/**
 	 * Class ZBP
 	 *
@@ -12,7 +12,7 @@
 	 * @param options {OPTION} Init option
 	 * @param jquery {object} jQuery
 	 */
-	var ZBP = function (options, jquery) {
+	var ZBP = function(options, jquery) {
 		/** Load jQuery library */
 		if (typeof jQuery == 'undefined' && typeof jquery == 'undefined') throw new Error('No jQuery!');
 		this.$ = jquery || jQuery;
@@ -46,26 +46,26 @@
 		this.userinfo.homepage = this.cookie.get("homepage");
 
 		/** Register system events */
-		this.plugin.on("userinfo.output", "system", function () {
+		this.plugin.on("userinfo.output", "system", function() {
 			this.$("#inpName").val(this.userinfo.username);
 			this.$("#inpEmail").val(this.userinfo.mail);
 			this.$("#inpHomePage").val(this.userinfo.homepage);
 		});
 
-		this.plugin.on("userinfo.savefromhtml", "system", function () {
+		this.plugin.on("userinfo.savefromhtml", "system", function() {
 			this.userinfo.username = this.$("#inpName").val();
 			this.userinfo.mail = this.$("#inpEmail").val();
 			this.userinfo.homepage = this.$("#inpHomePage").val();
 			this.userinfo.save();
 		});
 
-		this.plugin.on("userinfo.save", "system", function () {
+		this.plugin.on("userinfo.save", "system", function() {
 			this.cookie.set("name", this.userinfo.username);
 			this.cookie.set("email", this.userinfo.mail);
 			this.cookie.set("homepage", this.userinfo.homepage);
 		});
 
-		this.plugin.on("comment.verifydata", "system", function (error, formData) {
+		this.plugin.on("comment.verifydata", "system", function(error, formData) {
 
 			var regExList = {
 				// [canBeEmpty, errorCode, RegExp]
@@ -82,65 +82,67 @@
 					return error;
 				}
 			}
-			
+
 			var objSubmit = $("#inpId").parent("form").find(":submit");
-			objSubmit.data("orig", objSubmit.val()).val("Waiting...").attr("disabled","disabled").addClass("loading");
+			objSubmit.data("orig", objSubmit.val()).val("Waiting...").attr("disabled", "disabled").addClass("loading");
 
 		});
 
-		this.plugin.on("comment.posterror", "system", function (error, formData) {
+		this.plugin.on("comment.posterror", "system", function(error, formData) {
 
 			var objSubmit = $("#inpId").parent("form").find(":submit");
 			objSubmit.removeClass("loading").removeAttr("disabled").val(objSubmit.data("orig"));
 
 		});
 
-		this.plugin.on("comment.postsuccess", "system", function (formData, data, textStatus, jqXhr) {
+		this.plugin.on("comment.postsuccess", "system", function(formData, data, textStatus, jqXhr) {
 
 			var objSubmit = $("#inpId").parent("form").find(":submit");
 			objSubmit.removeClass("loading").removeAttr("disabled").val(objSubmit.data("orig"));
-			
-			if((data.search("faultCode") > 0) && (data.search("faultString")>0)) {
-				var errorData = s.match("<string>.+?</string>")[0].replace("<string>","").replace("</string>","");
+
+			if ((data.search("faultCode") > 0) && (data.search("faultString") > 0)) {
+				var errorData = s.match("<string>.+?</string>")[0].replace("<string>", "").replace("</string>", "");
 				alert(errorData);
-				try {console.log(arguments); console.log("ERROR - " + errorData)} catch (e) { /* do nothing*/}
-			}
-			else {
+				try {
+					console.log(arguments);
+					console.log("ERROR - " + errorData)
+				} catch (e) { /* do nothing*/ }
+			} else {
 				var cmt = data.match(/cmt\d+/);
-				if(formData.replyid == 0){
+				if (formData.replyid == 0) {
 					this.$(data).insertAfter("#AjaxCommentBegin");
 				} else {
 					this.$(data).insertAfter("#AjaxComment" + formData.replyid);
 				}
 				location.hash = "#" + cmt;
 				this.$("#txaArticle").val("");
-				
+
 				this.userinfo.saveFromHtml();
 			}
 
 		});
 
-		this.plugin.on("comment.get", "system", function (postid, page) {
-			this.$.get(this.options.bloghost + "zb_system/cmd.php?act=getcmt&postid=" + postid + "&page=" + page, function (data, textStatus, jqXhr) { 
-				this.plugin.emit("comment.got", [postid, page], data, textStatus, jqXhr)	
+		this.plugin.on("comment.get", "system", function(postid, page) {
+			this.$.get(this.options.bloghost + "zb_system/cmd.php?act=getcmt&postid=" + postid + "&page=" + page, function(data, textStatus, jqXhr) {
+				this.plugin.emit("comment.got", [postid, page], data, textStatus, jqXhr)
 			});
 		});
 
-		this.plugin.on("comment.got", "system", function (formData, data, textStatus, jqXhr) {
+		this.plugin.on("comment.got", "system", function(formData, data, textStatus, jqXhr) {
 			this.$('#AjaxCommentBegin').nextUntil('#AjaxCommentEnd').remove();
 			this.$('#AjaxCommentBegin').after(data);
 		});
 
-		this.plugin.on("comment.reply", "system", function (id) {
+		this.plugin.on("comment.reply", "system", function(id) {
 			var me = this;
 			this.$("#inpRevID").val(id);
-			this.$("#cancel-reply").show().bind("click", function () {
+			this.$("#cancel-reply").show().bind("click", function() {
 				me.$("#inpRevID").val(0);
 				me.$(this).hide();
 				window.location.hash = "#comment";
 				return false;
 			});
-			window.location.hash="#comment";
+			window.location.hash = "#comment";
 		});
 
 
@@ -158,23 +160,23 @@
 	 *
 	 * @param  object
 	 **/
-	var initMethods = function (self) {
+	var initMethods = function(self) {
 
 		/**
 		 * PLUGIN
 		 * @class PLUGIN
 		 */
-		var PLUGIN = function () {};
+		var PLUGIN = function() {};
 		/**
 		 * Add listener
 		 * @function
-	 	 * @memberOf PLUGIN
+		 * @memberOf PLUGIN
 		 * @param interfaceName {string}
 		 * @param pluginName {string}
 		 * @param callback {Function}
 		 * @return {object} this
 		 */
-		PLUGIN.prototype.bind = PLUGIN.prototype.on = PLUGIN.prototype.addListener = function (interfaceName, pluginName, callback) {
+		PLUGIN.prototype.bind = PLUGIN.prototype.on = PLUGIN.prototype.addListener = function(interfaceName, pluginName, callback) {
 			if (typeof self._plugins[interfaceName] == 'undefined') self._plugins[interfaceName] = {};
 			self._plugins[interfaceName][pluginName] = callback;
 			return self;
@@ -187,7 +189,7 @@
 		 * @param pluginName {string}
 		 * @return {object} this
 		 */
-		PLUGIN.prototype.unbind = PLUGIN.prototype.removeListener = function (interfaceName, pluginName) {
+		PLUGIN.prototype.unbind = PLUGIN.prototype.removeListener = function(interfaceName, pluginName) {
 			if (!pluginName) pluginName = "";
 			if (pluginName == "") {
 				self._plugins[interfaceName] = {};
@@ -204,7 +206,7 @@
 		 * @param interfaceName {string}
 		 * @return {object} this
 		 */
-		PLUGIN.prototype.emit = function (interfaceName) {
+		PLUGIN.prototype.emit = function(interfaceName) {
 			// var argu = self.$.extend([], arguments);
 			// argu.shift();
 			// Let's fuck IE6 together!
@@ -228,14 +230,14 @@
 		 * COOKIE
 		 * @class COOKIE
 		 */
-		var COOKIE = function () {};
+		var COOKIE = function() {};
 		/**
 		 * Get Cookie
 		 * @memberOf COOKIE
 		 * @param sCookieName {string} Cookie Key
 		 * @return cookieValue {string} Cookie Value
 		 */
-		COOKIE.prototype.get = function (sCookieName) {
+		COOKIE.prototype.get = function(sCookieName) {
 			var arr = document.cookie.match(new RegExp("(^| )" + sCookieName + "=([^;]*)(;|$)"));
 			return (arr ? unescape(arr[2]) : null);
 		};
@@ -247,7 +249,7 @@
 		 * @param iExpireDays {int} Cookie Expires
 		 * @return ZBP {ZBP}
 		 */
-		COOKIE.prototype.set = function (sCookieName, sCookieValue, iExpireDays) {
+		COOKIE.prototype.set = function(sCookieName, sCookieValue, iExpireDays) {
 			var dExpire = new Date();
 			if (iExpireDays) {
 				dExpire.setTime(dExpire.getTime() + parseInt(iExpireDays * 24 * 60 * 60 * 1000));
@@ -266,13 +268,13 @@
 		 * USERINFO
 		 * @class USERINFO
 		 */
-		var USERINFO = function () {};
+		var USERINFO = function() {};
 		/**
 		 * Output user information to DOM
 		 * @memberOf USERINFO
 		 * @return ZBP {ZBP}
 		 */
-		USERINFO.prototype.output = function () {
+		USERINFO.prototype.output = function() {
 			self.plugin.emit("userinfo.output");
 			return self;
 		}
@@ -281,7 +283,7 @@
 		 * @memberOf USERINFO
 		 * @return ZBP {ZBP}
 		 */
-		USERINFO.prototype.save = function () {
+		USERINFO.prototype.save = function() {
 			self.plugin.emit("userinfo.save");
 			return self;
 		}
@@ -290,7 +292,7 @@
 		 * @memberOf USERINFO
 		 * @return ZBP {ZBP}
 		 */
-		USERINFO.prototype.saveFromHtml = function () {
+		USERINFO.prototype.saveFromHtml = function() {
 			self.plugin.emit("userinfo.savefromhtml");
 			return self;
 		}
@@ -305,7 +307,7 @@
 		 * COMMENT
 		 * @class COMMENT
 		 */
-		var COMMENT = function () {};
+		var COMMENT = function() {};
 		/**
 		 * Get comments
 		 * @memberOf COMMENT
@@ -313,7 +315,7 @@
 		 * @param page {int} Page
 		 * @return ZBP {ZBP}
 		 */
-		COMMENT.prototype.get = function (postid, page) {
+		COMMENT.prototype.get = function(postid, page) {
 			self.plugin.emit("comment.get", postid, page);
 			return self;
 		}
@@ -323,7 +325,7 @@
 		 * @param int {int} Comment ID
 		 * @return ZBP {ZBP}
 		 */
-		COMMENT.prototype.reply = function (id) {
+		COMMENT.prototype.reply = function(id) {
 			self.plugin.emit("comment.reply", id);
 			return self;
 		}
@@ -352,16 +354,16 @@
 			 * @type {object}
 			 */
 			formData = formData || {};
-			formData.action   = formData.action   || $("#inpId").parent("form").attr("action");
-			formData.postid   = formData.postid   || $("#inpId").val();
-			formData.verify   = formData.verify   || $("#inpVerify").val();
-			formData.name     = formData.name     || $("#inpName").val();
-			formData.email    = formData.email    || $("#inpEmail").val();
-			formData.content  = formData.content  || $("#txaArticle").val();
+			formData.action = formData.action || $("#inpId").parent("form").attr("action");
+			formData.postid = formData.postid || $("#inpId").val();
+			formData.verify = formData.verify || $("#inpVerify").val();
+			formData.name = formData.name || $("#inpName").val();
+			formData.email = formData.email || $("#inpEmail").val();
+			formData.content = formData.content || $("#txaArticle").val();
 			formData.homepage = formData.homepage || $("#inpHomePage").val();
-			formData.replyid  = formData.replyid  || $("#inpRevID").val();
-			formData.isajax   = formData.isajax   || true;
-			
+			formData.replyid = formData.replyid || $("#inpRevID").val();
+			formData.isajax = formData.isajax || true;
+
 			var error = {
 				no: 0,
 				msg: ""
@@ -371,11 +373,14 @@
 
 			if (error.no != 0) {
 				alert(error.msg);
-				try { console.log(formData); console.log("ERROR - " + error.msg)} catch (e) { /* do nothing */}
+				try {
+					console.log(formData);
+					console.log("ERROR - " + error.msg)
+				} catch (e) { /* do nothing */ }
 				self.plugin.emit("comment.posterror", error, formData);
 				return false;
-			} 
-			self.$.post(formData.action, formData, function (data, textStatus, jqXhr) {
+			}
+			self.$.post(formData.action, formData, function(data, textStatus, jqXhr) {
 				self.plugin.emit("comment.postsuccess", formData, data, textStatus, jqXhr);
 			});
 
@@ -399,7 +404,7 @@
 			return ZBP;
 		});
 	} else if (typeof define === "function" && define.cmd) {
-		define("zbp", [], function (require, exports, module) {
+		define("zbp", [], function(require, exports, module) {
 			module.exports = ZBP;
 		});
 	} else {
