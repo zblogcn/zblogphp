c18d1aa8220ad8fe054e396d92e3e84d471214f9
zsx <zsx@zsxsoft.com>
2015-06-20 15:09:28 +0800

    用Promise重写clinic前端代码

--- zb_users/plugin/clinic/main.php
+++ zb_users/plugin/clinic/main.php
@@ -11,7 +11,7 @@ $module = (isset($clinic->modules[$module]) ? $clinic->modules[$module] : NULL);
 $blogtitle = 'Z-BlogPHP诊断工具' . ($module ? ' - ' . $module['name'] : '');
 
 require $blogpath . 'zb_system/admin/admin_header.php';
-echo '<style type="text/css">tr{height: 32px}</style><script type="text/javascript" src="include/clinic.js"></script>';
+echo '<style type="text/css">tr{height: 32px}</style><script type="text/javascript" src="include/bluebird.min.js"></script>';
 require $blogpath . 'zb_system/admin/admin_top.php';
 ?>
 <div id="divMain">
