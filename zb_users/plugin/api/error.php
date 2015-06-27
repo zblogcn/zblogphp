13535139af4d463dfc2386cdcdea5a385acff7ac
zsx <zsx@zsxsoft.com>
2015-06-27 17:57:21 +0800

    分类获取

--- zb_users/plugin/api/error.php
+++ zb_users/plugin/api/error.php
@@ -9,11 +9,13 @@ class API_ERROR {
 	
 	const OK = 0;
 	const NON_ACCEPT = 1;
+	const MISSING_PARAMATER = 2;
 	const CUSTOM = 65535;
 
 	public static $errorCode = array(
 		self::OK => '',
-		self::NON_ACCEPT => '无法发送指定数据结构'
+		self::NON_ACCEPT => '无法发送指定数据结构',
+		self::MISSING_PARAMATER => '缺少必须参数'
 	);
 
 }
--- zb_users/plugin/api/route/category.php
+++ zb_users/plugin/api/route/category.php
@@ -1,5 +1,43 @@
 <?php
 function api_category_get_function() {
+	global $zbp;
+	$id = (int)API::$IO->id;
+	//if ($id === 0) API::$IO->end(API_ERROR::MISSING_PARAMATER);
+	//
+	$ret = array();
+	if ($id === 0) {
+		foreach ($zbp->categorysbyorder as $category) {
+			if ($category->ParentID == 0)
+				array_push($ret, return_category($category));
+		}
+	} else {
+		array_push($ret, return_category($zbp->categories[$id]));
+	}
+
+	API::$IO->categories = $ret;
 	API::$IO->end();
+}
+
+function return_category($category) {
+var_dump($category);exit;
+	$ret = array();
+	$ret['id'] = $category->ID;
+	$ret['order']  = $category->Order;
+	$ret['url'] = $category->Url;
+	$ret['name'] = $category->Name;
+	$ret['alias'] = $category->Alias;
+	$ret['article_count'] = $category->Count;
+	$ret['parent_id'] = $category->ParentID;
+	$ret['root_id'] = $category->RootID;
+	$ret['template'] = $category->Template;
+	$ret['log_template'] = $category->LogTemplate;
+	$ret['intro'] = $category->Intro;
+	$ret['categories'] = array();
+
+	foreach($category->SubCategorys as $subCategory) {
+		array_push($ret['categories'], return_category($subCategory));
+	}
+	
+	return $ret;
 }
 API::$Route->get('/category/', 'api_category_get_function');
