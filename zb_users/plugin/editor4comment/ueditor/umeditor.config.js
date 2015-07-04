fdd9850294244687789fdf03747af511cf8a71af
zsx <zsx@zsxsoft.com>
2015-07-04 16:23:45 +0800

    editor4comment插件移出版本库

--- zb_users/plugin/editor4comment/ueditor/umeditor.config.js
+++
@@ -1,8 +0,0 @@
-window.UMEDITOR_CONFIG = {
-        UMEDITOR_HOME_URL : bloghost
-        ,toolbar:
-            'source | bold italic underline forecolor ',
-            'link unlink | emotion drafts'
-        ]
-    };
-})();
