032d9dae6ade5e36743c5d190b944d59c51c5727
zsx <zsx@zsxsoft.com>
2015-07-18 13:29:53 +0800

    Merge remote-tracking branch 'origin/master' Conflicts: 	zb_system/function/lib/base.php

--- update/131111/zb_system/admin/admin_left.php
+++
@@ -1,7 +0,0 @@
-<aside class="left">
-  <ul id="leftmenu">
-<?php
-ResponseAdmin_LeftMenu()
-?>
-  </ul>
-</aside>
--- update/Release/zb_users/theme/WhitePage/compile/sidebar4.php
+++
@@ -1,3 +0,0 @@
-<?php  foreach ( $sidebar4 as $module) { ?>
-<?php  include $this->GetTemplate('module');  ?>
-<?php }   ?>
