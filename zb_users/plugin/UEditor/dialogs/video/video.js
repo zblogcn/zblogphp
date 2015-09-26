dfa2f95a09bfd8d9c0b2d35dec2a597229a93f1d
zsx <zsx@zsxsoft.com>
2015-09-27 00:37:35 +0800

    更新编辑器到1.4.3.1

--- zb_users/plugin/UEditor/dialogs/video/video.js
+++ zb_users/plugin/UEditor/dialogs/video/video.js
@@ -166,19 +166,28 @@
     }
     function convert_url(url){
         if ( !url ) return '';
-        url = utils.trim(url)
-            .replace(/v\.youku\.com\/v_show\/id_([\w\-=]+)\.html/i, 'player.youku.com/player.php/sid/$1/v.swf')
-            .replace(/(www\.)?youtube\.com\/watch\?v=([\w\-]+)/i, "www.youtube.com/v/$2")
-            .replace(/youtu.be\/(\w+)$/i, "www.youtube.com/v/$1")
-            .replace(/v\.ku6\.com\/.+\/([\w\.]+)\.html.*$/i, "player.ku6.com/refer/$1/v.swf")
-            .replace(/www\.56\.com\/u\d+\/v_([\w\-]+)\.html/i, "player.56.com/v_$1.swf")
-            .replace(/www.56.com\/w\d+\/play_album\-aid\-\d+_vid\-([^.]+)\.html/i, "player.56.com/v_$1.swf")
-            .replace(/v\.pps\.tv\/play_([\w]+)\.html.*$/i, "player.pps.tv/player/sid/$1/v.swf")
-            .replace(/www\.letv\.com\/ptv\/vplay\/([\d]+)\.html.*$/i, "i7.imgs.letv.com/player/swfPlayer.swf?id=$1&autoplay=0")
-            .replace(/www\.tudou\.com\/programs\/view\/([\w\-]+)\/?/i, "www.tudou.com/v/$1")
-            .replace(/v\.qq\.com\/cover\/[\w]+\/[\w]+\/([\w]+)\.html/i, "static.video.qq.com/TPout.swf?vid=$1")
-            .replace(/v\.qq\.com\/.+[\?\&]vid=([^&]+).*$/i, "static.video.qq.com/TPout.swf?vid=$1")
-            .replace(/my\.tv\.sohu\.com\/[\w]+\/[\d]+\/([\d]+)\.shtml.*$/i, "share.vrs.sohu.com/my/v.swf&id=$1");
+        url = utils.trim(url);
+        var originalUrl = url;
+        url.replace(/v\.youku\.com\/v_show\/id_([\w\-=]+)\.html/i, 'player.youku.com/player.php/sid/$1/v.swf')
+           .replace(/(www\.)?youtube\.com\/watch\?v=([\w\-]+)/i, "www.youtube.com/v/$2")
+           .replace(/youtu.be\/(\w+)$/i, "www.youtube.com/v/$1")
+           .replace(/v\.ku6\.com\/.+\/([\w\.]+)\.html.*$/i, "player.ku6.com/refer/$1/v.swf")
+           .replace(/www\.56\.com\/u\d+\/v_([\w\-]+)\.html/i, "player.56.com/v_$1.swf")
+           .replace(/www.56.com\/w\d+\/play_album\-aid\-\d+_vid\-([^.]+)\.html/i, "player.56.com/v_$1.swf")
+           .replace(/v\.pps\.tv\/play_([\w]+)\.html.*$/i, "player.pps.tv/player/sid/$1/v.swf")
+           .replace(/www\.letv\.com\/ptv\/vplay\/([\d]+)\.html.*$/i, "i7.imgs.letv.com/player/swfPlayer.swf?id=$1&autoplay=0")
+           .replace(/www\.tudou\.com\/programs\/view\/([\w\-]+)\/?/i, "www.tudou.com/v/$1")
+           .replace(/v\.qq\.com\/cover\/[\w]+\/[\w]+\/([\w]+)\.html/i, "static.video.qq.com/TPout.swf?vid=$1")
+           .replace(/v\.qq\.com\/.+[\?\&]vid=([^&]+).*$/i, "static.video.qq.com/TPout.swf?vid=$1")
+           .replace(/my\.tv\.sohu\.com\/[\w]+\/[\d]+\/([\d]+)\.shtml.*$/i, "share.vrs.sohu.com/my/v.swf&id=$1")
+
+
+        if (originalUrl == url) {
+            // 如果都不是的话，直接视为<video>
+            isModifyUploadVideo = true;
+        } else {
+            isModifyUploadVideo = false;
+        }
 
         return url;
     }
@@ -714,7 +723,9 @@
 
             uploader.on('uploadBeforeSend', function (file, data, header) {
                 //这里可以通过data对象添加POST参数
-                header['X_Requested_With'] = 'XMLHttpRequest';
+                if (actionUrl.toLowerCase().indexOf('jsp') != -1) {
+                    header['X_Requested_With'] = 'XMLHttpRequest';
+                }
             });
 
             uploader.on('uploadProgress', function (file, percentage) {
--- zb_users/plugin/UEditor/ueditor.config.php
+++ zb_users/plugin/UEditor/ueditor.config.php
@@ -3,7 +3,7 @@
 require '../../../zb_system/function/c_system_base.php';
 require '../../../zb_system/function/c_system_admin.php';
 
-header("Content-type: application/x-javascript; charset=utf-8"); 
+header("Content-type: application/x-javascript; charset=utf-8");
 //Somecode here.
 
 ob_clean();
@@ -13,48 +13,39 @@ $upload_dir = 'zb_users/upload/' . date('Y/m') . '/';
 $upload_path = '';
 $upload_dir = $blogpath . $upload_dir;
 
-$UEditor_lang=$lang['lang'];
-if(strpos($UEditor_lang,'en')===0)$UEditor_lang='en';
-$UEditor_lang=strtolower('\'' . $UEditor_lang . '\'');
-
-$UEditor_initialStyle='';
-if(isset($lang['font_family']))$UEditor_initialStyle=trim($lang['font_family']);
-if(!$UEditor_initialStyle)$UEditor_initialStyle='微软雅黑,宋体,Arial,Helvetica,sans-serif;';
-$UEditor_initialStyle=str_replace('"','&qout;',$UEditor_initialStyle);
-$UEditor_initialStyle='"body{font-size:14px;font-family:'.$UEditor_initialStyle.'}"';
-
-
 #echo '/*' . $upload_dir . '*/' ;
 
-$output_js="(function(){var URL;URL = '{$bloghost}zb_users/plugin/UEditor/';window.UEDITOR_CONFIG = {";
+$output_js = "(function(){var URL;URL = '{$bloghost}zb_users/plugin/UEditor/';window.UEDITOR_CONFIG = {";
 
 $array_config = array(
 	'UEDITOR_HOME_URL' => 'URL',
 	'HOST_URL' => 'bloghost',
-    'serverUrl' => 'URL + "php/controller.php"',
-	'toolbars' => "[ [ 'source', '|', 'undo', 'redo', '|', 'bold', 'italic', 'underline', 'strikethrough', 'superscript', 'subscript','forecolor', 'backcolor', '|', 'insertorderedlist', 'insertunorderedlist','indent', 'justifyleft', 'justifycenter', 'justifyright','|', 'removeformat','formatmatch','autotypeset', 'searchreplace','pasteplain'],[ 'fontfamily', 'fontsize','|', 'emotion','link','music','insertimage','scrawl','insertvideo', 'attachment','spechars','|', 'map', 'gmap','|', "
-				  . ($zbp->option['ZC_SYNTAXHIGHLIGHTER_ENABLE']?"'insertcode',":'')
-				  . "'blockquote', 'wordimage','inserttable', 'horizontal','fullscreen']]",
-	'shortcutMenu' => "['fontfamily', 'fontsize', 'bold', 'italic', 'underline', 'forecolor', 'backcolor', 'insertorderedlist', 'insertunorderedlist']",
+	'serverUrl' => 'URL + "php/controller.php"',
+	'toolbars' => "[ " .
+	"[ 'source', '|', 'undo', 'redo', '|', 'bold', 'italic', 'underline', 'strikethrough', 'superscript', 'subscript','forecolor', 'backcolor', '|', " .
+	"'insertorderedlist', 'insertunorderedlist','indent', 'justifyleft', 'justifycenter', 'justifyright','|', 'removeformat','formatmatch','autotypeset', 'pasteplain'], " .
+	"['paragraph', 'fontfamily', 'fontsize','|', 'emotion','link','music','insertimage','scrawl','insertvideo', 'attachment','spechars', 'map','|', "
+	. ($zbp->option['ZC_SYNTAXHIGHLIGHTER_ENABLE'] ? "'insertcode'," : '')
+	. "'blockquote', 'wordimage','inserttable', 'horizontal','fullscreen']]",
+	'shortcutMenu' => "['fontfamily', 'fontsize', 'bold', 'italic', 'underline', 'forecolor', 'backcolor']",
 	'maximumWords' => 1000000000,
+	'wordCountMsg' => '"当前已输入 {#count} 个字符" ',
 	'initialContent' => '"<p></p>"',
-	'initialStyle' => $UEditor_initialStyle,
+	'initialStyle' => '"body{font-size:14px;font-family:微软雅黑,宋体,Arial,Helvetica,sans-serif;}"',
 	'wordCount' => 'true',
 	'elementPathEnabled' => 'true',
 	'initialFrameHeight' => '300',
 	'toolbarTopOffset' => '200',
-	'sourceEditor' => '\''.($zbp->option['ZC_CODEMIRROR_ENABLE']?'codemirror':'textarea').'\'',
+	'sourceEditor' => '\'' . ($zbp->option['ZC_CODEMIRROR_ENABLE'] ? 'codemirror' : 'textarea') . '\'',
 	'theme' => '"default"',
-    'themePath' => 'URL +"themes/"',
-	'lang' => $UEditor_lang,
+	'themePath' => 'URL +"themes/"',
+	'lang' => '\'zh-cn\'',
 	'langPath' => 'URL+"lang/"',
 	'codeMirrorJsUrl' => 'URL+ "third-party/codemirror/codemirror.js"',
 	'codeMirrorCssUrl' => 'URL+ "third-party/codemirror/codemirror.css"',
 	"maxUpFileSize" => $zbp->option['ZC_UPLOAD_FILESIZE'],
 	"allowDivTransToP" => 'false',
-	'catchRemoteImageEnable' => 'false'
 );
-
 
 foreach ($array_config as $key => $value) {
 	$output_js .= '"' . $key . '":' . $value . ',';
@@ -62,7 +53,6 @@ foreach ($array_config as $key => $value) {
 
 $output_js .= '"zb_full":""};';
 $output_js .= '})();';
-
 
 //Code here
 echo $output_js;
