067c818cffc41dcb43f4cd18c8bf0e306bd28922
zsx <zsx@zsxsoft.com>
2016-02-02 16:44:15 +0800

    Fix wearingtheme's bug

--- zb_users/plugin/wearingtheme/include.php
+++ zb_users/plugin/wearingtheme/include.php
@@ -6,61 +6,72 @@ function ActivePlugin_wearingtheme() {
 }
 
 function wearingtheme_index_begin() {
-	
+
 	global $zbp;
 	global $usersdir;
 
 	$app = new App;
 	$theme = GetVars('theme', 'GET');
-	if ($theme == '') $theme = GetVars('theme', 'COOKIE');
+	if ($theme == '') {
+		$theme = GetVars('theme', 'COOKIE');
+	}
+
 	$dir = $usersdir . 'theme/' . $theme;
-	
-	if ($theme == '') exit;
-	if (!is_dir($dir)) exit;
-	if(!$app->LoadInfoByXml('theme', $theme)) exit;	
-	
+
+	if ($theme == '') {
+		return;
+	}
+	if (!is_dir($dir)) {
+		return;
+	}
+
+	if (!$app->LoadInfoByXml('theme', $theme)) {
+		return
+	}
+
 	$zbp->Config('system')->ZC_BLOG_THEME = $theme;
 	$zbp->option['ZC_BLOG_THEME'] = $theme;
 	$zbp->activeapps[0] = $theme;
-	
+
 	foreach ($app->GetCssFiles() as $key => $value) {
 		$value = basename($value, '.css');
 		$zbp->Config('system')->ZC_BLOG_CSS = $value;
 		$zbp->option['ZC_BLOG_CSS'] = $value;
 		break;
 	}
-				
+
 	if (is_readable($filename = $dir . '/include.php')) {
 		require $filename;
 		if (isset($GLOBALS['plugins'][$theme])) {
 			$func_name = $GLOBALS['plugins'][$theme];
-			if(function_exists($func_name))
+			if (function_exists($func_name)) {
 				$func_name();
+			}
+
 		}
 	}
-	
+
 	$zbp->LoadTemplate();
 	$zbp->MakeTemplatetags();
 	$zbp->template = $zbp->PrepareTemplate();
-	
-	setcookie('theme', $theme, time() + 24*60*60);
-	
+
+	setcookie('theme', $theme, time() + 24 * 60 * 60);
+
 	/*
 		TODO:
-		
+
 		一、安全问题：
 			1. 主题任意函数内包含危害服务器安全的代码
 			2. 主题任意函数内包含危害其它主题的代码（比如new ZipArchive然后POST到新的服务器）
-			
+
 		二、主题从APP上传到theme
 	*/
-	
+
 }
 
 function InstallPlugin_wearingtheme() {
-	
-}
 
+}
 
 function UninstallPlugin_wearingtheme() {
 
