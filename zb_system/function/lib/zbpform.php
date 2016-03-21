bc14e581d03ea37db0c4222d26c9d9c2459eb59b
未寒 <involvements77@gmail.com>
2016-03-21 22:13:06 +0800

    更新zbpform

--- zb_system/function/lib/zbpform.php
+++ zb_system/function/lib/zbpform.php
@@ -10,28 +10,38 @@
 
 /**用法
 zbpform::radio('aaaa',array('选项1', '选项2'));
+zbpform::zbradio('aaaa',array('选项1', '选项2'),1);
 zbpform::select('aaaa',array('选项1', '选项2'));
-zbpform::zbcheckbox('aaaa',array('选项1', '选项2'),1);
 zbpform::checkbox('aaaa',array('选项1', '选项2'),1);
 zbpform::text('aaaa','文本框');
 zbpform::hidden('aaaa','文本框');
 zbpform::textarea('aaaa','多行文本');
 zbpform::password('aaaa','文本框');
-*/
+ */
 class ZbpForm {
 
 	public static function radio($name, $array = array('否', '是'), $checkedkey = 0) {
 		$s = '';
-		foreach((array)$array as $k=>$v) {
+		foreach ((array) $array as $k => $v) {
 			$checked = $k == $checkedkey ? ' checked="checked"' : '';
 			$s .= "<input type=\"radio\" name=\"$name\" id=\"$name\" class=\"$name\" value=\"$k\"$checked />$v\r\n";
 		}
 		echo $s;
 	}
 
+	public static function zbradio($name, $ischecked = 0, $array = array('否', '是')) {
+		$s = '';
+		//$checkedtext = $array[$ischecked];
+		$s .= "<input name=\"$name\" id=\"$name\" class=\"$name checkbox\" style=\"display:none;\" type=\"text\" value=\"$ischecked\">\r\n";
+		echo $s;
+	}
+
 	public static function select($name, $array, $checkedkey = 0, $change = '') {
-		if(empty($array)) return ;
-		$onchange = !empty($change) ? ' onchange="'.$change.'"' : '';
+		if (empty($array)) {
+			return;
+		}
+
+		$onchange = !empty($change) ? ' onchange="' . $change . '"' : '';
 		$s = "<select name=\"$name\" id=\"$name\" class=\"$name\"$onchange> \r\n";
 		$s .= self::options($array, $checkedkey);
 		$s .= "</select> \r\n";
@@ -40,7 +50,7 @@ class ZbpForm {
 
 	public static function options($array, $checkedkey = 0) {
 		$s = '';
-		foreach((array)$array as $k=>$v) {
+		foreach ((array) $array as $k => $v) {
 			$checked = $k == $checkedkey ? ' selected="selected"' : '';
 			$s .= "<option value=\"$k\"$checked>$v</option> \r\n";
 		}
@@ -51,15 +61,7 @@ class ZbpForm {
 		$s = '';
 		$checked = $ischecked ? ' checked="checked"' : '';
 		$checkedtext = $array[$ischecked];
-		$s .= "<input type=\"checkbox\" name=\"$name\" id=\"$name\" class=\"$name\" value=\"1\"$checked />$checkedtext \r\n";
-		echo $s;
-	}
-
-	public static function zbcheckbox($name, $array, $ischecked = 0) {
-		$s = '';
-		$checked = $ischecked ? ' checked="checked"' : '';
-		$checkedtext = $array[$ischecked];
-		$s .= "<input name=\"$name\" id=\"$name\" class=\"$name checkbox\" style=\"display:none;\" type=\"text\" value=\"1\"$checked >$checkedtext \r\n";
+		$s .= "<input type=\"checkbox\" name=\"$name\" id=\"$name\" class=\"$name\" value=\"$ischecked\"$checked />$checkedtext \r\n";
 		echo $s;
 	}
 
@@ -73,7 +75,7 @@ class ZbpForm {
 		echo $s;
 	}
 
-	public static function textarea($name, $value, $width = 250,  $height = 150) {
+	public static function textarea($name, $value, $width = 250, $height = 100) {
 		$s = "<textarea name=\"$name\" id=\"$name\" class=\"$name\" style=\"width: {$width}px; height: {$height}px;\">$value</textarea>\r\n";
 		echo $s;
 	}
