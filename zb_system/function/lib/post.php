c2d92f0c079fbf420175298029567e2c5d35a09d
zsx <zsx@zsxsoft.com>
2016-05-21 19:13:33 +0800

    格式化post.php代码

--- zb_system/function/lib/post.php
+++ zb_system/function/lib/post.php
@@ -82,40 +82,40 @@ class Post extends Base {
 	public function __set($name, $value) {
 		global $zbp;
 		switch ($name) {
-			case 'Category':
-			case 'Author':
-			case 'TypeName':
-			case 'Url':
-			case 'Tags':
-			case 'TagsName':
-			case 'TagsCount':
-			case 'CommentPostUrl':
-			case 'Prev':
-			case 'Next':
-			case 'RelatedList':
-				return null;
-				break;
-			case 'Template':
-				if ($value == $zbp->GetPostType_Template($this->Type)) {
-					$value = '';
-				}
+		case 'Category':
+		case 'Author':
+		case 'TypeName':
+		case 'Url':
+		case 'Tags':
+		case 'TagsName':
+		case 'TagsCount':
+		case 'CommentPostUrl':
+		case 'Prev':
+		case 'Next':
+		case 'RelatedList':
+			return null;
+			break;
+		case 'Template':
+			if ($value == $zbp->GetPostType_Template($this->Type)) {
+				$value = '';
+			}
 
-				return $this->data[$name] = $value;
-				break;
-			case 'TopType':
-				if ($value == 'global' || $value == 'category') {
-					$this->Metas->toptype = $value;
-				} elseif ($value == '' || $value == null) {
-					$this->Metas->Del('toptype');
-				} else {
-					$this->Metas->toptype = 'index';
-				}
+			return $this->data[$name] = $value;
+			break;
+		case 'TopType':
+			if ($value == 'global' || $value == 'category') {
+				$this->Metas->toptype = $value;
+			} elseif ($value == '' || $value == null) {
+				$this->Metas->Del('toptype');
+			} else {
+				$this->Metas->toptype = 'index';
+			}
 
-				return null;
-				break;
-			default:
-				parent::__set($name, $value);
-				break;
+			return null;
+			break;
+		default:
+			parent::__set($name, $value);
+			break;
 		}
 	}
 
@@ -126,132 +126,132 @@ class Post extends Base {
 	public function __get($name) {
 		global $zbp;
 		switch ($name) {
-			case 'Category':
-				return $zbp->GetCategoryByID($this->CateID);
-				break;
-			case 'Author':
-				return $zbp->GetMemberByID($this->AuthorID);
-				break;
-			case 'StatusName':
-				return $zbp->lang['post_status_name'][$this->Status];
-				break;
-			case 'Url':
-				foreach ($GLOBALS['hooks']['Filter_Plugin_Post_Url'] as $fpname => &$fpsignal) {
-					$fpsignal = PLUGIN_EXITSIGNAL_NONE;
-					$fpreturn = $fpname($this);
-					if ($fpsignal == PLUGIN_EXITSIGNAL_RETURN) {return $fpreturn;	}
-				}
-				$u = new UrlRule($zbp->GetPostType_UrlRule($this->Type));
-				$u->Rules['{%id%}'] = $this->ID;
-				if ($this->Alias) {
-					$u->Rules['{%alias%}'] = $this->Alias;
+		case 'Category':
+			return $zbp->GetCategoryByID($this->CateID);
+			break;
+		case 'Author':
+			return $zbp->GetMemberByID($this->AuthorID);
+			break;
+		case 'StatusName':
+			return $zbp->lang['post_status_name'][$this->Status];
+			break;
+		case 'Url':
+			foreach ($GLOBALS['hooks']['Filter_Plugin_Post_Url'] as $fpname => &$fpsignal) {
+				$fpsignal = PLUGIN_EXITSIGNAL_NONE;
+				$fpreturn = $fpname($this);
+				if ($fpsignal == PLUGIN_EXITSIGNAL_RETURN) {return $fpreturn;}
+			}
+			$u = new UrlRule($zbp->GetPostType_UrlRule($this->Type));
+			$u->Rules['{%id%}'] = $this->ID;
+			if ($this->Alias) {
+				$u->Rules['{%alias%}'] = $this->Alias;
+			} else {
+				if ($zbp->option['ZC_POST_ALIAS_USE_ID_NOT_TITLE'] == false) {
+					$u->Rules['{%alias%}'] = rawurlencode($this->Title);
 				} else {
-					if ($zbp->option['ZC_POST_ALIAS_USE_ID_NOT_TITLE'] == false) {
-						$u->Rules['{%alias%}'] = rawurlencode($this->Title);
-					} else {
-						$u->Rules['{%alias%}'] = $this->ID;
-					}
+					$u->Rules['{%alias%}'] = $this->ID;
 				}
-				$u->Rules['{%year%}'] = $this->Time('Y');
-				$u->Rules['{%month%}'] = $this->Time('m');
-				$u->Rules['{%day%}'] = $this->Time('d');
-				if ($this->Category->Alias) {
-					$u->Rules['{%category%}'] = $this->Category->Alias;
-				} else {
-					$u->Rules['{%category%}'] = rawurlencode($this->Category->Name);
-				}
-				if ($this->Author->Alias) {
-					$u->Rules['{%author%}'] = $this->Author->Alias;
-				} else {
-					$u->Rules['{%author%}'] = rawurlencode($this->Author->Name);
-				}
-				return $u->Make();
-				break;
-			case 'Tags':
-				return $zbp->LoadTagsByIDString($this->Tag);
-				break;
-			case 'TagsCount':
-				return substr_count($this->Tag, '{');
-				break;
-			case 'TagsName':
-				return $this->TagsToNameString();
-			case 'Template':
-				$value = $this->data[$name];
+			}
+			$u->Rules['{%year%}'] = $this->Time('Y');
+			$u->Rules['{%month%}'] = $this->Time('m');
+			$u->Rules['{%day%}'] = $this->Time('d');
+			if ($this->Category->Alias) {
+				$u->Rules['{%category%}'] = $this->Category->Alias;
+			} else {
+				$u->Rules['{%category%}'] = rawurlencode($this->Category->Name);
+			}
+			if ($this->Author->Alias) {
+				$u->Rules['{%author%}'] = $this->Author->Alias;
+			} else {
+				$u->Rules['{%author%}'] = rawurlencode($this->Author->Name);
+			}
+			return $u->Make();
+			break;
+		case 'Tags':
+			return $zbp->LoadTagsByIDString($this->Tag);
+			break;
+		case 'TagsCount':
+			return substr_count($this->Tag, '{');
+			break;
+		case 'TagsName':
+			return $this->TagsToNameString();
+		case 'Template':
+			$value = $this->data[$name];
+			if ($value == '') {
+				$value = GetValueInArray($this->Category->GetData(), 'LogTemplate');
 				if ($value == '') {
-					$value = GetValueInArray($this->Category->GetData(), 'LogTemplate');
-					if ($value == '') {
-						$value = $zbp->GetPostType_Template($this->Type);
-					}
+					$value = $zbp->GetPostType_Template($this->Type);
 				}
-				return $value;
-			case 'CommentPostUrl':
-				foreach ($GLOBALS['hooks']['Filter_Plugin_Post_CommentPostUrl'] as $fpname => &$fpsignal) {
-					$fpreturn = $fpname($this);
-					if ($fpsignal == PLUGIN_EXITSIGNAL_RETURN) {$fpsignal = PLUGIN_EXITSIGNAL_NONE;return $fpreturn;	}
-				}
-				//$key = '&amp;key=' . md5($zbp->guid . $this->ID . date('Y-m-d'));
-				$key = '&amp;key=' . md5($zbp->guid . $this->ID . $this->CommNums . date('Y-m-d') . $_SERVER["REMOTE_ADDR"] . $_SERVER["HTTP_USER_AGENT"]);
-				return $zbp->host . 'zb_system/cmd.php?act=cmt&amp;postid=' . $this->ID . $key;
-				break;
-			case 'ValidCodeUrl':
-				return $zbp->validcodeurl . '?id=cmt';
-				break;
-			case 'Prev':
-				if ($this->_prev !== '') {
-					return $this->_prev;
-				}
-
-				$articles = $zbp->GetPostList(
-					array('*'),
-					array(array('=', 'log_Type', 0), array('=', 'log_Status', 0), array('<', 'log_PostTime', $this->PostTime)),
-					array('log_PostTime' => 'DESC'),
-					array(1),
-					null
-				);
-				if (count($articles) == 1) {
-					$this->_prev = $articles[0];
-				} else {
-					$this->_prev = null;
-				}
+			}
+			return $value;
+		case 'CommentPostUrl':
+			foreach ($GLOBALS['hooks']['Filter_Plugin_Post_CommentPostUrl'] as $fpname => &$fpsignal) {
+				$fpreturn = $fpname($this);
+				if ($fpsignal == PLUGIN_EXITSIGNAL_RETURN) {$fpsignal = PLUGIN_EXITSIGNAL_NONE;return $fpreturn;}
+			}
+			//$key = '&amp;key=' . md5($zbp->guid . $this->ID . date('Y-m-d'));
+			$key = '&amp;key=' . md5($zbp->guid . $this->ID . $this->CommNums . date('Y-m-d') . $_SERVER["REMOTE_ADDR"] . $_SERVER["HTTP_USER_AGENT"]);
+			return $zbp->host . 'zb_system/cmd.php?act=cmt&amp;postid=' . $this->ID . $key;
+			break;
+		case 'ValidCodeUrl':
+			return $zbp->validcodeurl . '?id=cmt';
+			break;
+		case 'Prev':
+			if ($this->_prev !== '') {
 				return $this->_prev;
-				break;
-			case 'Next':
-				if ($this->_next !== '') {
-					return $this->_next;
-				}
+			}
 
-				$articles = $zbp->GetPostList(
-					array('*'),
-					array(array('=', 'log_Type', 0), array('=', 'log_Status', 0), array('>', 'log_PostTime', $this->PostTime)),
-					array('log_PostTime' => 'ASC'),
-					array(1),
-					null
-				);
-				if (count($articles) == 1) {
-					$this->_next = $articles[0];
-				} else {
-					$this->_next = null;
-				}
+			$articles = $zbp->GetPostList(
+				array('*'),
+				array(array('=', 'log_Type', 0), array('=', 'log_Status', 0), array('<', 'log_PostTime', $this->PostTime)),
+				array('log_PostTime' => 'DESC'),
+				array(1),
+				null
+			);
+			if (count($articles) == 1) {
+				$this->_prev = $articles[0];
+			} else {
+				$this->_prev = null;
+			}
+			return $this->_prev;
+			break;
+		case 'Next':
+			if ($this->_next !== '') {
 				return $this->_next;
-				break;
-			case 'RelatedList':
-				foreach ($GLOBALS['hooks']['Filter_Plugin_Post_RelatedList'] as $fpname => &$fpsignal) {
-					$fpreturn = $fpname($this);
-					if ($fpsignal == PLUGIN_EXITSIGNAL_RETURN) {$fpsignal = PLUGIN_EXITSIGNAL_NONE;return $fpreturn;	}
-				}
-				return GetList($zbp->option['ZC_RELATEDLIST_COUNT'], null, null, null, null, null, array('is_related' => $this->ID));
-			case 'TopType':
-				$toptype = $this->Metas->toptype;
-				if ($this->IsTop == true && $toptype == null) {
-					$toptype = 'index';
-				}
+			}
 
-				return $toptype;
-			case 'TypeName':
-				return $zbp->GetPostType_Name($this->Type);
-			default:
-				return parent::__get($name);
-				break;
+			$articles = $zbp->GetPostList(
+				array('*'),
+				array(array('=', 'log_Type', 0), array('=', 'log_Status', 0), array('>', 'log_PostTime', $this->PostTime)),
+				array('log_PostTime' => 'ASC'),
+				array(1),
+				null
+			);
+			if (count($articles) == 1) {
+				$this->_next = $articles[0];
+			} else {
+				$this->_next = null;
+			}
+			return $this->_next;
+			break;
+		case 'RelatedList':
+			foreach ($GLOBALS['hooks']['Filter_Plugin_Post_RelatedList'] as $fpname => &$fpsignal) {
+				$fpreturn = $fpname($this);
+				if ($fpsignal == PLUGIN_EXITSIGNAL_RETURN) {$fpsignal = PLUGIN_EXITSIGNAL_NONE;return $fpreturn;}
+			}
+			return GetList($zbp->option['ZC_RELATEDLIST_COUNT'], null, null, null, null, null, array('is_related' => $this->ID));
+		case 'TopType':
+			$toptype = $this->Metas->toptype;
+			if ($this->IsTop == true && $toptype == null) {
+				$toptype = 'index';
+			}
+
+			return $toptype;
+		case 'TypeName':
+			return $zbp->GetPostType_Name($this->Type);
+		default:
+			return parent::__get($name);
+			break;
 		}
 
 	}
