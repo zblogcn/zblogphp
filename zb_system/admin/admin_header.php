e1d9f475553f05926e3bdc8683580e2264032aa5
zsx <zsx@zsxsoft.com>
2016-06-02 14:18:03 +0800

    后台取消对IE8支持，处理样式
    同

--- zb_system/admin/admin_header.php
+++ zb_system/admin/admin_header.php
@@ -1,21 +1,18 @@
 <!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $lang['lang']?>" lang="<?php echo $lang['lang']?>">
+<html lang="<?php echo $lang['lang_bcp47'] ?>">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta http-equiv="X-UA-Compatible" content="IE=EDGE" />
-<meta name="generator" content="Z-BlogPHP <?php echo $option['ZC_BLOG_VERSION']?>" />
+<meta name="generator" content="Z-BlogPHP <?php echo ZC_VERSION_DISPLAY ?>" />
 <meta name="robots" content="none" />
-<title><?php echo $blogname . '-' . $blogtitle?></title>
-<link href="<?php echo $bloghost?>zb_system/css/admin2.css" rel="stylesheet" type="text/css" />
-<script src="<?php echo $bloghost?>zb_system/script/jquery-1.11.2.min.js" type="text/javascript"></script>
-<script src="<?php echo $bloghost?>zb_system/script/zblogphp.js" type="text/javascript"></script>
-<script src="<?php echo $bloghost?>zb_system/script/c_admin_js_add.php" type="text/javascript"></script>
-<script src="<?php echo $bloghost?>zb_system/script/jquery-ui.custom.min.js" type="text/javascript"></script>
-<link rel="stylesheet" type="text/css" href="<?php echo $bloghost?>zb_system/css/jquery-ui.custom.css"/>
-<!--[if lt IE 9]>
-<link rel="stylesheet" href="<?php echo $bloghost?>zb_system/css/jquery-ui.ie.css">
-<![endif]-->
-<script>if (!window.bloghost) alert("<?php echo $lang['msg']['error_load_js'];?>");</script>
+<title><?php echo $blogname . '-' . $blogtitle ?></title>
+<link href="<?php echo $bloghost ?>zb_system/css/admin2.css" rel="stylesheet" type="text/css" />
+<link rel="stylesheet" type="text/css" href="<?php echo $bloghost ?>zb_system/css/jquery-ui.custom.css"/>
+<script src="<?php echo $bloghost ?>zb_system/script/jquery-1.11.2.min.js" type="text/javascript"></script>
+<script src="<?php echo $bloghost ?>zb_system/script/zblogphp.js" type="text/javascript"></script>
+<script src="<?php echo $bloghost ?>zb_system/script/c_admin_js_add.php" type="text/javascript"></script>
+<script src="<?php echo $bloghost ?>zb_system/script/jquery-ui.custom.min.js" type="text/javascript"></script>
+<script>if (!window.bloghost) alert("<?php echo $lang['msg']['error_load_js']; ?>");</script>
 <?php
 foreach ($GLOBALS['hooks']['Filter_Plugin_Admin_Header'] as $fpname => &$fpsignal) {$fpname();}
 ?>
