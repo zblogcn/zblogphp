b99d9d752c7d0d27178d8639d3c9adc00c93106b
zsx <zsx@zsxsoft.com>
2016-06-13 17:55:18 +0800

    更新HTML5CSS3主题写法

--- zb_users/theme/HTML5CSS3/template/comment.php
+++ zb_users/theme/HTML5CSS3/template/comment.php
@@ -2,7 +2,7 @@
 	<figure><img src="{$comment.Author.Avatar}" alt="{$comment.Author.StaticName}" /></figure>
     <cite><b><a href="{$comment.Author.HomePage}" rel="nofollow" target="_blank">{$comment.Author.StaticName}</a></b> <time>发表时间 {$comment.Time()}</time></cite>
 	<q>{$comment.Content}
-&nbsp;&nbsp;<i class="revertcomment"><a href="#reply" onclick="RevertComment('{$comment.ID}')">回复</a></i>
+&nbsp;&nbsp;<i class="revertcomment"><a href="#reply" onclick="zbp.comment.reply('{$comment.ID}')">回复</a></i>
 {foreach $comment.Comments as $comment}
 {template:comment}
 {/foreach}
