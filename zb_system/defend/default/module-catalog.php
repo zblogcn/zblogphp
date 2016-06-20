37cda3e2aaf52d50e798c02454a58c92aa185e20
zx.asd <rainbowsoft@gmail.com>
2016-06-21 00:55:51 +0800

    catalog模块模版制作；

--- zb_system/defend/default/module-catalog.php
+++ zb_system/defend/default/module-catalog.php
@@ -1,67 +1,44 @@
-
+{$i = $maxLi}
+{$j = 0}
+{$s = ''}
 {if $style==2}
+    {foreach $catelogs as $catalog}
+        {if $catalog->Level == 0}
+            {$s = $s . '<li class="li-cate"><a href="' . $catalog->Url . '">' . $catalog->Name . '</a><!--' . $catalog->ID . 'begin--><!--' . $catalog->ID . 'end--></li>'}
+        {/if}
+    {/for}
 
+    {for $i = 1; $i <= 3; $i++}
+        {* 此处逻辑仍要继续修改 *}
+        {foreach $catelogs as $catalog}
+            {if $catalog->Level == $i}
+                {$s = str_replace('<!--' . $catalog->ParentID . 'end-->', '<li class="li-subcate"><a href="' . $value->Url . '">' . $catalog->Name . '</a><!--' . $catalog->ID . 'begin--><!--' . $catalog->ID . 'end--></li><!--' . $catalog->ParentID . 'end-->', $s)}
+            {/if}
+        {/for}
+    {/for}
+
+    {foreach $catelogs as $catalog}
+        {$s = str_replace('<!--' . $catalog->ID . 'begin--><!--' . $catalog->ID . 'end-->', '', $s)}
+    {/for}
+    {foreach $catelogs as $catalog}
+        {$s = str_replace('<!--' . $catalog->ID . 'begin-->', '<ul class="ul-subcates">', $s)}
+        {$s = str_replace('<!--' . $catalog->ID . 'end-->', '</ul>', $s)}
+    {/for}
+    {$s}
 {elseif $style==1}
-
+    {foreach $catelogs as $catalog}
+        <li>{$catalog->Symbol}<a href="{$catalog.Url}">{$catalog.Title}</a></li>
+        {$j =$j + 1}
+        {if $i != 0 && $j >= $i}
+            {php}break;{/php}
+        {/if}
+    {/for}
 {else}
-
+    {foreach $catelogs as $catalog}
+        <li><a href="{$catalog.Url}">{$catalog.Title}</a></li>
+        {$j =$j + 1}
+        {if $i != 0 && $j >= $i}
+            {php}break;{/php}
+        {/if}
+    {/for}
 {/if}
-
-
-        $i = $zbp->modulesbyfilename['catalog']->MaxLi;
-        $j = 0;
-        if ($zbp->option['ZC_MODULE_CATALOG_STYLE'] == '2') {
-            foreach ($zbp->categorysbyorder as $key => $value) {
-                if ($value->Level == 0) {
-                    $s .= '<li class="li-cate"><a href="' . $value->Url . '">' . $value->Name . '</a><!--' . $value->ID . 'begin--><!--' . $value->ID . 'end--></li>';
-                }
-                $j += 1;
-                if ($i != 0 && $j >= $i) {
-                    break;
-                }
-            }
-
-            for ($i = 1; $i <= 3; $i++) {
-                // 此处逻辑仍要继续修改
-                foreach ($zbp->categorysbyorder as $key => $value) {
-                    if ($value->Level == $i) {
-                        $s = str_replace('<!--' . $value->ParentID . 'end-->', '<li class="li-subcate"><a href="' . $value->Url . '">' . $value->Name . '</a><!--' . $value->ID . 'begin--><!--' . $value->ID . 'end--></li><!--' . $value->ParentID . 'end-->', $s);
-                    }
-                }
-            }
-
-            foreach ($zbp->categorysbyorder as $key => $value) {
-                $s = str_replace('<!--' . $value->ID . 'begin--><!--' . $value->ID . 'end-->', '', $s);
-            }
-            foreach ($zbp->categorysbyorder as $key => $value) {
-                $s = str_replace('<!--' . $value->ID . 'begin-->', '<ul class="ul-subcates">', $s);
-                $s = str_replace('<!--' . $value->ID . 'end-->', '</ul>', $s);
-            }
-
-        } elseif ($zbp->option['ZC_MODULE_CATALOG_STYLE'] == '1') {
-            foreach ($zbp->categorysbyorder as $key => $value) {
-                $s .= '<li>' . $value->Symbol . '<a href="' . $value->Url . '">' . $value->Name . '</a></li>';
-                $j += 1;
-                if ($i != 0 && $j >= $i) {
-                    break;
-                }
-
-            }
-        } else {
-            foreach ($zbp->categorysbyorder as $key => $value) {
-                $s .= '<li><a href="' . $value->Url . '">' . $value->Name . '</a></li>';
-                $j += 1;
-                if ($i != 0 && $j >= $i) {
-                    break;
-                }
-
-            }
-        }
-
-        return $s;
-
-
-
-{foreach $catelogs as $catalog}
-<li><a href="{$catalog.Url}">{$catalog.Title}</a></li>
-{/foreach}
