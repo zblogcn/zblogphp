6773f87fe7a09d73604c0873e0e0b939a16a6a57
zx.asd <rainbowsoft@gmail.com>
2016-07-01 22:14:05 +0800

    删除调试代码；

--- zb_system/function/c_system_event.php
+++ zb_system/function/c_system_event.php
@@ -2390,7 +2390,7 @@ function DelModule() {
 
     if (GetVars('source', 'GET') == 'theme') {
         $fn = GetVars('filename', 'GET');
-        if ($fn) {var_dump($zbp->GetModuleByFileName($fn));
+        if ($fn) {
             $mod = $zbp->GetModuleByFileName($fn);
             if ($mod->FileName == $fn) {
                 $mod->Del();
