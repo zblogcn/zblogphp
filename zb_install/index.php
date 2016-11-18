061702edf8a369830a464f02a1eec9106104ebfb
zsx <zsx@zsxsoft.com>
2016-11-18 21:57:53 +0800

    修正英语提示

--- zb_install/index.php
+++ zb_install/index.php
@@ -9,27 +9,17 @@
  *
  *****************************************************************************************************
  *    如果您看到了这个提示，那么我们很遗憾地通知您，您的空间不支持 PHP 。
- *    We regret to inform you that your web hosting not support PHP,
- *    and Z-BlogPHP CAN'T run on it if you see this prompt.
- *
  *    也就是说，您的空间可能是静态空间，或没有安装PHP，或没有为 Web 服务器打开 PHP 支持。
- *    It means that you may have a web hosting service supporting only static resources.
- *    Is PHP successfully installed on your server?
- *    Or, is HTTP Server configured correctly?
+ *    Sorry, PHP is not installed on your web hosting if you see this prompt.
+ *    Please check out the PHP configuration.
  *
  *    如您使用虚拟主机：
  *
  *        > 联系空间商，更换空间为支持 PHP 的空间。
  *        > Contact your service provider, and let them provice a new service which supports PHP.
  *
- *    如您使用 IIS，推荐您：
- *    Using IIS? Recommend:
- *
- *        > 下载并安装Z-Blog  > http://www.zblogcn.com/zblog/
- *        > Try Z-Blog > http://www.zblogcn.com/zblog/
- *
- *    如您使用其它 HTTP 服务器，推荐您：
- *    Using other HTTP Server? Recommend:
+ *    如您自行搭建服务器，推荐您：
+ *    Configuring manually? Recommend:
  *
  *        > 访问 PHP 官方网站获取安装帮助。
  *        > Visit PHP Official Website to get the documentation of installion and configuration.
@@ -765,7 +755,7 @@ function CheckServer() {
     if (function_exists("mb_language")) {
         $CheckResult['mbstring'][0] = mb_language();
         $CheckResult['mbstring'][1] = $CheckResult['mbstring'][0] ? bingo : error;
-    }    
+    }
     if (class_exists("PDO", false)) {
         if (extension_loaded('pdo_mysql')) {
             //$pdo = new PDO( 'mysql:');
