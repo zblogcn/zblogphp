a072a2afe731c9c4c6c29a702c623bb318a06905
月上之木 <movni@163.com>
2016-12-22 16:22:44 +0800

    更新linkmanage插件，勉强能用

--- zb_users/plugin/linkmanage/js.js
+++ zb_users/plugin/linkmanage/js.js
@@ -28,22 +28,41 @@ $(function() {
 			}
 	});
 });
+
+
+function edit_menu(id){
+	window.location.href="menuedit.php?id="+id;
+}
+
 function del_menu(id){
-	window.location.href="main.php?del="+id;
+	window.location.href="menuedit.php?del="+id;
 }
 
 function postsort(){
 	serialized = $('#menu-edit-body-content .nav-menu').nestedSortable('serialize',{ attribute:"sid"});
 	console.log(serialized);
 	$.post("save.php?type=sort", $("#menuName").serialize()+'&'+serialized , function(data) {
-		alert('保存成功');
-		return false;
+		//$( "#message" ).dialog();
+		//return false;
 	});
 }
 
 
 function del_link(id,menuid){
-	$.post("save.php?type=del_link", {"id":id,'menuid':menuid} , function(data) {if(data==1){alert('请先删除子链接再删除本链接');}else{window.location.href="main.php?id="+menuid;}return false;});
+	$.post(
+		"save.php?type=del_link", 
+		{
+			"id":id,'menuid':menuid
+		},
+		function(data) {
+			if(data==1) {
+				alert('请先删除子链接再删除本链接');
+			} else {
+				$('li[sid$='+id+']').remove();
+				//$( "#message" ).dialog();
+			}
+		return false;
+	});
 }
 
 function save_link(id){
@@ -56,7 +75,15 @@ function save_link(id){
 	//var img = $(input_name+"img]']").val();
 	var img = '';
 
-	$.post("save.php?type=save_link", { id:id,title:title,url:url,newtable:newtable,img:img} , function(data) {alert('保存成功');return false;});
+	$.post(
+		"save.php?type=save_link",
+		{
+			id:id,title:title,url:url,newtable:newtable,img:img
+		},
+		function(data) {
+			//$( "#message" ).dialog();
+			return false;
+		});
 }
 
 function add_menu(){
@@ -66,59 +93,80 @@ function add_menu(){
     });
 }
 
-function addsekectpartent(item,type,menuid){
+function add_link(item,type,menuid){
 	var foo = [];
-	links = $(item).parent().prev().find("select");
-	links.find(":selected").each(function(i, selected){
-	  foo[i] = {
-	  	"sid":new Date().getTime(),
-	  	"title":$(selected).text(),
-	  	"url":$(selected).val(),
-	  	"newtable":0,
-	  	"img":"",
-	  	"type":type,
-	  };
-	  create_item(foo[i],menuid)
-	});
-	$.post("save.php?type=menu", { id:foo[0].sid,title:foo[0].title,url:foo[0].url,newtable:foo[0].newtable,img:foo[0].img,type:foo[0].type} , function(data) {return false;});
-	console.log(foo);
+	if (type !== "custom"){
+		links = $(item).parent().prev().find("select");
+		links.find(":selected").each(function(i, selected){
+		  foo[i] = {
+		  	"id":new Date().getTime(),
+		  	"title":$(selected).text(),
+		  	"url":$(selected).val(),
+		  	"newtable":0,
+		  	"img":"",
+		  	"type":type,
+		  };
+		  create_item(foo[i],type,menuid);
+		});
+	} else {
+		foo[0] = {
+		  	"id":new Date().getTime(),
+		  	"title":"新链接",
+		  	"url":"http://",
+		  	"newtable":0,
+		  	"img":"",
+		  	"type":type,
+		}
+		create_item(foo[0],type,menuid)
+	}
+	//TODO
 }
 
-function create_item(item,menuid){
-	$('<li sid="menuItem_'+ item.sid +'">'+
+function create_item(item,type,menuid){
+	var display = "style='display: none;'";
+	if (type == "custom"){
+		display = "";
+	}
+
+	$('<li sid="menuItem_'+ item.id +'">'+
 '		<div class="menu-item-bar">'+
 '			<div class="menu-item-handle ui-sortable-handle">'+
-'				<span class="item-title"><span class="menu-item-title">'+ item.title +'</span> </span>'+
+'				<span class="item-title"><span class="menu-item-title">'+ item.title +'</span>'+
 '				<span class="item-controls"> '+
 '					<span class="item-type">'+ item.type +'</span>'+
 '					<span class="item-edit ui-icon ui-icon-triangle-1-n"></span>'+
 '				</span>'+
 '			</div>'+
 '		</div>'+
-'		<div class="menu-item-settings" style="display: none;">'+
+'		<div class="menu-item-settings form-horizontal" '+ display +'>'+
 '			<p class="link-p">'+
 '				<label class="link-edit" for="custom-menu-item-url">'+
 '					<span>URL</span>'+
-'					<input name="menu-item['+ item.sid +'][menu-item-url]" type="text" class="code menu-item-textbox custom-menu-item-url" value="'+ item.url +'">'+
+'					<input name="menu-item['+ item.id +'][menu-item-url]" type="text" class="code menu-item-textbox custom-menu-item-url" value="'+ item.url +'">'+
 '				</label>'+
 '			</p>'+
 '			<p class="link-p">'+
 '				<label class="link-edit" for="custom-menu-item-name">'+
-'					<span>链接文本</span>'+
-'					<input name="menu-item['+ item.sid +'][menu-item-title]" type="text" class="regular-text menu-item-textbox input-with-default-title custom-menu-item-url" value="'+ item.title +'">'+
+'					<span>描述</span>'+
+'					<input name="menu-item['+ item.id +'][menu-item-title]" type="text" class="regular-text menu-item-textbox input-with-default-title custom-menu-item-url" value="'+ item.title +'">'+
 '				</label>'+
 '			</p>'+
-'<p class="link-p">'+
-'											<label class="link-edit" for="custom-menu-item-name">'+
-'												<span>新窗口打开链接</span>'+
-'												<input type="text" name="menu-item['+ item.sid +'][menu-item-newtable]" class="checkbox" value="'+ item.newtable +'" style="display: none;">'+
-'											</label>'+
-'										</p>'+
+'			<p class="link-p">'+
+'				<label class="link-edit" for="custom-menu-item-name">'+
+'					<span>新窗口打开 </span>'+
+'					<input type="text" name="menu-item['+ item.id +'][menu-item-newtable]" class="checkbox" value="'+ item.newtable +'" style="display: none;">'+
+'				</label>'+
+'			</p>'+
 '			<p class="button-controls">'+
-'					<button class="ui-button-primary ui-button ui-widget ui-state-default ui-corner-all ui-button-text-only" role="button" aria-disabled="false"><span class="ui-button-text" onclick="del_link('+ item.sid +',\''+menuid+'\');return false;">删除链接</span></button>'+
-'					<button class="ui-button-primary ui-button ui-widget ui-state-default ui-corner-all ui-button-text-only" role="button" aria-disabled="false"><span class="ui-button-text" onclick="save_link('+ item.sid +');return false;">保存链接</span></button>'+
+'					<button class="ui-button-primary ui-button ui-widget ui-state-default ui-corner-all ui-button-text-only" role="button" aria-disabled="false"><span class="ui-button-text" onclick="del_link('+ item.id +',\''+menuid+'\');return false;">删除链接</span></button>'+
+'					<button class="ui-button-primary ui-button ui-widget ui-state-default ui-corner-all ui-button-text-only" role="button" aria-disabled="false"><span class="ui-button-text" onclick="save_link('+ item.id +');return false;">保存链接</span></button>'+
 '			</p>'+
 '		</div>'+
 '	</li>').appendTo("#menu-edit-body-content .nav-menu");
 
+	$.post("save.php?type=creat_link", item ,
+		function(data) {
+			console.log(data);
+			return false;
+	});
 }
---
+++ zb_users/plugin/linkmanage/menuedit.php
@@ -0,0 +1,229 @@
+<?php
+require '../../../zb_system/function/c_system_base.php';
+require '../../../zb_system/function/c_system_admin.php';
+$zbp->Load();
+$action = 'root';
+if (!$zbp->CheckRights($action)) {
+    $zbp->ShowError(6);
+    die();
+}
+if (!$zbp->CheckPlugin('linkmanage')) {
+    $zbp->ShowError(48);
+    die();
+}
+$Navs = linkmanageGetNav();
+global $sysMenu;
+$blogtitle = '导航菜单编辑';
+require $blogpath.'zb_system/admin/admin_header.php';
+require $blogpath.'zb_system/admin/admin_top.php';
+
+if (GetVars('del', 'GET')) {
+    linkmanage_deleteNav(GetVars('del', 'GET'));
+}
+
+if (GetVars('creat', 'POST') == 'new') {
+    linkmanage_creatNav(GetVars('id', 'POST'));
+}
+$menuID = GetVars('id');
+if (isset($menuID) && $menuID == '') {
+    unset($menuID);
+}
+
+?>
+<script type="text/javascript" src="jquery.mjs.nestedSortable.js"></script>
+<script type="text/javascript" src="js.js"></script>
+<link href="style.css" rel="stylesheet" type="text/css" />
+
+<div id="divMain">
+  <div class="divHeader"><?php echo $blogtitle; ?></div>
+ <div class="SubMenu"><?php linkmanage_SubMenu(1);?></div>
+  <div id="divMain2">
+
+<?php if (isset($menuID)) {
+    ?>
+	<div id="nav-menus-frame">
+		<div id="menu-settings">
+			<div class="clear"></div>
+			<div class="menu-settings-header">
+				<p>常用链接</p>
+			</div>
+			<div id="accordion" class="accordion-container">
+				  <div class="group">
+				    <h3>文章</h3>
+				    <div class="accordion-section-content">
+					    <div class="input-control select">
+							<select multiple="1" size="6" id="post">
+								<?php linkmanage_get_link('post');
+    ?>
+							</select>
+						</div>
+						<p class="button-controls">
+								<button class="ui-button-primary ui-button ui-widget ui-state-default ui-corner-all ui-button-text-only" role="button" aria-disabled="false" onclick="add_link(this,'post','<?php echo $menuID;?>')"><span class="ui-button-text">添加</span></button>
+						</p>
+				    </div>
+				  </div>
+
+				 <div class="group">
+				    <h3>页面</h3>
+				    <div class="accordion-section-content">
+				    	<div class="input-control select">
+							<select multiple="1" size="6" id="page">
+								<?php linkmanage_get_link('page');
+    ?>
+							</select>
+						</div>
+						<p class="button-controls">
+							<button class="ui-button-primary ui-button ui-widget ui-state-default ui-corner-all ui-button-text-only" role="button" aria-disabled="false" onclick="add_link(this,'page','<?php echo $menuID;?>')"><span class="ui-button-text">添加</span></button>
+						</p>
+					</div>
+				  </div>
+
+				  <div class="group">
+				    <h3>分类</h3>
+				    <div class="accordion-section-content">
+					    <div class="input-control select">
+							<select multiple="1" size="6" id="category">
+								<?php linkmanage_get_link('category');
+    ?>
+							</select>
+						</div>
+						<p class="button-controls">
+								<button class="ui-button-primary ui-button ui-widget ui-state-default ui-corner-all ui-button-text-only" role="button" aria-disabled="false" onclick="add_link(this,'category','<?php echo $menuID;?>')"><span class="ui-button-text">添加</span></button>
+						</p>
+				    </div>
+				  </div>
+
+				  <div class="group">
+				    <h3>标签</h3>
+				    <div class="accordion-section-content">
+					    <div class="input-control select">
+							<select multiple="1" size="6" id="tags">
+								<?php linkmanage_get_link('tags');
+    ?>
+							</select>
+						</div>
+						<p class="button-controls">
+								<button class="ui-button-primary ui-button ui-widget ui-state-default ui-corner-all ui-button-text-only" role="button" aria-disabled="false" onclick="add_link(this,'tags','<?php echo $menuID;?>')"><span class="ui-button-text">添加</span></button>
+						</p>
+				    </div>
+				  </div>
+			</div>
+		</div>
+
+		<div id="menu-management">
+			<div id="menu-manage">
+				<div class="menu-edit">
+					<form id="menuName" method="POST">
+					<input name="id" type="hidden" value="<?php echo $menuID;
+    ?>">
+					<div id="nav-menu-header">
+						<div class="major-publishing-actions">
+							<label class="menu-name-label howto open-label" for="menu-name">
+								<span>导航菜单名称(<?php echo $Navs['data'][$menuID]['id'];
+    ?>)：</span>
+								<input name="MenuName" id="menu-name" type="text" class="menu-name regular-text menu-item-textbox" title="在此输入名称" value="<?php echo $Navs['data'][$menuID]['name'];
+    ?>">
+							</label>
+							<div class="publishing-action">
+								<button  name="save_menu" id="save_menu_header"  class="ui-button-primary ui-button ui-widget ui-state-default ui-corner-all ui-button-text-only" onclick="postsort();return false;">保存更改</button>
+							</div><!-- END .publishing-action -->
+						</div><!-- END .major-publishing-actions -->
+					</div>
+					</form>
+					<div id="menu-edit-body">
+						<div id="menu-edit-body-content">
+							<ol class="nav-menu ui-sortable">
+							<?php
+	$html = '';
+    $Menus = linkmanageGetMenu();
+    $menuIDarray = json_decode($zbp->Config('linkmanage')->$menuID, true);
+    foreach ($menuIDarray as $key => $value) {
+        $menu = $Menus['ID'.$key];
+        $html_tmp = '
+			<li sid="menuItem_'.$menu['id'].'">
+				<div class="menu-item-bar">
+					<div class="menu-item-handle ui-sortable-handle">
+						<span class="item-title"><span class="menu-item-title">'.$menu['title'].'</span>
+						<span class="item-controls">
+							<span class="item-type">'.$menu['type'].'</span>
+							<span class="item-edit ui-icon ui-icon-triangle-1-n"></span>
+						</span>
+					</div>
+				</div>
+				<div class="menu-item-settings" style="display: none;">
+					<p class="link-p">
+						<label class="link-edit" for="custom-menu-item-url">
+							<span>URL</span>
+							<input name="menu-item['.$menu['id'].'][menu-item-url]" type="text" class="code menu-item-textbox custom-menu-item-url" value="'.$menu['url'].'">
+						</label>
+					</p>
+					<p class="link-p">
+						<label class="link-edit" for="custom-menu-item-name">
+							<span>描述</span>
+							<input name="menu-item['.$menu['id'].'][menu-item-title]" type="text" class="regular-text menu-item-textbox input-with-default-title custom-menu-item-url" value="'.$menu['title'].'">
+						</label>
+					</p>
+					<p class="link-p">
+						<label class="link-edit" for="custom-menu-item-name">
+							<span>新窗口打开 </span>
+							<input type="text" name="menu-item['.$menu['id'].'][menu-item-newtable]" class="checkbox" value="'.$menu['newtable'].'" style="display: none;">
+						</label>
+					</p>
+					<p class="button-controls">
+						<button class="ui-button-primary ui-button ui-widget ui-state-default ui-corner-all ui-button-text-only" role="button" aria-disabled="false"><span class="ui-button-text"  onclick="del_link('.$menu['id'].',\''.$menuID.'\');return false;">删除链接</span></button>
+						<button class="ui-button-primary ui-button ui-widget ui-state-default ui-corner-all ui-button-text-only" role="button" aria-disabled="false"><span class="ui-button-text" onclick="save_link('.$menu['id'].');return false;">保存链接</span></button>
+					</p>
+				</div>
+				<span id="'.$menu['id'].'"></span>
+			</li>';
+        if ($value == 'null') {
+            $html .= $html_tmp;
+        } else {
+            $html = str_replace('<span id="'.$value.'"></span>', '<span id="'.$value.'"></span><ol>'.$html_tmp.'</ol>', $html);
+        }
+    }
+    echo $html;
+    ?>
+
+							</ol>
+						</div>
+					</div>
+
+					<div id="menu-edit-footer">
+						<div class="addlink-action">
+							<button class="ui-button-primary ui-button ui-widget ui-state-default ui-corner-all ui-button-text-only" role="button" aria-disabled="false" onclick="add_link(this,'custom','<?php echo $menuID;?>')"><span class="ui-button-text">添加自定义链接</span></button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+<?php
+
+}
+?>
+
+<div id="dialog" title="创建新导航" style="display:none;">
+	<form id="edit" name="edit" method="post" action="">
+	  <input id="edtID" name="creat" type="hidden" value="new">
+	  <p>
+		<span class="title">htmlID(英文标识):</span><span class="star">(*)</span><br>
+		<input id="id" class="edit" size="40" name="id" maxlength="20" type="text" value="">
+	  </p>
+
+	  <p>
+		<span class="title">名称:</span><span class="star">(*)</span><br>
+		<input id="name" class="edit" size="40" name="name" maxlength="20" type="text" value="">
+	  </p>
+	  <p>
+		<input type="submit" class="button" value="提交" id="btnPost">
+	  </p>
+	</form>
+</div>
+<div id="message" title="消息通知" style="display:none;"><p>操作成功</p></div>
+	</div>
+  </div>
+</div>
+<?php
+require $blogpath.'zb_system/admin/admin_footer.php';
+RunTime();
+?>
