c8570810675ffbd6cee75bc79d2e89ebe30972e6
zsx <zsx@zsxsoft.com>
2016-12-26 13:33:05 +0800

    版本号抽离

--- zb_system/function/c_system_base.php
+++ zb_system/function/c_system_base.php
@@ -13,24 +13,9 @@ defined('ZBP_PATH') || define('ZBP_PATH', rtrim(str_replace('\\', '/', realpath(
 defined('ZBP_HOOKERROR') || define('ZBP_HOOKERROR', true);
 
 /**
- * 定义版本号
- * 因为版本号要经常改，所以往上面放
- */
-define('ZC_VERSION_MAJOR', '1');
-define('ZC_VERSION_MINOR', '5');
-define('ZC_VERSION_BUILD', '1');
-define('ZC_VERSION_COMMIT', '1705');
-define('ZC_VERSION_CODENAME', 'Zero');
-define('ZC_VERSION', ZC_VERSION_MAJOR . '.' . ZC_VERSION_MINOR . '.' . ZC_VERSION_BUILD . '.' . ZC_VERSION_COMMIT);
-define('ZC_VERSION_DISPLAY', ZC_VERSION_MAJOR . '.' . ZC_VERSION_MINOR . '.' . ZC_VERSION_BUILD  .' ' . ZC_VERSION_CODENAME);
-define('ZC_VERSION_FULL', ZC_VERSION . ' (' . ZC_VERSION_CODENAME . ')');
-define('ZC_BLOG_COMMIT', ZC_VERSION_COMMIT); // 为写入系统配置统一风格
-$GLOBALS['blogversion'] = ZC_VERSION_MAJOR . ZC_VERSION_MINOR . ZC_VERSION_COMMIT;
-define('ZC_BLOG_VERSION', ZC_VERSION_DISPLAY . ' Build ' . $GLOBALS['blogversion']);
-
-/**
  * 加载系统基础函数
  */
+require ZBP_PATH . 'zb_system/function/c_system_version.php';
 require ZBP_PATH . 'zb_system/function/c_system_plugin.php';
 require ZBP_PATH . 'zb_system/function/c_system_debug.php';
 require ZBP_PATH . 'zb_system/function/c_system_common.php';
