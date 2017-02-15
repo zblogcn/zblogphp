116d27d5f975aca77502e79559d6738ba91998e5
zx.asd <rainbowsoft@gmail.com>
2017-02-15 16:00:32 +0800

    修正了涂涂指出的空用户token总是变的问题；

--- zb_system/function/lib/zblogphp.php
+++ zb_system/function/lib/zblogphp.php
@@ -2472,7 +2472,7 @@ class ZBlogPHP {
      */
     public function GetWebToken($wt_id = '', $day = 1 ) {
         $t = intval( $day * 24 * 3600 ) + time();
-        return CreateWebToken($wt_id, $t ,$this->guid, $this->user->Guid, $this->user->ID, $this->user->Password);
+        return CreateWebToken($wt_id, $t ,$this->guid, $this->user->Status, $this->user->ID, $this->user->Password);
     }
 
     /**
@@ -2483,7 +2483,7 @@ class ZBlogPHP {
      */
     public function ValidWebToken($wt, $wt_id = '') {
 
-        if( VerifyWebToken($wt, $wt_id, $this->guid, $this->user->Guid, $this->user->ID, $this->user->Password) === true ){
+        if( VerifyWebToken($wt, $wt_id, $this->guid, $this->user->Status, $this->user->ID, $this->user->Password) === true ){
             return true;
         }
 
@@ -2496,7 +2496,8 @@ class ZBlogPHP {
      * @return string
      */
     public function GetToken($id = '') {
-        return md5($this->guid . $this->user->ID . $this->user->Password . $id . date('Ymdh'));
+        $s = $this->user->ID . $this->user->Password . $this->user->Status;
+        return md5($this->guid . $s . $id . date('Ymdh'));
     }
 
     /**
@@ -2506,10 +2507,11 @@ class ZBlogPHP {
      * @return bool
      */
     public function ValidToken($t, $id = '') {
-        if ($t == md5($this->guid . $this->user->ID . $this->user->Password . $id . date('Ymdh'))) {
+        $s = $this->user->ID . $this->user->Password . $this->user->Status;
+        if ($t == md5($this->guid . $s . $id . date('Ymdh'))) {
             return true;
         }
-        if ($t == md5($this->guid . $this->user->ID . $this->user->Password . $id. date('Ymdh', time() - (3600 * 1)))) {
+        if ($t == md5($this->guid . $s . $id. date('Ymdh', time() - (3600 * 1)))) {
             return true;
         }
 
