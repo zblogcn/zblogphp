69a36017b8de7c12979720386f64df6148c4785e
zx.asd <rainbowsoft@gmail.com>
2017-04-07 17:00:59 +0800

    admin/index.php的增强；

--- zb_system/admin/index.php
+++ zb_system/admin/index.php
@@ -14,68 +14,89 @@ $zbp->Load();
 
 $action = GetVars('act', 'GET');
 
-if (($action == '') || ($action == null)) {
-    $action = 'admin';
-}
+$admin_action_add = null;
+$admin_function = null;
 
-foreach ($GLOBALS['hooks']['Filter_Plugin_Admin_Begin'] as $fpname => &$fpsignal) {
-    $fpname();
-}
+if (($action == '') || ($action == null)) {$action = 'admin';}
 
-if (!$zbp->CheckRights($action)) {
-    $zbp->ShowError(6, __FILE__, __LINE__);
-    die();
-}
+if (!$zbp->CheckRights($action)) {$zbp->ShowError(6, __FILE__, __LINE__);die();}
 
-$f = null;
+foreach ($GLOBALS['hooks']['Filter_Plugin_Admin_Begin'] as $fpname => &$fpsignal) {$fpname();}
+
 switch ($action) {
     case 'ArticleMng':
-        $f = 'Admin_ArticleMng';
-        $blogtitle = $lang['msg']['article_manage'];
+        if(is_null($admin_function)){
+            $admin_function = 'Admin_ArticleMng';
+            $blogtitle = $lang['msg']['article_manage'];
+        }
         break;
     case 'PageMng':
-        $f = 'Admin_PageMng';
-        $blogtitle = $lang['msg']['page_manage'];
+        if(is_null($admin_function)){
+            $admin_function = 'Admin_PageMng';
+            $blogtitle = $lang['msg']['page_manage'];
+        }
         break;
     case 'CategoryMng':
-        $f = 'Admin_CategoryMng';
-        $blogtitle = $lang['msg']['category_manage'];
+        if(is_null($admin_function)){
+            $admin_function = 'Admin_CategoryMng';
+            $blogtitle = $lang['msg']['category_manage'];
+        }
         break;
     case 'CommentMng':
-        $f = 'Admin_CommentMng';
-        $blogtitle = $lang['msg']['comment_manage'];
+        if(is_null($admin_function)){
+            $admin_function = 'Admin_CommentMng';
+            $blogtitle = $lang['msg']['comment_manage'];
+        }
         break;
     case 'MemberMng':
-        $f = 'Admin_MemberMng';
-        $blogtitle = $lang['msg']['member_manage'];
+        if(is_null($admin_function)){
+            $admin_function = 'Admin_MemberMng';
+            $blogtitle = $lang['msg']['member_manage'];
+        }
         break;
     case 'UploadMng':
-        $f = 'Admin_UploadMng';
-        $blogtitle = $lang['msg']['upload_manage'];
+        if(is_null($admin_function)){
+            $admin_function = 'Admin_UploadMng';
+            $blogtitle = $lang['msg']['upload_manage'];
+        }
         break;
     case 'TagMng':
-        $f = 'Admin_TagMng';
-        $blogtitle = $lang['msg']['tag_manage'];
+        if(is_null($admin_function)){
+            $admin_function = 'Admin_TagMng';
+            $blogtitle = $lang['msg']['tag_manage'];
+        }
         break;
     case 'PluginMng':
-        $f = 'Admin_PluginMng';
-        $blogtitle = $lang['msg']['plugin_manage'];
+        if(is_null($admin_function)){
+            $admin_function = 'Admin_PluginMng';
+            $blogtitle = $lang['msg']['plugin_manage'];
+        }
         break;
     case 'ThemeMng':
-        $f = 'Admin_ThemeMng';
-        $blogtitle = $lang['msg']['theme_manage'];
+        if(is_null($admin_function)){
+            $admin_function = 'Admin_ThemeMng';
+            $blogtitle = $lang['msg']['theme_manage'];
+        }
         break;
     case 'ModuleMng':
-        $f = 'Admin_ModuleMng';
-        $blogtitle = $lang['msg']['module_manage'];
+        if(is_null($admin_function)){
+            $admin_function = 'Admin_ModuleMng';
+            $blogtitle = $lang['msg']['module_manage'];
+        }
         break;
     case 'SettingMng':
-        $f = 'Admin_SettingMng';
-        $blogtitle = $lang['msg']['settings'];
+        if(is_null($admin_function)){
+            $admin_function = 'Admin_SettingMng';
+            $blogtitle = $lang['msg']['settings'];
+        }
         break;
     case 'admin':
-        $f = 'Admin_SiteInfo';
-        $blogtitle = $lang['msg']['dashboard'];
+        if(is_null($admin_function)){
+            $admin_function = 'Admin_SiteInfo';
+            $blogtitle = $lang['msg']['dashboard'];
+        }
+        break;
+    case $admin_action_add:
         break;
     default:
         $zbp->ShowError(6, __FILE__, __LINE__);
@@ -89,15 +110,13 @@ require ZBP_PATH . 'zb_system/admin/admin_top.php';
 ?>
 <div id="divMain">
 <?php
-$f();
+$admin_function();
 ?>
 </div>
 <?php
 require ZBP_PATH . 'zb_system/admin/admin_footer.php';
 
-foreach ($GLOBALS['hooks']['Filter_Plugin_Admin_End'] as $fpname => &$fpsignal) {
-    $fpname();
-}
+foreach ($GLOBALS['hooks']['Filter_Plugin_Admin_End'] as $fpname => &$fpsignal) {$fpname();}
 
 RunTime();
 ?>
