62546d94c32fb19a5705809570d5e49193d8dbf7
zsx <zsxsoft@outlook.com>
2019-03-22 06:55:48 +0000

    Apply fixes from StyleCI

--- zb_users/plugin/LinksManage/main.php
+++ zb_users/plugin/LinksManage/main.php
@@ -4,75 +4,77 @@ require '../../../zb_system/function/c_system_admin.php';
 $zbp->Load();
 $action = 'root';
 if (!$zbp->CheckRights($action)) {
-  $zbp->ShowError(6);
-  die();
+    $zbp->ShowError(6);
+    die();
 }
 if (!$zbp->CheckPlugin('LinksManage')) {
-  $zbp->ShowError(48);
-  die();
+    $zbp->ShowError(48);
+    die();
 }
 InstallPlugin_LinksManage();
 $act = GetVars('act', 'GET');
 $suc = GetVars('suc', 'GET');
 if (GetVars('act', 'GET') == 'save') {
-  if (function_exists('CheckIsRefererValid')) {
-    CheckIsRefererValid();
-  }
-  $mod = $zbp->GetModuleByID(GetVars('ID', 'POST'));
-  $content = '';
-  $sub = 0;
-  $tree = (int)$_POST['tree'] == 1;
-  $items = array();
-  $parent = null;
-  foreach ($_POST['name'] as $k => $v) {
-    $item = (object)array();
-    if ($k == count($_POST['name']) - 1) continue;
-    $item->href = $_POST['href'][$k];
-    $item->title = $_POST['title'][$k];
-    $item->target = !$_POST['target'][$k] ? '' : '_blank';
-    $item->name = $_POST['name'][$k];
-    $item->subs = array();
-    $item->issub = 0;
-    if ($k > 0 && $_POST['sub'][$k]) {
-      $item->issub = 1;
-      $parent->subs[] = $item;
-    } else {
-      $items[$k] = $item;
-      $parent = &$items[$k];
+    if (function_exists('CheckIsRefererValid')) {
+        CheckIsRefererValid();
     }
-  }
-  $fileName = GetVars('FileName', 'POST');
-  $outTpl = "Links_defend";
-  if (isset($zbp->template->templates["Links_{$fileName}"])) {
-    $outTpl = "Links_{$fileName}";
-  }
-  foreach ($items as $item) {
-    $zbp->template->SetTags('id', $fileName);
-    $zbp->template->SetTags('item', $item);
-    $content .= $zbp->template->Output($outTpl);
-  }
-  $content = str_replace(array('target="" ', ' target=""', "\n"), "", CloseTags($content));
-  $content = preg_replace('/>\s+</', "><", $content);
-  $mod->Content = $content;
-  $mod->Name = $_POST['Name'];
-  if ($mod->ID == 0) {
-    $mod->FileName = $_POST['FileName'];
-  }
-  $mod->HtmlID = $_POST['HtmlID'];
-  $mod->Source = $_POST['Source'];
-  $mod->Type = 'ul';
-  $mod->MaxLi = 0;
-  FilterModule($mod);
-  $mod->Save();
-  $zbp->AddBuildModule($mod->FileName);
-  $zbp->BuildModule();
+    $mod = $zbp->GetModuleByID(GetVars('ID', 'POST'));
+    $content = '';
+    $sub = 0;
+    $tree = (int) $_POST['tree'] == 1;
+    $items = array();
+    $parent = null;
+    foreach ($_POST['name'] as $k => $v) {
+        $item = (object) array();
+        if ($k == count($_POST['name']) - 1) {
+            continue;
+        }
+        $item->href = $_POST['href'][$k];
+        $item->title = $_POST['title'][$k];
+        $item->target = !$_POST['target'][$k] ? '' : '_blank';
+        $item->name = $_POST['name'][$k];
+        $item->subs = array();
+        $item->issub = 0;
+        if ($k > 0 && $_POST['sub'][$k]) {
+            $item->issub = 1;
+            $parent->subs[] = $item;
+        } else {
+            $items[$k] = $item;
+            $parent = &$items[$k];
+        }
+    }
+    $fileName = GetVars('FileName', 'POST');
+    $outTpl = "Links_defend";
+    if (isset($zbp->template->templates["Links_{$fileName}"])) {
+        $outTpl = "Links_{$fileName}";
+    }
+    foreach ($items as $item) {
+        $zbp->template->SetTags('id', $fileName);
+        $zbp->template->SetTags('item', $item);
+        $content .= $zbp->template->Output($outTpl);
+    }
+    $content = str_replace(array('target="" ', ' target=""', "\n"), "", CloseTags($content));
+    $content = preg_replace('/>\s+</', "><", $content);
+    $mod->Content = $content;
+    $mod->Name = $_POST['Name'];
+    if ($mod->ID == 0) {
+        $mod->FileName = $_POST['FileName'];
+    }
+    $mod->HtmlID = $_POST['HtmlID'];
+    $mod->Source = $_POST['Source'];
+    $mod->Type = 'ul';
+    $mod->MaxLi = 0;
+    FilterModule($mod);
+    $mod->Save();
+    $zbp->AddBuildModule($mod->FileName);
+    $zbp->BuildModule();
 
-  // 写入文件
-  $file = LinksManage_Path("usr") . $mod->FileName . ".json";
-  file_put_contents($file, json_encode($items));
+    // 写入文件
+    $file = LinksManage_Path("usr") . $mod->FileName . ".json";
+    file_put_contents($file, json_encode($items));
 
-  $zbp->SetHint('good');
-  Redirect($_POST['stay'] == '1' ? $_SERVER['HTTP_REFERER'] : '../../../zb_system/cmd.php?act=ModuleMng');
+    $zbp->SetHint('good');
+    Redirect($_POST['stay'] == '1' ? $_SERVER['HTTP_REFERER'] : '../../../zb_system/cmd.php?act=ModuleMng');
 }
 
 $mod = new Module();
@@ -85,54 +87,71 @@ $tree = null;
 $delbtn = '';
 
 if ($edit = GetVars('edit', 'GET')) {
-  if (!empty($edit)) {
-    $mod = $zbp->modulesbyfilename[$edit];
-  }
-  $file = LinksManage_Path("usr") . $edit . ".json";
-  if (is_file($file) && $items = json_decode(file_get_contents($file))) {
-    $list = '';
-    foreach ($items as $item) {
-      $zbp->template->SetTags('item', $item);
-      $list .= $zbp->template->Output("Links_admin");
+    if (!empty($edit)) {
+        $mod = $zbp->modulesbyfilename[$edit];
     }
-  } else {
-    $content = $mod->Content;
-    preg_match('/<\/ul><\/li>/i', $content, $tree);
-    if ($tree) $content = str_replace(array('<ul>', '</ul></li>'), array("</li>\n", ''), $content);
-    $preg = array(
-      'tag' => '/<li.*?<\/li>/',
-      'sub' => '/<li.*?class=[\'|\"](.*?)[\'|\"]/i',
-      'href' => '/<a.*?href=[\'|\"](.*?)[\'|\"]/i',
+    $file = LinksManage_Path("usr") . $edit . ".json";
+    if (is_file($file) && $items = json_decode(file_get_contents($file))) {
+        $list = '';
+        foreach ($items as $item) {
+            $zbp->template->SetTags('item', $item);
+            $list .= $zbp->template->Output("Links_admin");
+        }
+    } else {
+        $content = $mod->Content;
+        preg_match('/<\/ul><\/li>/i', $content, $tree);
+        if ($tree) {
+            $content = str_replace(array('<ul>', '</ul></li>'), array("</li>\n", ''), $content);
+        }
+        $preg = array(
+      'tag'    => '/<li.*?<\/li>/',
+      'sub'    => '/<li.*?class=[\'|\"](.*?)[\'|\"]/i',
+      'href'   => '/<a.*?href=[\'|\"](.*?)[\'|\"]/i',
       'target' => '/<a.*?target=[\'|\"](.*?)[\'|\"]/i',
-      'name' => '/<a.*?>(.*?)<\/a>/i', 'title'=> '/<a.*?title=[\'|\"](.*?)[\'|\"]/i', ); $link=array();
-    preg_match_all($preg['tag'], $content, $tag); foreach ($tag[0] as $key=> $val) {
-    foreach ($preg as $k => $v) {
-    preg_match($v, $val, $m);
-    if (count($m) > 1) {
-    if ($k == 'name') $m[1] = preg_replace('/<img.*?[\/]>/i', '' , $m[1]); if ($k=='sub' ) $m[1]=!preg_match('/sub/i',
-      $m[1]) ? '' : 'LinksManageSub' ; $link[$k][$key]=$m[1]; } else { $link[$k][$key]='' ; } } } if ($link) { $list=''
-      ; foreach ($link['tag'] as $k=> $v) {
-        $list .= '<tr class="' . $link['sub'][$k] . '">
+      'name'   => '/<a.*?>(.*?)<\/a>/i', 'title'=> '/<a.*?title=[\'|\"](.*?)[\'|\"]/i', );
+        $link = array();
+        preg_match_all($preg['tag'], $content, $tag);
+        foreach ($tag[0] as $key=> $val) {
+            foreach ($preg as $k => $v) {
+                preg_match($v, $val, $m);
+                if (count($m) > 1) {
+                    if ($k == 'name') {
+                        $m[1] = preg_replace('/<img.*?[\/]>/i', '', $m[1]);
+                    }
+                    if ($k == 'sub') {
+                        $m[1] = !preg_match('/sub/i',
+      $m[1]) ? '' : 'LinksManageSub';
+                    }
+                    $link[$k][$key] = $m[1];
+                } else {
+                    $link[$k][$key] = '';
+                }
+            }
+        }
+        if ($link) {
+            $list = '';
+            foreach ($link['tag'] as $k=> $v) {
+                $list .= '<tr class="' . $link['sub'][$k] . '">
           <td><input name="href[]" value="' . $link['href'][$k] . '" size="30" /></td>
           <td><input name="title[]" value="' . $link['title'][$k] . '" size="30" /></td>
           <td><input name="name[]" value="' . $link['name'][$k] . '" size="20" /></td>
           <td><input name="target[]" value="' . ($link['target'][$k] ? 1 : 0) . '" class="checkbox" /></td>
           <td><input name="sub[]" value="' . ($link['sub'][$k] ? 1 : 0) . '" class="checkbox" /></td>
         <tr>';
-      }
+            }
+        }
     }
-  }
-  // if ($mod->Source == 'system' || $mod->Source == 'theme') {
+    // if ($mod->Source == 'system' || $mod->Source == 'theme') {
     $islock = 'readonly="readonly"';
-  // }
-  $delbtn = $mod->Source === 'plugin_LinksManage' ? '&nbsp;<a title="删除当前模块"
+    // }
+    $delbtn = $mod->Source === 'plugin_LinksManage' ? '&nbsp;<a title="删除当前模块"
     onclick="return window.confirm(\'' . $zbp->lang['msg']['confirm_operating'] . '\');"
     href="' . BuildSafeCmdURL('act=ModuleDel&amp;source=theme&amp;filename=' . $mod->FileName) . '"><img
       src="' . $zbp->host . 'zb_system/image/admin/delete.png" alt="删除" title="删除" width="16"></a>' : '';
-  $bakFile = LinksManage_Path("bakdir") . "{$mod->FileName}.txt";
-  if (is_file($bakFile)) {
-    $bakUrl = str_replace($zbp->path, $zbp->host, $bakFile);
-  }
+    $bakFile = LinksManage_Path("bakdir") . "{$mod->FileName}.txt";
+    if (is_file($bakFile)) {
+        $bakUrl = str_replace($zbp->path, $zbp->host, $bakFile);
+    }
 }
 
 $blogtitle = '链接管理';
@@ -205,14 +224,16 @@ require $blogpath . 'zb_system/admin/admin_top.php';
         <input type="submit" class="button" value="<?php echo $lang['msg']['submit'] ?>"
           onclick="return checkInfo();" />
         <input type="text" name="stay" class="checkbox" value="0" /> 提交后返回本页
-        <?php if (isset($bakUrl)) { ?>
+        <?php if (isset($bakUrl)) {
+    ?>
         <a title="查看备份" href="<?php echo $bakUrl; ?>" target="_blank">查看备份（<?php echo $mod->FileName; ?>）</a>
-        <?php } ?>
+        <?php
+} ?>
       </p>
       ------
-      <p>对于每个li，会默认添加 "文件名-item" 作为类名，当前为：<?php echo "{$mod->FileName}-item";?></p>
-      <p>默认模板路径：<?php echo LinksManage_Path("u-temp");?></p>
-      <p>(暂未实现)自定义模板路径：<?php echo LinksManage_Path("usr/{$mod->FileName}.li");?></p>
+      <p>对于每个li，会默认添加 "文件名-item" 作为类名，当前为：<?php echo "{$mod->FileName}-item"; ?></p>
+      <p>默认模板路径：<?php echo LinksManage_Path("u-temp"); ?></p>
+      <p>(暂未实现)自定义模板路径：<?php echo LinksManage_Path("usr/{$mod->FileName}.li"); ?></p>
       <p>模板编译时会加<b>"Links_"</b>前缀，默认模板编译为<b>Links_defend</b></p>
     </form>
   </div>
