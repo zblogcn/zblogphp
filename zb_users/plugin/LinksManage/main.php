4270e1aae5c623d778cef232d921ce21aee2f389
zx.asd <rainbowsoft@gmail.com>
2019-04-10 16:35:32 +0800

    被代码机器人搞死了！

--- zb_users/plugin/LinksManage/main.php
+++ zb_users/plugin/LinksManage/main.php
@@ -4,19 +4,41 @@ require '../../../zb_system/function/c_system_admin.php';
 $zbp->Load();
 $action = 'root';
 if (!$zbp->CheckRights($action)) {
-    $zbp->ShowError(6);
-    die();
+  $zbp->ShowError(6);
+  die();
 }
 if (!$zbp->CheckPlugin('LinksManage')) {
-    $zbp->ShowError(48);
-    die();
+  $zbp->ShowError(48);
+  die();
 }
 InstallPlugin_LinksManage();
 $act = GetVars('act', 'GET');
 $suc = GetVars('suc', 'GET');
 if (GetVars('act', 'GET') == 'save') {
-    if (function_exists('CheckIsRefererValid')) {
-        CheckIsRefererValid();
+  if (function_exists('CheckIsRefererValid')) {
+    CheckIsRefererValid();
+  }
+  $mod = $zbp->GetModuleByID(GetVars('ID', 'POST'));
+  $content = '';
+  $sub = 0;
+  $tree = (int)$_POST['tree'] == 1;
+  $items = array();
+  $parent = null;
+  foreach ($_POST['name'] as $k => $v) {
+    $item = (object)array();
+    if ($k == count($_POST['name']) - 1) continue;
+    $item->href = $_POST['href'][$k];
+    $item->title = $_POST['title'][$k];
+    $item->target = !$_POST['target'][$k] ? '' : '_blank';
+    $item->name = $_POST['name'][$k];
+    $item->subs = array();
+    $item->issub = 0;
+    if ($k > 0 && $_POST['sub'][$k]) {
+      $item->issub = 1;
+      $parent->subs[] = $item;
+    } else {
+      $items[$k] = $item;
+      $parent = &$items[$k];
     }
   }
   //$fileName = GetVars('FileName', 'POST');
@@ -52,16 +74,13 @@ if (GetVars('act', 'GET') == 'save') {
   $zbp->SetHint('good');
   Redirect($_POST['stay'] == '1' ? $_SERVER['HTTP_REFERER'] : '../../../zb_system/cmd.php?act=ModuleMng');
 }
-
 $mod = new Module();
 $mod->ID = 0;
 $mod->Source = 'plugin_LinksManage';
 $list = '<tr><td><input type="text" name="href[]" value="http://" size="30" /></td><td><input type="text" name="title[]" value="链接描述" size="30" /></td><td><input type="text" name="name[]" value="新名称" size="20" /></td><td><input type="text" name="target[]" class="checkbox" value="0" /></td><td><input type="text" name="sub[]" class="checkbox" value="0" /></td></tr>';
-
 $islock = '';
 $tree = null;
 $delbtn = '';
-
 if ($edit = GetVars('edit', 'GET')) {
   if (!empty($edit)) {
     $mod = $zbp->modulesbyfilename[$edit];
@@ -73,66 +92,41 @@ if ($edit = GetVars('edit', 'GET')) {
       $zbp->template->SetTags('item', $item);
       $list .= $zbp->template->Output("Links_admin");
     }
-    $file = LinksManage_Path("usr") . $edit . ".json";
-    if (is_file($file) && $items = json_decode(file_get_contents($file))) {
-        $list = '';
-        foreach ($items as $item) {
-            $zbp->template->SetTags('item', $item);
-            $list .= $zbp->template->Output("Links_admin");
-        }
-    } else {
-        $content = $mod->Content;
-        preg_match('/<\/ul><\/li>/i', $content, $tree);
-        if ($tree) {
-            $content = str_replace(array('<ul>', '</ul></li>'), array("</li>\n", ''), $content);
-        }
-        $preg = array(
-      'tag'    => '/<li.*?<\/li>/',
-      'sub'    => '/<li.*?class=[\'|\"](.*?)[\'|\"]/i',
-      'href'   => '/<a.*?href=[\'|\"](.*?)[\'|\"]/i',
+  } else {
+    $content = $mod->Content;
+    preg_match('/<\/ul><\/li>/i', $content, $tree);
+    if ($tree) $content = str_replace(array('<ul>', '</ul></li>'), array("</li>\n", ''), $content);
+    $preg = array(
+      'tag' => '/<li.*?<\/li>/',
+      'sub' => '/<li.*?class=[\'|\"](.*?)[\'|\"]/i',
+      'href' => '/<a.*?href=[\'|\"](.*?)[\'|\"]/i',
       'target' => '/<a.*?target=[\'|\"](.*?)[\'|\"]/i',
-      'name'   => '/<a.*?>(.*?)<\/a>/i', 'title'=> '/<a.*?title=[\'|\"](.*?)[\'|\"]/i', );
-        $link = array();
-        preg_match_all($preg['tag'], $content, $tag);
-        foreach ($tag[0] as $key=> $val) {
-            foreach ($preg as $k => $v) {
-                preg_match($v, $val, $m);
-                if (count($m) > 1) {
-                    if ($k == 'name') {
-                        $m[1] = preg_replace('/<img.*?[\/]>/i', '', $m[1]);
-                    }
-                    if ($k == 'sub') {
-                        $m[1] = !preg_match('/sub/i',
-      $m[1]) ? '' : 'LinksManageSub';
-                    }
-                    $link[$k][$key] = $m[1];
-                } else {
-                    $link[$k][$key] = '';
-                }
-            }
-        }
-        if ($link) {
-            $list = '';
-            foreach ($link['tag'] as $k=> $v) {
-                $list .= '<tr class="' . $link['sub'][$k] . '">
+      'name' => '/<a.*?>(.*?)<\/a>/i', 'title'=> '/<a.*?title=[\'|\"](.*?)[\'|\"]/i', ); $link=array();
+    preg_match_all($preg['tag'], $content, $tag); foreach ($tag[0] as $key=> $val) {
+    foreach ($preg as $k => $v) {
+    preg_match($v, $val, $m);
+    if (count($m) > 1) {
+    if ($k == 'name') $m[1] = preg_replace('/<img.*?[\/]>/i', '' , $m[1]); if ($k=='sub' ) $m[1]=!preg_match('/sub/i',
+      $m[1]) ? '' : 'LinksManageSub' ; $link[$k][$key]=$m[1]; } else { $link[$k][$key]='' ; } } } if ($link) { $list=''
+      ; foreach ($link['tag'] as $k=> $v) {
+        $list .= '<tr class="' . $link['sub'][$k] . '">
           <td><input name="href[]" value="' . $link['href'][$k] . '" size="30" /></td>
           <td><input name="title[]" value="' . $link['title'][$k] . '" size="30" /></td>
           <td><input name="name[]" value="' . $link['name'][$k] . '" size="20" /></td>
           <td><input name="target[]" value="' . ($link['target'][$k] ? 1 : 0) . '" class="checkbox" /></td>
           <td><input name="sub[]" value="' . ($link['sub'][$k] ? 1 : 0) . '" class="checkbox" /></td>
         <tr>';
-            }
-        }
+      }
     }
-    if ($mod->Source == 'system' || $mod->Source == 'theme') {
-        $islock = 'readonly="readonly"';
-    }
-    $delbtn = $mod->Source === 'plugin_LinksManage' ? '&nbsp;<a title="删除当前模块"
+  }
+  if ($mod->Source == 'system' || $mod->Source == 'theme') {
+    $islock = 'readonly="readonly"';
+  }
+  $delbtn = $mod->Source === 'plugin_LinksManage' ? '&nbsp;<a title="删除当前模块"
     onclick="return window.confirm(\'' . $zbp->lang['msg']['confirm_operating'] . '\');"
     href="' . BuildSafeCmdURL('act=ModuleDel&amp;source=theme&amp;filename=' . $mod->FileName) . '"><img
       src="' . $zbp->host . 'zb_system/image/admin/delete.png" alt="删除" title="删除" width="16"></a>' : '';
 }
-
 $blogtitle = '链接管理';
 $blogtitle .= $mod->Name !== "" ? "（{$mod->Name}）" : "";
 require $blogpath . 'zb_system/admin/admin_header.php';
@@ -204,11 +198,9 @@ require $blogpath . 'zb_system/admin/admin_top.php';
         <input type="submit" class="button" value="<?php echo $lang['msg']['submit'] ?>"
           onclick="return checkInfo();" />
         <input type="text" name="stay" class="checkbox" value="0" /> 提交后返回本页
-        <?php if (isset($bakUrl)) {
-    ?>
+        <?php if (isset($bakUrl)) { ?>
         <a title="查看备份" href="<?php echo $bakUrl; ?>" target="_blank">查看备份（<?php echo $mod->FileName; ?>）</a>
-        <?php
-} ?>
+        <?php } ?>
       </p>
       <hr style="border-top:1px solid #ddd!important;visibility:visible;"/>
       <p>对于每个li，会默认添加 "文件名-item" 作为类名，当前为：<?php echo "{$mod->FileName}-item";?></p>
@@ -237,4 +229,3 @@ function checkInfo() {
 <?php
 require $blogpath . 'zb_system/admin/admin_footer.php';
 RunTime();
-?>
