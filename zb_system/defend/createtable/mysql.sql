7899fdd58fda9ebd496af9c0439e3a68fe6a8b7b
zhuzhuxf <zhuzhuxf@gmail.com>
2020-02-28 04:31:54 +0800

    config的value放宽为longtext；

--- zb_system/defend/createtable/mysql.sql
+++ zb_system/defend/createtable/mysql.sql
@@ -64,7 +64,7 @@ CREATE TABLE IF NOT EXISTS %pre%comment (
 CREATE TABLE IF NOT EXISTS %pre%config (
   conf_ID int(11) NOT NULL AUTO_INCREMENT,
   conf_Name varchar(255) NOT NULL DEFAULT '',
-  conf_Value text,
+  conf_Value longtext,
   PRIMARY KEY (conf_ID),
   KEY %pre%conf_Name (conf_Name)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
