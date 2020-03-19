27023095e9fb544f994c0aadce157d834a5c8fd2
zhouzishu <me@zzs.im>
2020-03-19 15:25:58 +0800

    去掉 LoadInfoByField 多余的代码 (#251)

--- zb_system/function/lib/base.php
+++ zb_system/function/lib/base.php
@@ -206,20 +206,9 @@ class Base
      */
     public function LoadInfoByField($field, $field_value)
     {
-        global $table, $datainfo;
-        $field_table = array_flip($table);
-        $field_table = $field_table[$this->table];
-        $field_name = $datainfo[$field_table][$field][0];
-        $sql = $this->db->sql->Select($this->table, array('*'), array(array('=', $field_name, $field_value)), null, 1, null);
-        $array = $this->db->Query($sql);
-
-        if (count($array) > 0) {
-            $this->LoadInfoByAssoc($array[0]);
-
-            return true;
-        } else {
-            return false;
-        }
+        return $this->LoadInfoByFields(array(
+            $field => $field_value
+        ));
     }
 
     /**
