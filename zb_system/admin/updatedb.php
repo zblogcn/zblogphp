d21da626a1ce37024e40e41e1c54a6bc0898d0b6
zhuzhuxf <zhuzhuxf@gmail.com>
2020-04-04 19:47:10 +0800

    @号错误抑制功能加强；updatedb页开始使用了！

--- zb_system/admin/updatedb.php
+++ zb_system/admin/updatedb.php
@@ -20,26 +20,34 @@ if (!$zbp->CheckRights($action)) {
 
 function updatedb_162090to169999()
 {
-    global $zbp, $table;
-    ZBlogException::SuspendErrorHook();
-    if ($zbp->db->type == 'mysql') {
-        //$zbp->db->Query("ALTER TABLE " . $table['Tag'] . " ADD  `tag_Type` INT(11) NOT NULL DEFAULT '0';");
-        //$zbp->db->Query("ALTER TABLE " . $table['Category'] . " ADD  `cate_Type` INT(11) NOT NULL DEFAULT '0';");
-    } elseif ($zbp->db->type == 'sqlite') {
-        //$zbp->db->Query("ALTER TABLE " . $table['Tag'] . " ADD  tag_Type integer NOT NULL DEFAULT 0;");
-        //$zbp->db->Query("ALTER TABLE " . $table['Category'] . " ADD  cate_Type integer NOT NULL DEFAULT 0;");
+    global $zbp, $table, $datainfo;
+    $t = &$table;
+    $d = &$datainfo;
+    //ZBlogException::SuspendErrorHook();
+    $old = @$zbp->db->Query($zbp->db->sql->Select($t['Config'], '*', array(array('=', $d['Config']['Key'][0], ''))));
+    if (count($old) == 1 && $old[0] === false) {
+        $zbp->db->Query("ALTER TABLE {$table['Config']} ADD {$d['Config']['Key'][0]} VARCHAR(255) NOT NULL DEFAULT '';");
     }
-    if ($zbp->db->type == 'mysql') {
-        $zbp->db->Query("ALTER TABLE " . $table['Post'] . " MODIFY  `log_Type` INT(11) NOT NULL DEFAULT '0';");
-        $zbp->db->Query("ALTER TABLE " . $table['Tag'] . " MODIFY  `tag_Type` INT(11) NOT NULL DEFAULT '0';");
-        $zbp->db->Query("ALTER TABLE " . $table['Category'] . " MODIFY  `cate_Type` INT(11) NOT NULL DEFAULT '0';");
+    $old = @$zbp->db->Query('' .  $zbp->db->sql->Select($t['Post'], '*', array(array('=', $d['Post']['CreateTime'][0], ''))) . '');
+    if (count($old) == 1 && $old[0] === false) {
+        $zbp->db->Query("ALTER TABLE {$t['Post']} ADD {$d['Post']['CreateTime'][0]} integer NOT NULL DEFAULT 0;");
     }
-    ZBlogException::ResumeErrorHook();
+    $old = @$zbp->db->Query('' .  $zbp->db->sql->Select($t['Post'], '*', array(array('=', $d['Post']['UpdateTime'][0], ''))) . '');
+    if (count($old) == 1 && $old[0] === false) {
+        $zbp->db->Query("ALTER TABLE {$t['Post']} ADD {$d['Post']['UpdateTime'][0]} integer NOT NULL DEFAULT 0;");
+    }
+
+    if ($zbp->db->type == 'mysql') {
+        $zbp->db->Query("ALTER TABLE " . $t['Post'] . " MODIFY  `log_Type` INT(11) NOT NULL DEFAULT '0';");
+        $zbp->db->Query("ALTER TABLE " . $t['Tag'] . " MODIFY  `tag_Type` INT(11) NOT NULL DEFAULT '0';");
+        $zbp->db->Query("ALTER TABLE " . $t['Category'] . " MODIFY  `cate_Type` INT(11) NOT NULL DEFAULT '0';");
+    }
+    //ZBlogException::ResumeErrorHook();
     $zbp->option['ZC_LAST_VERSION'] = 169999;
     $zbp->SaveOption();
 }
 
-if ($zbp->version >= 169999 && (int) $zbp->option['ZC_LAST_VERSION'] < 169999) {
+//if ($zbp->version >= 169999 && (int) $zbp->option['ZC_LAST_VERSION'] < 169999) {
     updatedb_162090to169999();
-}
+//}
 die;
