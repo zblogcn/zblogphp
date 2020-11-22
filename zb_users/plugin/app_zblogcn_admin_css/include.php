37583266c073abc263d7d35ab9ab73798b9744ca
zhouzishu <me@zzs.im>
2020-11-22 13:23:36 +0800

    优化 app_zblogcn_admin_css 插件

--- zb_users/plugin/app_zblogcn_admin_css/include.php
+++ zb_users/plugin/app_zblogcn_admin_css/include.php
@@ -5,7 +5,7 @@ RegisterPlugin("app_zblogcn_admin_css","ActivePlugin_app_zblogcn_admin_css");
 function ActivePlugin_app_zblogcn_admin_css()
 {
     Add_Filter_Plugin('Filter_Plugin_Admin_Header', 'app_zblogcn_admin_css');
-    Add_Filter_Plugin('Filter_Plugin_Admin_LeftMenu', 'app_zblogcn_admin_css_logo');
+    Add_Filter_Plugin('Filter_Plugin_Admin_LeftMenu', 'app_zblogcn_admin_css_left_menu');
 }
 
 function InstallPlugin_app_zblogcn_admin_css()
@@ -22,10 +22,18 @@ function app_zblogcn_admin_css()
     echo '<link rel="stylesheet" type="text/css" href="' . $zbp->host . 'zb_users/plugin/app_zblogcn_admin_css/admin.css"/>' . "\r\n";
 }
 
-function app_zblogcn_admin_css_logo(&$m)
+function app_zblogcn_admin_css_left_menu(&$m)
 {
     global $zbp;
+
+    // logo
     array_unshift($m, '<div class="left-logo"><img src="'.$zbp->host.'zb_system/image/admin/logo_white.svg"/></div>');
+
+    // change icons for app.zblogcn.com
+    if ($zbp->theme == 'appcentre_server') {
+        $m['nav_new'] = str_replace('icon-pencil-square-fill', 'icon-cloud-upload-fill', $m['nav_new']);
+        $m['nav_article'] = str_replace('icon-stickies', 'icon-collection-fill', $m['nav_article']);
+    }
 }
 
 function UninstallPlugin_app_zblogcn_admin_css()
