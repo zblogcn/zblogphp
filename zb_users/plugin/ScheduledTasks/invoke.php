c8bffbfa07aec7f0e041a6f7514103dfa6b262a6
zhouzishu <me@zzs.im>
2020-12-26 16:32:42 +0800

    一次可以执行多个任务

--- zb_users/plugin/ScheduledTasks/invoke.php
+++ zb_users/plugin/ScheduledTasks/invoke.php
@@ -8,4 +8,13 @@ require '../../../zb_system/function/c_system_base.php';
 
 $zbp->Load();
 
-echo ScheduledTasks_Polling();
+$polling_results =  ScheduledTasks_Polling();
+if (count($polling_results) === 0) {
+    echo json_encode(array('msg' => 'No task was executed.', 'results' => array()));
+    exit;
+}
+
+echo json_encode(array(
+    'msg' => ((string) count($polling_results)) . ' tasks were executed.',
+    'results' => $polling_results,
+));
