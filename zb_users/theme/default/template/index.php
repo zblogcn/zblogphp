923343e040218537b7e7484d017c271af2b0f645
沉冰浮水 <wdssmq@qq.com>
2021-02-14 18:27:05 +0800

    默认主题 html 闭合

--- zb_users/theme/default/template/index.php
+++ zb_users/theme/default/template/index.php
@@ -1,31 +1,45 @@
 {* Template Name:首页及列表页 *}
-{template:header}
+<!DOCTYPE html>
+<html lang="{$lang['lang_bcp47']}">
+
+<head>
+  {template:header}
+</head>
+
 <body class="multi {$type}">
-<div id="divAll">
-	<div id="divPage">
-	<div id="divMiddle">
-		<div id="divTop">
-			<h1 id="BlogTitle"><a href="{$host}">{$name}</a></h1>
-			<h3 id="BlogSubTitle">{$subname}</h3>
-		</div>
-		<div id="divNavBar">
-<ul>
-{module:navbar}
-</ul>
-		</div>
-		<div id="divMain">
-{foreach $articles as $article}
+  <div id="divAll">
+    <div id="divPage">
+      <div id="divMiddle">
+        <div id="divTop">
+          <h1 id="BlogTitle"><a href="{$host}">{$name}</a></h1>
+          <h3 id="BlogSubTitle">{$subname}</h3>
+        </div>
+        <div id="divNavBar">
+          <ul>
+            {module:navbar}
+          </ul>
+        </div>
+        <div id="divMain">
+          {foreach $articles as $article}
 
-{if $article.TopType}
-{template:post-istop}
-{else}
-{template:post-multi}
-{/if}
+          {if $article.TopType}
+          {template:post-istop}
+          {else}
+          {template:post-multi}
+          {/if}
 
-{/foreach}
-<div class="pagebar">{template:pagebar}</div>
-		</div>
-		<div id="divSidebar">
-{template:sidebar}
-		</div>
-{template:footer}
+          {/foreach}
+          <div class="pagebar">{template:pagebar}</div>
+        </div>
+        <div id="divSidebar">
+          {template:sidebar}
+        </div>
+        {template:footer}
+      </div>
+      <div class="clear"></div>
+    </div>
+    <div class="clear"></div>
+  </div>
+</body>
+
+</html>
