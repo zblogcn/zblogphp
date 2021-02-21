e16ea3e82a5c9967bd31746c6782bf345dc2613c
zhouzishu <me@zzs.im>
2021-02-21 19:54:04 +0800

    修复 os2020 分页问题

--- zb_users/theme/os2020/template/index.php
+++ zb_users/theme/os2020/template/index.php
@@ -10,15 +10,15 @@
     window.bloghost = '{$host}';
     window.zbpCopyright = '{$copyright}';
   </script>
-  <link href="zb_users/theme/os2020/static/css/chunk-65dd580c.37499b8e.css" rel="prefetch">
-  <link href="zb_users/theme/os2020/static/css/chunk-c34ff3ce.2d37d1de.css" rel="prefetch">
-  <link href="zb_users/theme/os2020/static/js/chunk-2d0e95df.216420d9.js" rel="prefetch">
-  <link href="zb_users/theme/os2020/static/js/chunk-65dd580c.16812c5c.js" rel="prefetch">
-  <link href="zb_users/theme/os2020/static/js/chunk-c34ff3ce.b604db2e.js" rel="prefetch">
+  <link href="zb_users/theme/os2020/static/css/chunk-65dd580c.a8189d03.css" rel="prefetch">
+  <link href="zb_users/theme/os2020/static/css/chunk-71aae004.1abf374d.css" rel="prefetch">
+  <link href="zb_users/theme/os2020/static/js/chunk-2d0e95df.c44365ae.js" rel="prefetch">
+  <link href="zb_users/theme/os2020/static/js/chunk-65dd580c.5c503258.js" rel="prefetch">
+  <link href="zb_users/theme/os2020/static/js/chunk-71aae004.a91b6f78.js" rel="prefetch">
   <link href="zb_users/theme/os2020/static/css/chunk-vendors.902ebb66.css" rel="preload" as="style">
   <link href="zb_users/theme/os2020/static/css/index.095a8c6c.css" rel="preload" as="style">
-  <link href="zb_users/theme/os2020/static/js/chunk-vendors.daa570c6.js" rel="preload" as="script">
-  <link href="zb_users/theme/os2020/static/js/index.c5b5fb4e.js" rel="preload" as="script">
+  <link href="zb_users/theme/os2020/static/js/chunk-vendors.5bb807b7.js" rel="preload" as="script">
+  <link href="zb_users/theme/os2020/static/js/index.2eaf2d8a.js" rel="preload" as="script">
   <link href="zb_users/theme/os2020/static/css/chunk-vendors.902ebb66.css" rel="stylesheet">
   <link href="zb_users/theme/os2020/static/css/index.095a8c6c.css" rel="stylesheet">
 </head>
@@ -26,8 +26,8 @@
 <body><noscript><strong>We're sorry but {$name} doesn't work properly without JavaScript enabled. Please enable it to
       continue.</strong></noscript>
   <div id="app"></div>
-  <script src="zb_users/theme/os2020/static/js/chunk-vendors.daa570c6.js"></script>
-  <script src="zb_users/theme/os2020/static/js/index.c5b5fb4e.js"></script>
+  <script src="zb_users/theme/os2020/static/js/chunk-vendors.5bb807b7.js"></script>
+  <script src="zb_users/theme/os2020/static/js/index.2eaf2d8a.js"></script>
 </body>
 
 </html>
