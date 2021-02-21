ce31e5fb198aa89482d51b0a667a6ca08f28185f
zhouzishu <me@zzs.im>
2021-02-21 22:14:13 +0800

    更新 os2020 样式

--- zb_users/theme/os2020/template/index.php
+++ zb_users/theme/os2020/template/index.php
@@ -16,18 +16,18 @@
   <link href="zb_users/theme/os2020/static/js/chunk-2d0e95df.c44365ae.js" rel="prefetch">
   <link href="zb_users/theme/os2020/static/js/chunk-65dd580c.5c503258.js" rel="prefetch">
   <link href="zb_users/theme/os2020/static/css/chunk-vendors.902ebb66.css" rel="preload" as="style">
-  <link href="zb_users/theme/os2020/static/css/index.735497e5.css" rel="preload" as="style">
+  <link href="zb_users/theme/os2020/static/css/index.58538027.css" rel="preload" as="style">
   <link href="zb_users/theme/os2020/static/js/chunk-vendors.5bb807b7.js" rel="preload" as="script">
-  <link href="zb_users/theme/os2020/static/js/index.3225736b.js" rel="preload" as="script">
+  <link href="zb_users/theme/os2020/static/js/index.f8bcbf66.js" rel="preload" as="script">
   <link href="zb_users/theme/os2020/static/css/chunk-vendors.902ebb66.css" rel="stylesheet">
-  <link href="zb_users/theme/os2020/static/css/index.735497e5.css" rel="stylesheet">
+  <link href="zb_users/theme/os2020/static/css/index.58538027.css" rel="stylesheet">
 </head>
 
 <body><noscript><strong>We're sorry but {$name} doesn't work properly without JavaScript enabled. Please enable it to
       continue.</strong></noscript>
   <div id="app"></div>
   <script src="zb_users/theme/os2020/static/js/chunk-vendors.5bb807b7.js"></script>
-  <script src="zb_users/theme/os2020/static/js/index.3225736b.js"></script>
+  <script src="zb_users/theme/os2020/static/js/index.f8bcbf66.js"></script>
 </body>
 
 </html>
