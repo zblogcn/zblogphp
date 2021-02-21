1409e1a0865ac4cde43a6c5d67d8854cfecf3ee7
zhouzishu <me@zzs.im>
2021-02-21 18:21:16 +0800

    纳入橙光的 os2020 SPA 主题

---
+++ zb_users/theme/os2020/include.php
@@ -0,0 +1,7 @@
+<?php
+#注册插件
+RegisterPlugin("os2020","ActivePlugin_os2020");
+
+function ActivePlugin_os2020() {}
+function InstallPlugin_os2020() {}
+function UninstallPlugin_os2020() {}
---
+++ zb_users/theme/os2020/template/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge, Chrome=1">
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+  <title>{$name}-{$title}</title>
+  <script>
+    window.bloghost = '{$host}';
+    window.zbpCopyright = '{$copyright}';
+  </script>
+  <link href="zb_users/theme/os2020/static/css/chunk-65dd580c.37499b8e.css" rel="prefetch">
+  <link href="zb_users/theme/os2020/static/css/chunk-c34ff3ce.2d37d1de.css" rel="prefetch">
+  <link href="zb_users/theme/os2020/static/js/chunk-2d0e95df.216420d9.js" rel="prefetch">
+  <link href="zb_users/theme/os2020/static/js/chunk-65dd580c.16812c5c.js" rel="prefetch">
+  <link href="zb_users/theme/os2020/static/js/chunk-c34ff3ce.b604db2e.js" rel="prefetch">
+  <link href="zb_users/theme/os2020/static/css/chunk-vendors.902ebb66.css" rel="preload" as="style">
+  <link href="zb_users/theme/os2020/static/css/index.095a8c6c.css" rel="preload" as="style">
+  <link href="zb_users/theme/os2020/static/js/chunk-vendors.daa570c6.js" rel="preload" as="script">
+  <link href="zb_users/theme/os2020/static/js/index.c5b5fb4e.js" rel="preload" as="script">
+  <link href="zb_users/theme/os2020/static/css/chunk-vendors.902ebb66.css" rel="stylesheet">
+  <link href="zb_users/theme/os2020/static/css/index.095a8c6c.css" rel="stylesheet">
+</head>
+
+<body><noscript><strong>We're sorry but {$name} doesn't work properly without JavaScript enabled. Please enable it to
+      continue.</strong></noscript>
+  <div id="app"></div>
+  <script src="zb_users/theme/os2020/static/js/chunk-vendors.daa570c6.js"></script>
+  <script src="zb_users/theme/os2020/static/js/index.c5b5fb4e.js"></script>
+</body>
+
+</html>
