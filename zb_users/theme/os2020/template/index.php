d9b3756f2b1e3d1b8ec26468ba9fff595801cee7
zhouzishu <me@zzs.im>
2021-02-21 21:53:25 +0800

    os2020 主题搜索功能

--- zb_users/theme/os2020/template/index.php
+++ zb_users/theme/os2020/template/index.php
@@ -10,24 +10,24 @@
     window.bloghost = '{$host}';
     window.zbpCopyright = '{$copyright}';
   </script>
+  <link href="zb_users/theme/os2020/static/css/chunk-22a5707b.5b5dc877.css" rel="prefetch">
   <link href="zb_users/theme/os2020/static/css/chunk-65dd580c.a8189d03.css" rel="prefetch">
-  <link href="zb_users/theme/os2020/static/css/chunk-71aae004.1abf374d.css" rel="prefetch">
+  <link href="zb_users/theme/os2020/static/js/chunk-22a5707b.97bf40bd.js" rel="prefetch">
   <link href="zb_users/theme/os2020/static/js/chunk-2d0e95df.c44365ae.js" rel="prefetch">
   <link href="zb_users/theme/os2020/static/js/chunk-65dd580c.5c503258.js" rel="prefetch">
-  <link href="zb_users/theme/os2020/static/js/chunk-71aae004.a91b6f78.js" rel="prefetch">
   <link href="zb_users/theme/os2020/static/css/chunk-vendors.902ebb66.css" rel="preload" as="style">
-  <link href="zb_users/theme/os2020/static/css/index.095a8c6c.css" rel="preload" as="style">
+  <link href="zb_users/theme/os2020/static/css/index.735497e5.css" rel="preload" as="style">
   <link href="zb_users/theme/os2020/static/js/chunk-vendors.5bb807b7.js" rel="preload" as="script">
-  <link href="zb_users/theme/os2020/static/js/index.2eaf2d8a.js" rel="preload" as="script">
+  <link href="zb_users/theme/os2020/static/js/index.3225736b.js" rel="preload" as="script">
   <link href="zb_users/theme/os2020/static/css/chunk-vendors.902ebb66.css" rel="stylesheet">
-  <link href="zb_users/theme/os2020/static/css/index.095a8c6c.css" rel="stylesheet">
+  <link href="zb_users/theme/os2020/static/css/index.735497e5.css" rel="stylesheet">
 </head>
 
 <body><noscript><strong>We're sorry but {$name} doesn't work properly without JavaScript enabled. Please enable it to
       continue.</strong></noscript>
   <div id="app"></div>
   <script src="zb_users/theme/os2020/static/js/chunk-vendors.5bb807b7.js"></script>
-  <script src="zb_users/theme/os2020/static/js/index.2eaf2d8a.js"></script>
+  <script src="zb_users/theme/os2020/static/js/index.3225736b.js"></script>
 </body>
 
 </html>
