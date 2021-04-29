963e0d16f08d15d07ee0a5aae3b7f01899324ab0
rainbowsoft <rainbowsoft@gmail.com>
2021-04-29 13:41:42 +0800

    改进AdminColor;

--- zb_users/plugin/AdminColor/css.php
+++ zb_users/plugin/AdminColor/css.php
@@ -6,6 +6,8 @@ $id = (int) $zbp->Config('AdminColor')->ColorID;
 $c = '/*admincolor*/';
 if($zbp->Config('AdminColor')->FontSize >= 12 && $zbp->Config('AdminColor')->FontSize <= 14){
     $c .= "body{font-size:{$zbp->Config('AdminColor')->FontSize}px}";
+}else{
+    $zbp->Config('AdminColor')->FontSize = '13';
 }
 $c .= '
   .ui-tooltip, .arrow_leftmenu:after {
