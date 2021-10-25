44ffe56b505764d2c868a8b842115393e35d5172
橙色阳光 <1122@hizhou.cn>
2021-10-25 16:51:14 +0800

    fix: 修复os2020主题上一页、下一页未绑定事件问题

--- zb_users/theme/os2020/template/index.php
+++ zb_users/theme/os2020/template/index.php
@@ -11,25 +11,26 @@
     window.zbpCopyright = '{$copyright}';
   </script>
   <link href="zb_users/theme/os2020/static/css/chunk-22a5707b.5b5dc877.css" rel="prefetch">
-  <link href="zb_users/theme/os2020/static/css/chunk-65dd580c.a8189d03.css" rel="prefetch">
-  <link href="zb_users/theme/os2020/static/js/chunk-22a5707b.97bf40bd.js" rel="prefetch">
-  <link href="zb_users/theme/os2020/static/js/chunk-2d0e95df.c44365ae.js" rel="prefetch">
-  <link href="zb_users/theme/os2020/static/js/chunk-65dd580c.5c503258.js" rel="prefetch">
+  <link href="zb_users/theme/os2020/static/css/chunk-65dd580c.37499b8e.css" rel="prefetch">
+  <link href="zb_users/theme/os2020/static/js/chunk-22a5707b.4d650083.js" rel="prefetch">
+  <link href="zb_users/theme/os2020/static/js/chunk-2d0e95df.cd0d9b0a.js" rel="prefetch">
+  <link href="zb_users/theme/os2020/static/js/chunk-65dd580c.e90c3a90.js" rel="prefetch">
   <link href="zb_users/theme/os2020/static/css/chunk-vendors.902ebb66.css" rel="preload" as="style">
-  <link href="zb_users/theme/os2020/static/css/index.58538027.css" rel="preload" as="style">
-  <link href="zb_users/theme/os2020/static/js/chunk-vendors.5bb807b7.js" rel="preload" as="script">
-  <link href="zb_users/theme/os2020/static/js/index.f8bcbf66.js" rel="preload" as="script">
+  <link href="zb_users/theme/os2020/static/css/index.d0d60a26.css" rel="preload" as="style">
+  <link href="zb_users/theme/os2020/static/js/chunk-vendors.daa570c6.js" rel="preload" as="script">
+  <link href="zb_users/theme/os2020/static/js/index.a5a2f478.js" rel="preload" as="script">
   <link href="zb_users/theme/os2020/static/css/chunk-vendors.902ebb66.css" rel="stylesheet">
-  <link href="zb_users/theme/os2020/static/css/index.58538027.css" rel="stylesheet">
+  <link href="zb_users/theme/os2020/static/css/index.d0d60a26.css" rel="stylesheet">
 </head>
 
 <body>
   <noscript>
-    <strong>We're sorry but {$name} doesn't work properly without JavaScript enabled. Please enable it to continue.</strong>
+    <strong>We're sorry but {$name} doesn't work properly without JavaScript enabled. Please enable it to
+      continue.</strong>
   </noscript>
   <div id="app"></div>
-  <script src="zb_users/theme/os2020/static/js/chunk-vendors.5bb807b7.js"></script>
-  <script src="zb_users/theme/os2020/static/js/index.f8bcbf66.js"></script>
+  <script src="zb_users/theme/os2020/static/js/chunk-vendors.daa570c6.js"></script>
+  <script src="zb_users/theme/os2020/static/js/index.a5a2f478.js"></script>
 </body>
 
 </html>
