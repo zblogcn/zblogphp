3646c9e733ca89132791ba1a3ceedacdd0d15e3e
rainbowsoft <rainbowsoft@gmail.com>
2022-02-22 15:10:07 +0800

    修正了一个关于feed输出的校验问题;

--- zb_system/function/c_system_route.php
+++ zb_system/function/c_system_route.php
@@ -703,8 +703,9 @@ function ViewFeed()
         }
     }
 
+    @ob_clean();
     if (!headers_sent()) {
-        header("Content-type:text/xml; Charset=utf-8");
+        header("Content-type:text/xml; charset=utf-8");
     }
 
     echo $rss2->saveXML();
