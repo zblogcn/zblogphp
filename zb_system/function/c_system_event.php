44344088e6da3949fe3d73634e03031794863766
rainbowsoft <rainbowsoft@gmail.com>
2022-11-16 14:51:40 +0800

    给cookie的username增加一个httponly

--- zb_system/function/c_system_event.php
+++ zb_system/function/c_system_event.php
@@ -74,7 +74,7 @@ function SetLoginCookie($user, $cookieTime)
     $addinfo['useralias'] = $user->StaticName;
     $token = $zbp->GenerateUserToken($user, $cookieTime);
     $secure = HTTP_SCHEME == 'https://';
-    setcookie('username_' . hash("crc32b", $zbp->guid), $user->Name, $cookieTime, $zbp->cookiespath, '', $secure, false);
+    setcookie('username_' . hash("crc32b", $zbp->guid), $user->Name, $cookieTime, $zbp->cookiespath, '', $secure, $zbp->cookie_tooken_httponly);
     setcookie('token_' . hash("crc32b", $zbp->guid), $token, $cookieTime, $zbp->cookiespath, '', $secure, $zbp->cookie_tooken_httponly);
     setcookie('addinfo' . str_replace('/', '', $zbp->cookiespath), json_encode($addinfo), $cookieTime, $zbp->cookiespath, '', $secure, false);
 
