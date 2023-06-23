feaab181fda172aa24c8675ef3650d989ff3f5ea
rainbowsoft <rainbowsoft@gmail.com>
2023-06-23 09:09:11 +0800

    删除2个主题插件;

--- zb_users/theme/OnePage/template/post-multi.php
+++
@@ -1,8 +0,0 @@
-<div class="post multi">
-	<p class="post-date">{$article.Time('Y年m月d日')}</p>
-	<h2 class="post-title"><a href="{$article.Url}">{$article.Title}</a></h2>
-	<div class="post-body">{$article.Intro}</div>
-	<p class="post-footer">
-		作者:{$article.Author.StaticName}&nbsp;,&nbsp;分类:{$article.Category.Name}&nbsp;,&nbsp;浏览:{$article.ViewNums}&nbsp;,&nbsp;评论:{$article.CommNums}
-	</p>
-</div>
