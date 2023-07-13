c3bf240d4e039a6b4bf014901d2f462e70f014b4
zhouzishu <me@zzs.im>
2023-07-13 12:25:23 +0800

    修复 VerifyLogin 中的代码缺失

--- zb_system/function/c_system_event.php
+++ zb_system/function/c_system_event.php
@@ -23,6 +23,20 @@ function VerifyLogin($throwException = true, $ignoreValidCode = true, $ignoreCsr
 {
     global $zbp;
 
+    if ($zbp->option['ZC_LOGIN_CSRFCHECK_ENABLE'] && $ignoreCsrfCheck == false) {
+        $zbp->csrfExpirationMinute = 5;
+        if ($zbp->VerifyCSRFToken(GetVars('csrfToken' , 'POST'), 'login', 'minute') == false) {
+            $zbp->ShowError(5, __FILE__, __LINE__);
+        }
+    }
+
+    if ($zbp->option['ZC_LOGIN_VERIFY_ENABLE'] && $ignoreValidCode == false) {
+        $zbp->verifyCodeExpirationMinute = 5;
+        if ($zbp->CheckValidCode(GetVars('verify' , 'POST'), 'login', 'minute') == false) {
+            $zbp->ShowError(38, __FILE__, __LINE__);
+        }
+    }
+
     /* @var Member $m */
     $m = null;
     if ($zbp->Verify_MD5(trim(GetVars('username', 'POST')), trim(GetVars('password', 'POST')), $m)) {
