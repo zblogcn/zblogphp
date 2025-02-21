8feafc23665942b7b895675d31bd77e6cf63826c
rainbowsoft <rainbowsoft@gmail.com>
2025-02-21 14:30:02 +0800

    up;

--- zb_system/function/c_system_version.php
+++ zb_system/function/c_system_version.php
@@ -22,7 +22,7 @@ $zbpvers['151626']='1.5 Zero Build 151626';
 define('ZC_VERSION_MAJOR', '1');
 define('ZC_VERSION_MINOR', '7');
 define('ZC_VERSION_BUILD', '4');
-define('ZC_VERSION_COMMIT', '3400');
+define('ZC_VERSION_COMMIT', '3410');
 define('ZC_VERSION_CODENAME', 'Shelter');
 define('ZC_VERSION', ZC_VERSION_MAJOR . '.' . ZC_VERSION_MINOR . '.' . ZC_VERSION_BUILD . '.' . ZC_VERSION_COMMIT);
 if (strcasecmp(ZC_VERSION_CODENAME, 'Beta') == 0 || strcasecmp(ZC_VERSION_CODENAME, 'Alpha') == 0) {
