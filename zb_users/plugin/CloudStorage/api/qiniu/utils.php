f3d57d804ddce2d02b3730d42c6106364fcf83b3
未寒 <im@imzhou.com>
2025-09-22 23:44:47 +0800

    chore: 移除支付宝和云存储插件相关文件
    删除支付宝插件和云存储插件的所有相关文件，包括PHP代码、配置文件、图片资源和XML描述文件等。这些插件已不再使用或维护，清理代码库以保持整洁。

--- zb_users/plugin/CloudStorage/api/qiniu/utils.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function Qiniu_Encode($str) // URLSafeBase64Encode
-{
-    $find = array('+', '/');
-    $replace = array('-', '_');
-
-    return str_replace($find, $replace, base64_encode($str));
-}
