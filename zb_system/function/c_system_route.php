e48df0f54824acf401e2cb7e5159392f571bec3f
rainbowsoft <rainbowsoft@gmail.com>
2026-01-31 22:01:28 +0800

    修正了一个搜索后url出现strong的问题;

--- zb_system/function/c_system_route.php
+++ zb_system/function/c_system_route.php
@@ -835,7 +835,7 @@ function ViewSearch()
             $r->Intro = str_replace($q, '<strong>' . $q . '</strong>', $t);
             $r->Content = $a->Content;
         }
-        $r->Title = str_ireplace($q, '<strong>' . $q . '</strong>', $r->Title);
+        //$r->Title = str_ireplace($q, '<strong>' . $q . '</strong>', $r->Title);
         $article->Content .= '<a href="' . $a->Url . '">' . $a->Url . '</a><br/></p>';
         $results[] = $r;
     }
