aeb2fa5698686a4e12acf800fa5b5d696bc072af
wdssmq <wdssmq@qq.com>
2026-09-07 19:18:05 +0800

    up(c_system_version.php): 更新版本号

--- zb_system/function/c_system_version.php
+++ zb_system/function/c_system_version.php
@@ -22,8 +22,8 @@ $zbpvers['151626']='1.5 Zero Build 151626';
 define('ZC_VERSION_MAJOR', '1');
 define('ZC_VERSION_MINOR', '8');
 define('ZC_VERSION_BUILD', '0');
-define('ZC_VERSION_COMMIT', '4000');
-define('ZC_VERSION_CODENAME', 'Martis'); //Arrakis
+define('ZC_VERSION_COMMIT', '4010');
+define('ZC_VERSION_CODENAME', 'Martis'); // Arrakis
 define('ZC_VERSION', ZC_VERSION_MAJOR . '.' . ZC_VERSION_MINOR . '.' . ZC_VERSION_BUILD . '.' . ZC_VERSION_COMMIT);
 if (0 == strcasecmp(ZC_VERSION_CODENAME, 'Beta') || 0 == strcasecmp(ZC_VERSION_CODENAME, 'Alpha')) {
     define('ZC_VERSION_DISPLAY', ZC_VERSION_MAJOR . '.' . ZC_VERSION_MINOR . '.' . ZC_VERSION_BUILD . '.' . ZC_VERSION_COMMIT . ' ' . ZC_VERSION_CODENAME);
